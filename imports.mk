a5947ef35720207e83ed09b99d6e522a8ea96d9c
Maciej Jablonski <mjablonski@solarflare.com>
2020-03-20 18:11:46 +0000

    cleanup: firwmare: switch to RELEASECOLLECTION_VERSION, 7.8.4.1000-internal and sfutils to v8_1_2_1009
    Change-Id: I1f9ed46fce2f8d1a565c31d145b217e263521a34

--- imports.mk
+++ imports.mk
@@ -5,8 +5,14 @@
 
 # This refers to the version of the sfutils codebase and does not contain any
 # information about the firmware versions included in sfupdate.
-SFUTILS_VERSION=v7_6_1_1003
+SFUTILS_VERSION=v8_1_2_1009
 
-# This refers to the firmwarefamily version (replace periods with underscores),
-# which will in turn contain different firmware versions for Siena, Torino, etc.
-FIRMWARE_VERSION=v7_6_5_1003-internal
+# Release collection includes different firmware family versions
+# for different products ranges and within these different selections of
+# firmware images per product. The version number below is two levels of
+# indirection above M/C firmware version.
+#
+# Note: FIRMWARE_VERSIN or FIRMWAREFAMILY_VERSION is deprecated and works
+# with legacy sfutils versions.
+RELEASECOLLECTION_VERSION=v7.8.4.1000-internal
+
