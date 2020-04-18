Xilinx-CNS/onload

commit def49fa6eaa1f5d117a589ed17362306e2bfb8ca

ON-12002: ulhelper: disable ICMP handling

src/driver/linux_onload/driverlink_ip.c
@@ -58,6 +58,7 @@ MODULE_PARM_DESC(oo_bond_peak_polls,
 #endif
 
 
+#if CI_CFG_HANDLE_ICMP
 /* Check whether device may match software filters for Onload.
  *
  * In the ideal world, we'd like to have a fast check if the device is onloadable.
@@ -211,6 +212,7 @@ static struct nf_hook_ops oo_netfilter_ip6_hook = {
 #endif
   .priority = NF_IP_PRI_FIRST,
 };
+#endif
 #endif
 
 
@@ -533,20 +535,24 @@ void oo_driverlink_unregister(void)
 #ifdef EFRM_HAVE_NF_NET_HOOK
 int oo_register_nfhook(struct net *net)
 {
-  int rc;
+  int rc = 0;
+#if CI_CFG_HANDLE_ICMP
   if( (rc = nf_register_net_hook(net, &oo_netfilter_ip_hook)) != 0 )
     return rc;
 #if CI_CFG_IPV6
   if( (rc = nf_register_net_hook(net, &oo_netfilter_ip6_hook)) != 0 )
     nf_unregister_net_hook(net, &oo_netfilter_ip_hook);
 #endif
+#endif
   return rc;
 }
 void oo_unregister_nfhook(struct net *net)
 {
+#if CI_CFG_HANDLE_ICMP
   nf_unregister_net_hook(net, &oo_netfilter_ip_hook);
 #if CI_CFG_IPV6
   nf_unregister_net_hook(net, &oo_netfilter_ip6_hook);
+#endif
 #endif
 }
 #endif

src/driver/linux_onload/linux_stats.c
@@ -77,7 +77,9 @@ struct proc_dir_entry *oo_proc_root = NULL;
  *--------------------------------------------------------------------*/
 
 static const struct file_operations efab_version_fops;
+#if CI_CFG_HANDLE_ICMP
 static const struct file_operations efab_dlfilters_fops;
+#endif
 
 /*--------------------------------------------------------------------
  *
@@ -92,7 +94,9 @@ typedef struct ci_proc_efab_entry_s {
 } ci_proc_efab_entry_t;
 static ci_proc_efab_entry_t ci_proc_efab_table[] = {
     {"version",       &efab_version_fops},
+#if CI_CFG_HANDLE_ICMP
     {"dlfilters",     &efab_dlfilters_fops},
+#endif
 };
 
 #define CI_PROC_EFAB_TABLE_SIZE \
@@ -321,6 +325,7 @@ static const struct file_operations efab_version_fops = {
 };
 
 
+#if CI_CFG_HANDLE_ICMP
 /****************************************************************************
  *
  * /proc/driver/onload/dlfilters
@@ -349,6 +354,7 @@ static const struct file_operations efab_dlfilters_fops = {
     .llseek  = seq_lseek,
     .release = single_release,
 };
+#endif
 
 
 /****************************************************************************

src/include/ci/internal/transport_config_opt.h
@@ -646,6 +646,8 @@
 /* NIC reset, suspend and hot-plug support */
 #define CI_CFG_NIC_RESET_SUPPORT 1
 
+/* Handle incoming ICMP for Onloaded sockets */
+#define CI_CFG_HANDLE_ICMP 1
 
 #ifdef __KERNEL__
 #include <linux/version.h>

src/include/ci/internal/transport_config_opt_ulhelper.h
@@ -24,5 +24,7 @@
 #define CI_CFG_INJECT_PACKETS 0
 #undef CI_CFG_NIC_RESET_SUPPORT
 #define CI_CFG_NIC_RESET_SUPPORT 0
+#undef CI_CFG_HANDLE_ICMP
+#define CI_CFG_HANDLE_ICMP 0
 
 #endif

src/include/onload/driverlink_filter.h
@@ -17,6 +17,7 @@
 #define __CI_DRIVER_EFAB_DRIVERLINK__FILTER_H__
 
 
+#if CI_CFG_HANDLE_ICMP
 struct ci_ether_hdr_s;
 struct efx_dlfilt_cb_s;
 
@@ -66,5 +67,6 @@ extern void
 efx_dlfilter_count_stats(struct efx_dlfilt_cb_s* fcb,
                          int *n_empty, int *n_tomp, int *n_used);
 
+#endif
 #endif /* __CI_DRIVER_EFAB_DRIVERLINK__FILTER_H__ */
 /*! \cidoxg_end */

src/include/onload/driverlink_filter_private.h
@@ -24,6 +24,7 @@
 #endif
 
 
+#if CI_CFG_HANDLE_ICMP
 /*! Defines one entry in the master filter table */
 typedef struct efx_dlfilt_entry_s {
   int       thr_id;     /*!< TCP helper res. ID from char driver 
@@ -58,6 +59,7 @@ typedef struct efx_dlfilt_cb_s {
   void* ctx;
   efx_dlfilter_is_onloaded_t is_onloaded;
 } efx_dlfilter_cb_t;
+#endif
 
 
 #endif /* __CI_DRIVER_EFAB_DRIVERLINK__FILTER__PRIVATE_H__ */

src/include/onload/linux_ip_protocols.h
@@ -27,6 +27,7 @@
 #include <ci/internal/transport_config_opt.h> /* for CI_CFG_ERROR_PASS_UP */
 
 
+#if CI_CFG_HANDLE_ICMP
 /*! efab_handle_ipp_pkt_task -
  * ICMP, IGMP, UDP etc. delivery handler.  Called from the 
  * ARP keventd tasklet. */
@@ -69,6 +70,7 @@ efab_ipp_icmp_for_thr( tcp_helper_resource_t* thr,
 extern void efab_ipp_icmp_qpkt( tcp_helper_resource_t* thr, 
 				struct ci_sock_cmn_s* s,
 				efab_ipp_addr* addr );
+#endif
 
 #endif
 

src/include/onload/tcp_driver.h
@@ -74,7 +74,9 @@ typedef struct efab_tcp_driver_s {
   /*! An overall limit of pinned pages for all sendpage() users */
   int sendpage_pinpages_max;
 
+#if CI_CFG_HANDLE_ICMP
   struct efx_dlfilt_cb_s* dlfilter;
+#endif
 
   /* Dynamic stack list update: flag and wait queue.  Used by tcpdump */
   ci_uint32         stack_list_seq;

commit a2f8e20091bc2339a6b78e28506d25b98e1a67c6

ON-12002: fix RHEL7 ulhelper build
Fixes: def49fa6eaa1f5d117a589ed17362306e2bfb8ca

src/driver/linux_onload/driverlink_ip.c
@@ -493,7 +493,7 @@ int oo_driverlink_register(void)
   if (rc != 0)
     goto fail2;
 
-#ifndef EFRM_HAVE_NF_NET_HOOK
+#if CI_CFG_HANDLE_ICMP && ! defined(EFRM_HAVE_NF_NET_HOOK)
   if( (rc = nf_register_hook(&oo_netfilter_ip_hook)) < 0 )
     goto fail4;
 #if CI_CFG_IPV6
@@ -504,7 +504,7 @@ int oo_driverlink_register(void)
 
   return 0;
 
-#ifndef EFRM_HAVE_NF_NET_HOOK
+#if CI_CFG_HANDLE_ICMP && ! defined(EFRM_HAVE_NF_NET_HOOK)
 #if CI_CFG_IPV6
   fail5:
    nf_unregister_hook(&oo_netfilter_ip_hook);
@@ -522,7 +522,7 @@ int oo_driverlink_register(void)
 
 void oo_driverlink_unregister(void)
 {
-#ifndef EFRM_HAVE_NF_NET_HOOK
+#if CI_CFG_HANDLE_ICMP && ! defined(EFRM_HAVE_NF_NET_HOOK)
   nf_unregister_hook(&oo_netfilter_ip_hook);
 #if CI_CFG_IPV6
   nf_unregister_hook(&oo_netfilter_ip6_hook);