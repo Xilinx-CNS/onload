Xilinx-CNS/onload

commit ff9c62a4c8f8a39e09dc740f3b70e41b77bf995f

ON-11937: ulhelper: do not take stack lock at dtor time
This code should be moved to ulhelper process eventually.

src/lib/efthrm/tcp_helper_resource.c
@@ -5315,6 +5315,7 @@ void tcp_helper_dtor(tcp_helper_resource_t* trs)
    */
   tcp_helper_stop(trs);
 
+#if ! CI_CFG_UL_INTERRUPT_HELPER
   /* Get the stack lock; it is needed for filter removal and leak check. */
   if( ~trs->netif.flags & CI_NETIF_FLAG_WEDGED ) {
     if( efab_tcp_helper_netif_try_lock(trs, 0) ) {
@@ -5323,9 +5324,7 @@ void tcp_helper_dtor(tcp_helper_resource_t* trs)
       oo_inject_packets_kernel(trs, 1);
       oo_deferred_free(&trs->netif);
 
-#if ! CI_CFG_UL_INTERRUPT_HELPER
       tcp_helper_gracious_dtor(trs);
-#endif
     }
     else {
       /* Pretend to be wedged and do not check for leaks */
@@ -5339,6 +5338,7 @@ void tcp_helper_dtor(tcp_helper_resource_t* trs)
       ci_assert(0);
     }
   }
+#endif
 
   /* Remove all filters - and make sure we do not send anything, while
    * closing socket or as a reply to a network packet. */

commit cbabd38fd5a368cc48ce58d83b8e8e483469dc00

ON-12048: ON-11937: ulhelper: pretend to be locked at stack dtor time
Fixes: ff9c62a4c8f8a39e09dc740f3b70e41b77bf995f

src/lib/efthrm/tcp_helper_resource.c
@@ -5350,6 +5350,9 @@ void tcp_helper_dtor(tcp_helper_resource_t* trs)
       ci_assert(0);
     }
   }
+#elif ! defined(NDEBUG)
+  /* Appease dtor code, pretend to be locked */
+  trs->netif.state->lock.lock = CI_EPLOCK_LOCKED;
 #endif
 
   /* Remove all filters - and make sure we do not send anything, while