Xilinx-CNS/onload

commit 544aec383cea559fecad6cca9b7c3b183c8b0bbb

ON-12053: use new TCP helper resource refcounting
On one side, this patch is larger than I'd like to - it is hard to review
and fix it.
On the other side, it leaves unfixed almost all the code which does not
check the result of refcount-taking functions.

src/lib/efthrm/tcp_helper_cluster.c
@@ -123,10 +123,10 @@ static int thc_get_an_orphan(tcp_helper_cluster_t* thc,
   CI_DLLIST_FOR_EACH(link, &thc->thc_thr_list) {
     tcp_helper_resource_t* thr_walk = CI_CONTAINER(tcp_helper_resource_t,
                                                    thc_thr_link, link);
-    if( thr_walk->k_ref_count & TCP_HELPER_K_RC_NO_USERLAND ) {
+    if( thr_walk->ref[OO_THR_REF_APP] == 0 ) {
       rc = 0;
       if( thr_out != NULL ) {
-        rc = efab_tcp_helper_k_ref_count_inc(thr_walk);
+        rc = oo_thr_ref_get(thr_walk->ref, OO_THR_REF_BASE);
         *thr_out = thr_walk;
       }
       break;
@@ -163,7 +163,7 @@ static int /*bool*/ thc_has_live_stacks(tcp_helper_cluster_t* thc)
   CI_DLLIST_FOR_EACH(link, &thc->thc_thr_list) {
     tcp_helper_resource_t* thr_walk = CI_CONTAINER(tcp_helper_resource_t,
                                                    thc_thr_link, link);
-    if( ! (thr_walk->k_ref_count & TCP_HELPER_K_RC_NO_USERLAND) ) {
+    if( thr_walk->ref[OO_THR_REF_APP] != 0 ) {
       rc = 1;
       break;
     }
@@ -567,7 +567,7 @@ static int thc_kill_an_orphan(tcp_helper_cluster_t* thc)
     /* remove reference taken by thc_get_an_orphan()
      * Note: this will likely trigger stack destruction
      */
-    efab_tcp_helper_k_ref_count_dec(thr);
+    oo_thr_ref_drop(thr->ref, OO_THR_REF_BASE);
 
   rc = wait_event_interruptible_timeout(thc->thr_release_done,
                                         ! thc_contains_thr(thc, thr), 10 * HZ);
@@ -674,8 +674,8 @@ static int thc_get_prior_round_robin_index(const tcp_helper_cluster_t* thc)
 
 /* Look for a suitable stack within the cluster.
  *
- * You need to efab_thr_release() the stack returned by this function
- * when done.
+ * You need to oo_thr_ref_drop(OO_THR_REF_BASE) the stack returned by this
+ * function when done.
  *
  * You must hold the thc_mutex before calling this function.
  */
@@ -698,11 +698,11 @@ static int thc_get_thr(tcp_helper_cluster_t* thc,
   CI_DLLIST_FOR_EACH(link, &thc->thc_thr_list) {
     tcp_helper_resource_t* thr_walk = CI_CONTAINER(tcp_helper_resource_t,
                                                    thc_thr_link, link);
-    if( (~thr_walk->k_ref_count & TCP_HELPER_K_RC_NO_USERLAND) &&
+    if( thr_walk->ref[OO_THR_REF_APP] != 0 &&
        thr_walk->thc_tid == current->pid &&
        oof_socket_can_update_stack(oo_filter_ns_to_manager(thc->thc_filter_ns),
                                    oofilter, thr_walk) ) {
-      efab_thr_ref(thr_walk);
+      oo_thr_ref_get(thr_walk->ref, OO_THR_REF_BASE);
       *thr_out = thr_walk;
       ci_irqlock_unlock(&THR_TABLE.lock, &lock_flags);
       return 0;
@@ -723,8 +723,8 @@ static int thc_get_thr(tcp_helper_cluster_t* thc,
  * If a stack is found for reheating, the stack owner will be updated to
  * the current pid/tid.
  *
- * You need to efab_thr_release() the stack returned by this function
- * when done.
+ * You need to oo_thr_ref_drop(OO_THR_REF_APP) the stack returned by this
+ * function when done.
  *
  * You must hold the thc_mutex before calling this function.
  *
@@ -798,11 +798,7 @@ static int thc_get_thr_reheat(tcp_helper_cluster_t* thc,
       thc->thc_thr_rrobin_index = 0;
   }
   if( thr != NULL ) {
-    if( ~thr->k_ref_count & TCP_HELPER_K_RC_NO_USERLAND ) {
-      efab_thr_ref(thr);
-      rc = 0;
-    }
-    else {
+    if( (rc = oo_thr_ref_get(thr->ref, OO_THR_REF_APP)) != 0 ) {
       /* Stack is dying; let thc_alloc_thr() kill an orphan. */
       thr = NULL;
       rc = 1;
@@ -849,8 +845,8 @@ static void thc_round_robin_add(tcp_helper_cluster_t*   thc,
 
 /* Allocates a new stack in thc.
  *
- * You need to efab_thr_release() the stack returned by this function
- * when done.
+ * You need to oo_thr_ref_drop(OO_THR_REF_APP) the stack returned by this
+ * function when done.
  */
 static int thc_alloc_thr(tcp_helper_cluster_t* thc,
                          int cluster_restart_opt,
@@ -1226,7 +1222,7 @@ int tcp_helper_cluster_alloc_thr(const char* cname,
     /* TODO: wait for resources if another's cluster destruction is in progress */
     rc = thc_install_tproxy(thc, ni_opts);
     if( rc != 0 )
-      efab_thr_release(thr);
+      oo_thr_ref_drop(thr->ref, OO_THR_REF_APP);
   }
   if( thc_alloced || thc_found )
     /* free the reference we have taken in thc_alloc() or thc_search_by_name() */
@@ -1561,7 +1557,7 @@ int efab_tcp_helper_reuseport_bind(ci_private_t *priv, void *arg)
      * (firmware needs initialized vi) */
     rc = thc_install_tproxy(thc, &NI_OPTS(ni));
     if( rc != 0 )
-      efab_thr_release(thr);
+      oo_thr_ref_drop(thr->ref, OO_THR_REF_APP);
   }
 
   tcp_helper_cluster_release(thc, NULL);
@@ -1583,10 +1579,10 @@ int efab_tcp_helper_reuseport_bind(ci_private_t *priv, void *arg)
    * * reference to the destination stack (thr) */
 
   /* thr referencing scheme comes from efab_file_move_to_alien_stack_rsop */
-  efab_thr_ref(thr);
+  oo_thr_ref_get(thr->ref, OO_THR_REF_APP);
   rc = efab_file_move_to_alien_stack(priv, &thr->netif, 0, &new_sock_id);
   if( rc != 0 ) {
-    efab_thr_release(thr);
+    oo_thr_ref_drop(thr->ref, OO_THR_REF_APP);
     do_sock_unlock = 0;
     /* both sockets are unlocked, and there is still single reference to thr */
   }
@@ -1610,7 +1606,7 @@ int efab_tcp_helper_reuseport_bind(ci_private_t *priv, void *arg)
     oof_socket_del(fm, &dummy_oofilter);
   /* Drop the reference we got from thc_get_thr or thc_alloc_thr().
    * If things went wrong both stack and cluster might disappear. */
-  efab_thr_release(thr);
+  oo_thr_ref_drop(thr->ref, OO_THR_REF_APP);
   oof_socket_dtor(&dummy_oofilter);
   mutex_unlock(&thc_init_mutex);
 unlock_sock:

src/driver/linux_onload/driver.c
@@ -332,7 +332,7 @@ int oo_fop_release(struct inode* inode, struct file* filp)
   filp->private_data = 0;
   if (priv->thr != NULL) {
     TCP_HELPER_RESOURCE_ASSERT_VALID(priv->thr, 0);
-    efab_thr_release(priv->thr);
+    oo_thr_ref_drop(priv->thr->ref, OO_THR_REF_APP);
   }
   if (priv->priv_cp != NULL)
     cp_release(priv->priv_cp);

src/driver/linux_onload/driverlink_ip.c
@@ -335,7 +335,8 @@ static void oo_dl_remove(struct efx_dl_device* dl_dev)
     efrm_client_disable_post_reset(onic->efrm_client);
 
     onic->oo_nic_flags |= OO_NIC_UNPLUGGED;
-    while( iterate_netifs_unlocked(&ni, 0, 0) == 0 )
+    while( iterate_netifs_unlocked(&ni, OO_THR_REF_BASE,
+                                     OO_THR_REF_INFTY) == 0 )
       tcp_helper_flush_resets(ni);
 #endif
 
@@ -372,7 +373,8 @@ static void oo_fixup_wakeup_breakage(struct net_device* dev)
   int hwport, intf_i;
   if( (onic = oo_nic_find_dev(dev)) != NULL ) {
     hwport = onic - oo_nics;
-    while( iterate_netifs_unlocked(&ni, 0, 0) == 0 )
+    while( iterate_netifs_unlocked(&ni, OO_THR_REF_BASE,
+                                   OO_THR_REF_INFTY) == 0 )
       if( (intf_i = ni->hwport_to_intf_i[hwport]) >= 0 )
         ci_bit_clear(&ni->state->evq_primed, intf_i);
   }

src/driver/linux_onload/epoll_device.c
@@ -139,8 +139,8 @@ static int oo_epoll_add_stack(struct oo_epoll_private* priv,
       continue;
     priv->stacks[i] = fd_thr;
     /* We already keep ref for this thr via file,
-     * so efab_tcp_helper_k_ref_count_inc() can't fail. */
-    efab_tcp_helper_k_ref_count_inc(fd_thr);
+     * so oo_thr_ref_get() can't fail. */
+    oo_thr_ref_get(fd_thr->ref, OO_THR_REF_BASE);
     break;
   }
   spin_unlock(&priv->lock);
@@ -155,7 +155,7 @@ static void oo_epoll_release_common(struct oo_epoll_private* priv)
   for( i = 0; i < epoll_max_stacks; i++ ) {
     if( priv->stacks[i] == NULL )
       break;
-    efab_tcp_helper_k_ref_count_dec(priv->stacks[i]);
+    oo_thr_ref_drop(priv->stacks[i]->ref, OO_THR_REF_BASE);
     priv->stacks[i] = NULL;
   }
   kfree(priv->stacks);
@@ -175,12 +175,12 @@ static int set_max_stacks(const char *val,
   return 0;
 }
 
-#define OO_EPOLL_FOR_EACH_STACK(priv, i, thr, ni)                       \
-  for( i = 0; i < epoll_max_stacks; ++i )                              \
-    if( (thr = (priv)->stacks[i]) == NULL )                             \
-      break;                                                            \
-    else if(unlikely( thr->k_ref_count & TCP_HELPER_K_RC_NO_USERLAND )) \
-      continue;                                                         \
+#define OO_EPOLL_FOR_EACH_STACK(priv, i, thr, ni)      \
+  for( i = 0; i < epoll_max_stacks; ++i )              \
+    if( (thr = (priv)->stacks[i]) == NULL )            \
+      break;                                           \
+    else if(unlikely( thr->ref[OO_THR_REF_APP] == 0 )) \
+      continue;                                        \
     else if( (ni = &thr->netif) || 1 )
 
 

src/driver/linux_onload/linux_stats.c
@@ -118,7 +118,7 @@ efab_stacks_seq_start(struct seq_file *seq, loff_t *ppos)
   int i, rc;
 
   for( i = 0; i <= *ppos; i++) {
-    rc = iterate_netifs_unlocked(&ni, 0, 0);
+    rc = iterate_netifs_unlocked(&ni, OO_THR_REF_BASE, OO_THR_REF_INFTY);
     if( rc != 0 )
       return NULL;
   }
@@ -131,7 +131,7 @@ efab_stacks_seq_next(struct seq_file *seq, void *v, loff_t *ppos)
   ci_netif *ni = v;
   int rc;
   (*ppos)++;
-  rc = iterate_netifs_unlocked(&ni, 0, 0);
+  rc = iterate_netifs_unlocked(&ni, OO_THR_REF_BASE, OO_THR_REF_INFTY);
   if( rc != 0 )
     return NULL;
   return ni;
@@ -141,7 +141,7 @@ static void
 efab_stacks_seq_stop(struct seq_file *seq, void *v)
 {
   if( v )
-    iterate_netifs_unlocked_dropref(v);
+    iterate_netifs_unlocked_dropref(v, OO_THR_REF_BASE);
 }
 
 static int
@@ -204,7 +204,7 @@ efab_stacks_seq_start_ul(struct seq_file *seq, loff_t *ppos)
   int i, rc;
 
   for( i = 0; i <= *ppos; i++) {
-    rc = iterate_netifs_unlocked(&ni, 0, 1);
+    rc = iterate_netifs_unlocked(&ni, OO_THR_REF_FILE, OO_THR_REF_INFTY);
     if( rc != 0 )
       return NULL;
   }
@@ -217,16 +217,23 @@ efab_stacks_seq_next_ul(struct seq_file *seq, void *v, loff_t *ppos)
   ci_netif *ni = v;
   int rc;
   (*ppos)++;
-  rc = iterate_netifs_unlocked(&ni, 0, 1);
+  rc = iterate_netifs_unlocked(&ni, OO_THR_REF_FILE, OO_THR_REF_INFTY);
   if( rc != 0 )
     return NULL;
   return ni;
 }
 
+static void
+efab_stacks_seq_stop_ul(struct seq_file *seq, void *v)
+{
+  if( v )
+    iterate_netifs_unlocked_dropref(v, OO_THR_REF_FILE);
+}
+
 static struct seq_operations efab_stacks_ul_seq_ops = {
   .start    = efab_stacks_seq_start_ul,
   .next     = efab_stacks_seq_next_ul,
-  .stop     = efab_stacks_seq_stop,
+  .stop     = efab_stacks_seq_stop_ul,
   .show     = efab_stacks_seq_show,
 };
 
@@ -254,7 +261,7 @@ efab_stacks_seq_start_k(struct seq_file *seq, loff_t *ppos)
   int i, rc;
 
   for( i = 0; i <= *ppos; i++) {
-    rc = iterate_netifs_unlocked(&ni, 1, 0);
+    rc = iterate_netifs_unlocked(&ni, OO_THR_REF_BASE, OO_THR_REF_APP);
     if( rc != 0 )
       return NULL;
   }
@@ -267,7 +274,7 @@ efab_stacks_seq_next_k(struct seq_file *seq, void *v, loff_t *ppos)
   ci_netif *ni = v;
   int rc;
   (*ppos)++;
-  rc = iterate_netifs_unlocked(&ni, 1, 0);
+  rc = iterate_netifs_unlocked(&ni, OO_THR_REF_BASE, OO_THR_REF_APP);
   if( rc != 0 )
     return NULL;
   return ni;

src/driver/linux_onload/mmap.c
@@ -238,8 +238,8 @@ static void vm_op_open(struct vm_area_struct* vma)
   map = (tcp_helper_resource_t*) vma->vm_private_data;
   TCP_HELPER_RESOURCE_ASSERT_VALID(map, 0);
 
-  OO_DEBUG_TRAMP(ci_log("vm_op_open: %u vma=%p rs_refs=%d",
-		 map->id, vma, (int) oo_atomic_read(&map->ref_count)));
+  OO_DEBUG_TRAMP(ci_log("vm_op_open: %u vma=%p refs: "OO_THR_REF_FMT,
+		 map->id, vma, OO_THR_REF_ARG(map->ref)));
 
   if( OO_MMAP_OFFSET_TO_MAP_ID(VMA_OFFSET(vma)) == CI_NETIF_MMAP_ID_STATE ) {
     rc = efab_add_mm_ref (vma->vm_mm);
@@ -254,8 +254,8 @@ static void vm_op_close(struct vm_area_struct* vma)
   tcp_helper_resource_t* map;
   map = (tcp_helper_resource_t*) vma->vm_private_data;
 
-  OO_DEBUG_TRAMP(ci_log("vm_op_close: %u vma=%p rs_refs=%d",
-		 map->id, vma, (int) oo_atomic_read(&map->ref_count)));
+  OO_DEBUG_TRAMP(ci_log("vm_op_close: %u vma=%p refs: "OO_THR_REF_FMT,
+		 map->id, vma, OO_THR_REF_ARG(map->ref)));
 
   if( OO_MMAP_OFFSET_TO_MAP_ID(VMA_OFFSET(vma)) == CI_NETIF_MMAP_ID_STATE )
     efab_del_mm_ref (vma->vm_mm);

src/driver/linux_onload/onloadfs.c
@@ -383,7 +383,7 @@ oo_create_fd(tcp_helper_resource_t* thr, oo_sp ep_id, int flags, int fd_type,
                           rc, thr->id, ep_id));
       goto ret_put_fd;
     }
-    efab_thr_ref(thr);
+    oo_thr_ref_get(thr->ref, OO_THR_REF_APP);
   }
 
   fd_install(fd, priv->_filp);

src/driver/linux_onload/terminate.c
@@ -158,7 +158,7 @@ efab_terminate_find_all_stacks(tcp_helper_resource_t *stacks[],
         /* We already keep a ref for this thr via file, but we need
          * one more ref because other threads may close this file
          * before we'll kill them. */
-        efab_thr_ref(thr);
+        oo_thr_ref_get(thr->ref, OO_THR_REF_BASE);
         stacks[stacks_num++] = thr;
       }
       fput(file);
@@ -192,7 +192,7 @@ efab_terminate_lock_all_stacks(tcp_helper_resource_t *stacks[],
         != 0 ) {
       OO_DEBUG_ERR(ci_log("Pid %d failed to terminate stack %d properly",
                           current->pid, thr->id));
-      efab_thr_release(thr);
+      oo_thr_ref_drop(thr->ref, OO_THR_REF_BASE);
       stacks[0] = NULL;
     }
     ci_assert(ci_netif_is_locked(&thr->netif));
@@ -242,7 +242,7 @@ efab_terminate_lock_all_stacks(tcp_helper_resource_t *stacks[],
         if( tries >= MAX_ADD_TRIES + stacks_num ) {
           OO_DEBUG_ERR(ci_log("Pid %d failed to terminate stack %d properly",
                               current->pid, thr->id));
-          efab_thr_release(thr);
+          oo_thr_ref_drop(stacks[i]->ref, OO_THR_REF_BASE);
           stacks[i] = NULL;
         }
       }
@@ -270,7 +270,7 @@ efab_terminate_unlock_all_stacks(tcp_helper_resource_t *stacks[],
       continue;
     ci_netif_unlock(&stacks[i]->netif);
     TERM_DEBUG("%s: %d unlock stack %d", __func__, current->pid, stacks[i]->id);
-    efab_thr_release(stacks[i]);
+    oo_thr_ref_drop(stacks[i]->ref, OO_THR_REF_BASE);
   }
 }
 

commit 681a5195865c9c5385e3ca8feec592e6f3347cc3

ON-12053: fix clustered stack refcounting
Use OO_THR_REF_APP for clustered stacks which will go to user.

Fixes: 544aec383ce ON-12053: use new TCP helper resource refcounting

src/lib/efthrm/tcp_helper_cluster.c
@@ -126,7 +126,7 @@ static int thc_get_an_orphan(tcp_helper_cluster_t* thc,
     if( thr_walk->ref[OO_THR_REF_APP] == 0 ) {
       rc = 0;
       if( thr_out != NULL ) {
-        rc = oo_thr_ref_get(thr_walk->ref, OO_THR_REF_BASE);
+        rc = oo_thr_ref_get(thr_walk->ref, OO_THR_REF_APP);
         *thr_out = thr_walk;
       }
       break;
@@ -567,7 +567,7 @@ static int thc_kill_an_orphan(tcp_helper_cluster_t* thc)
     /* remove reference taken by thc_get_an_orphan()
      * Note: this will likely trigger stack destruction
      */
-    oo_thr_ref_drop(thr->ref, OO_THR_REF_BASE);
+    oo_thr_ref_drop(thr->ref, OO_THR_REF_APP);
 
   rc = wait_event_interruptible_timeout(thc->thr_release_done,
                                         ! thc_contains_thr(thc, thr), 10 * HZ);
@@ -674,7 +674,7 @@ static int thc_get_prior_round_robin_index(const tcp_helper_cluster_t* thc)
 
 /* Look for a suitable stack within the cluster.
  *
- * You need to oo_thr_ref_drop(OO_THR_REF_BASE) the stack returned by this
+ * You need to oo_thr_ref_drop(OO_THR_REF_APP) the stack returned by this
  * function when done.
  *
  * You must hold the thc_mutex before calling this function.
@@ -702,7 +702,7 @@ static int thc_get_thr(tcp_helper_cluster_t* thc,
        thr_walk->thc_tid == current->pid &&
        oof_socket_can_update_stack(oo_filter_ns_to_manager(thc->thc_filter_ns),
                                    oofilter, thr_walk) ) {
-      oo_thr_ref_get(thr_walk->ref, OO_THR_REF_BASE);
+      oo_thr_ref_get(thr_walk->ref, OO_THR_REF_APP);
       *thr_out = thr_walk;
       ci_irqlock_unlock(&THR_TABLE.lock, &lock_flags);
       return 0;