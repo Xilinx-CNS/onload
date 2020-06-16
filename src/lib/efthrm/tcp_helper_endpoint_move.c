Xilinx-CNS/onload

commit 4b9f3ce0164976c948f86306e40ff8fe7399aafe

ON-12053: warn_unused_result for thr refcounting
Fix all remaining cases when tcp helper reference counter was incremented
without proper check.

src/lib/efthrm/tcp_helper_endpoint_move.c
@@ -600,31 +600,37 @@ int efab_file_move_to_alien_stack_rsop(ci_private_t *stack_priv, void *arg)
 
   w = SP_TO_WAITABLE(&old_thr->netif, sock_priv->sock_id);
   rc = ci_sock_lock(&old_thr->netif, w);
-  if( rc != 0 ) {
-    fput(sock_file);
-    return rc;
-  }
+  if( rc != 0 )
+    goto sock_lock_fail;
 
   rc = ci_netif_lock(&old_thr->netif);
-  if( rc != 0 ) {
-    ci_sock_unlock(&old_thr->netif, w);
+  if( rc != 0 )
+    goto netif_lock_fail;
+
+  rc = oo_thr_ref_get(new_thr->ref, OO_THR_REF_APP);
+  if( rc != 0 )
+    goto ref_get_fail;
+
+  rc = efab_file_move_to_alien_stack(sock_priv, &stack_priv->thr->netif, 0,
+                                     &new_sock_id);
+
+  if( rc == 0 ) {
+    fput(sock_file);
+    return 0;
+  }
+  else {
+    /* Stack and socket are unlocked by move_to_alien() in case of error. */
+    oo_thr_ref_drop(new_thr->ref, OO_THR_REF_APP);
     fput(sock_file);
     return rc;
   }
 
-  oo_thr_ref_get(new_thr->ref, OO_THR_REF_APP);
-  rc = efab_file_move_to_alien_stack(sock_priv, &stack_priv->thr->netif, 0,
-                                     &new_sock_id);
+ ref_get_fail:
+  ci_netif_unlock(&old_thr->netif);
+ netif_lock_fail:
+  ci_sock_unlock(&old_thr->netif, w);
+ sock_lock_fail:
   fput(sock_file);
-
-  if( rc != 0 )
-    oo_thr_ref_drop(new_thr->ref, OO_THR_REF_APP);
-  else {
-    ci_netif_unlock(&new_thr->netif);
-    ci_sock_unlock(&new_thr->netif,
-                   SP_TO_WAITABLE(&new_thr->netif, new_sock_id));
-  }
-
   return rc;
 }
 

src/driver/linux_onload/epoll_device.c
@@ -120,6 +120,7 @@ static int oo_epoll_add_stack(struct oo_epoll_private* priv,
                               tcp_helper_resource_t* fd_thr)
 {
   unsigned i;
+  int rc;
 
   /* Common case is that we already know about this stack, so make that
    * fast.
@@ -138,13 +139,12 @@ static int oo_epoll_add_stack(struct oo_epoll_private* priv,
     if( priv->stacks[i] != NULL )
       continue;
     priv->stacks[i] = fd_thr;
-    /* We already keep ref for this thr via file,
-     * so oo_thr_ref_get() can't fail. */
-    oo_thr_ref_get(fd_thr->ref, OO_THR_REF_BASE);
-    break;
+    rc = oo_thr_ref_get(fd_thr->ref, OO_THR_REF_BASE);
+    spin_unlock(&priv->lock);
+    return rc == 0;
   }
   spin_unlock(&priv->lock);
-  return i < epoll_max_stacks;
+  return 0;
 }
 
 static void oo_epoll_release_common(struct oo_epoll_private* priv)

src/driver/linux_onload/onloadfs.c
@@ -368,9 +368,11 @@ oo_create_fd(tcp_helper_resource_t* thr, oo_sp ep_id, int flags,
                           rc, thr->id, ep_id));
       goto ret_put_fd;
     }
-    oo_thr_ref_get(thr->ref,
-                   (fd_flags & OO_FDFLAG_SERVICE) ?
-                   OO_THR_REF_FILE : OO_THR_REF_APP);
+    rc = oo_thr_ref_get(thr->ref,
+                        (fd_flags & OO_FDFLAG_SERVICE) ?
+                        OO_THR_REF_FILE : OO_THR_REF_APP);
+    if( rc != 0 )
+      goto ret_put_file;
   }
 
   fd_install(fd, priv->_filp);
@@ -378,6 +380,8 @@ oo_create_fd(tcp_helper_resource_t* thr, oo_sp ep_id, int flags,
     *_filp = priv->_filp;
   return fd;
 
+ret_put_file:
+  fput(priv->_filp);
 ret_put_fd:
   put_unused_fd(fd);
   return rc;

src/driver/linux_onload/terminate.c
@@ -158,8 +158,8 @@ efab_terminate_find_all_stacks(tcp_helper_resource_t *stacks[],
         /* We already keep a ref for this thr via file, but we need
          * one more ref because other threads may close this file
          * before we'll kill them. */
-        oo_thr_ref_get(thr->ref, OO_THR_REF_BASE);
-        stacks[stacks_num++] = thr;
+        if( oo_thr_ref_get(thr->ref, OO_THR_REF_BASE) == 0 )
+          stacks[stacks_num++] = thr;
       }
       fput(file);
     }

src/include/onload/tcp_helper_ref.h
@@ -57,6 +57,9 @@ extern oo_thr_ref_release_fn oo_thr_ref_release[OO_THR_REF_INFTY];
  * created.
  */
 static inline int
+oo_thr_ref_get_one(oo_thr_ref_t ref,
+                   enum oo_thr_ref_type type) OO_MUST_CHECK_RET;
+static inline int
 oo_thr_ref_get_one(oo_thr_ref_t ref, enum oo_thr_ref_type type)
 {
   ci_uint32 val;
@@ -102,6 +105,9 @@ oo_thr_ref_drop_one(oo_thr_ref_t ref, enum oo_thr_ref_type type)
  */
 static inline int
 oo_thr_ref_upgrade(oo_thr_ref_t ref, enum oo_thr_ref_type from,
+                       enum oo_thr_ref_type to) OO_MUST_CHECK_RET;
+static inline int
+oo_thr_ref_upgrade(oo_thr_ref_t ref, enum oo_thr_ref_type from,
                        enum oo_thr_ref_type to)
 {
   enum oo_thr_ref_type t;
@@ -124,6 +130,8 @@ oo_thr_ref_upgrade(oo_thr_ref_t ref, enum oo_thr_ref_type from,
 }
 
 /* Get a full stack of refcounts up to type. */
+static inline int
+oo_thr_ref_get(oo_thr_ref_t ref, enum oo_thr_ref_type type) OO_MUST_CHECK_RET;
 static inline int
 oo_thr_ref_get(oo_thr_ref_t ref, enum oo_thr_ref_type type)
 {

src/lib/efthrm/tcp_helper_cluster.c
@@ -1553,7 +1553,11 @@ int efab_tcp_helper_reuseport_bind(ci_private_t *priv, void *arg)
    * * reference to the destination stack (thr) */
 
   /* thr referencing scheme comes from efab_file_move_to_alien_stack_rsop */
-  oo_thr_ref_get(thr->ref, OO_THR_REF_APP);
+  rc = oo_thr_ref_get(thr->ref, OO_THR_REF_APP);
+  if( rc != 0 ) {
+    ci_netif_unlock(ni);
+    goto drop_and_done;
+  }
   rc = efab_file_move_to_alien_stack(priv, &thr->netif, 0, &new_sock_id);
   if( rc != 0 ) {
     oo_thr_ref_drop(thr->ref, OO_THR_REF_APP);

src/lib/efthrm/tcp_helper_resource.c
@@ -701,9 +701,8 @@ int efab_thr_table_lookup(const char* name, struct net* netns,
       }
       else {
         /* Success */
-        oo_thr_ref_get(thr->ref, OO_THR_REF_BASE);
+        rc = oo_thr_ref_get(thr->ref, OO_THR_REF_BASE);
         *thr_p = thr;
-        rc = 0;
       }
       break;
     }

commit 7cebd9c33041583e1871e28d1c2a57dcf7fb3f41

ON-12210: fix move_fd
Properly unlock stack and socket in OO_IOC_MOVE_FD when it succeeds.

Fixes: 4b9f3ce0164 ON-12053: warn_unused_result for thr refcounting

src/lib/efthrm/tcp_helper_endpoint_move.c
@@ -615,6 +615,9 @@ int efab_file_move_to_alien_stack_rsop(ci_private_t *stack_priv, void *arg)
                                      &new_sock_id);
 
   if( rc == 0 ) {
+    ci_netif_unlock(&new_thr->netif);
+    ci_sock_unlock(&new_thr->netif,
+                   SP_TO_WAITABLE(&new_thr->netif, new_sock_id));
     fput(sock_file);
     return 0;
   }