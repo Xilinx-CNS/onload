Xilinx-CNS/onload

commit 7243d3d5c73f27935008b0563647f3c3cf73ead2

ON-12053: fix race between kill_stack() and oo_netif_apps_gone()
Move n_ep_orphaned back to kernel-only structure in case of non-ulhelper
build.  It allows to ensure that tcp_helper_kill_stack() is able to tell
dtor process "do not care about orphan endpoints", even if kill_stack()
runs before that orphan endpoints are counted by dtor.

Get the orphan-associated refcount when the stack is created.  It serves
2 goals: fixes the race above and removes oo_thr_ref_get() from dtor path.
oo_thr_ref_get() can fail, and it is not easy to handle such failure
on the dtor path.

src/lib/efthrm/tcp_helper_resource.c
@@ -465,10 +465,10 @@ void tcp_helper_kill_stack(tcp_helper_resource_t *thr)
   /* Atomically replace n_ep_orphaned by 0: the code which sets 0 is
    * responsible for decrementing refcount. */
   do {
-    n_ep_orphaned = netif->state->n_ep_orphaned;
+    n_ep_orphaned = netif->n_ep_orphaned;
     if( n_ep_orphaned == 0 )
       break;
-  } while( ci_cas32u_fail(&netif->state->n_ep_orphaned, n_ep_orphaned, 0) );
+  } while( ci_cas32u_fail(&netif->n_ep_orphaned, n_ep_orphaned, 0) );
 
   if( n_ep_orphaned > 0 ) {
     ci_log("%s: ERROR: force-kill stack [%d]: "
@@ -2049,6 +2049,12 @@ allocate_netif_resources(ci_resource_onload_alloc_t* alloc,
 #endif
   ns->n_ep_bufs = 0;
   ns->nic_n = trs->netif.nic_n;
+
+#if CI_CFG_UL_INTERRUPT_HELPER
+  ns->n_ep_orphaned = OO_N_EP_ORPHANED_INIT;
+#else
+  ni->n_ep_orphaned = OO_N_EP_ORPHANED_INIT;
+#endif
 
   /* An entry in intf_i_to_hwport should not be touched if the intf does
    * not exist.  Belt-and-braces: initialise to 0.
@@ -3673,8 +3679,10 @@ int tcp_helper_rm_alloc(ci_resource_onload_alloc_t* alloc,
    * that lower-level dtor does not start until the previous one has
    * finished.  To archive this, each dtor level drops next-level refcount
    * when done.
+   *
+   * And another additional BASE reference is for non-zero n_ep_orphaned.
    */
-  rs->ref[OO_THR_REF_BASE] = 3;
+  rs->ref[OO_THR_REF_BASE] = 4;
   rs->ref[OO_THR_REF_FILE] = 2;
   rs->ref[OO_THR_REF_APP] = 1;
 
@@ -5065,12 +5073,16 @@ efab_tcp_helper_rm_free_locked(tcp_helper_resource_t* trs)
    */
   tcp_helper_close_pending_endpoints(trs);
 
-  /* The netif->state is not mapped to UL any more, so we can use the state
-   * values as trusted after oo_netif_apps_gone() has set it.
-   */
-  if( oo_netif_apps_gone(netif) ) {
-    /* Get a refcount associated with non-zero n_ep_orphaned */
-    oo_thr_ref_get(trs->ref, OO_THR_REF_BASE);
+  {
+    /* Do we have any endpoints which can't be dropped yet? */
+    ci_uint32 n_orphaned = 0;
+    n_orphaned = oo_netif_apps_gone(netif);
+    if( ci_cas32u_succeed(&netif->n_ep_orphaned, OO_N_EP_ORPHANED_INIT,
+                          n_orphaned) && n_orphaned == 0 ) {
+      /* Who sets n_ep_orphaned to zero, the same code should drop the
+       * associated BASE refcount. */
+      oo_thr_ref_drop(trs->ref, OO_THR_REF_BASE);
+    }
   }
 
   /* Drop lock so that sockets can proceed towards close. */
@@ -7896,6 +7908,14 @@ static void thr_release_app(oo_thr_ref_t ref)
   OO_DEBUG_TCPH(ci_log("%s [%d] "OO_THR_REF_FMT, __func__, thr->id,
                        OO_THR_REF_ARG(ref)));
   efab_notify_stacklist_change(thr);
+  /* We could run the most part of efab_tcp_helper_rm_free_locked() here.
+   * But as we don't trust the "services" running in UL, we'll have to
+   * re-do this at thr_release_file() above.  So it does not worth the
+   * hassle.
+   *
+   * However it may result in slightly different behaviour when
+   * onload_tcpdump is present in somewhat broken cases like ON-2108.
+   */
   oo_thr_ref_drop(ref, OO_THR_REF_FILE);
 }
 

src/include/ci/internal/ip_shared_types.h
@@ -1203,11 +1203,14 @@ struct ci_netif_state_s {
   ci_uint32  max_ep_bufs;                /**< Upper limit of end points */
   CI_ULCONST ci_uint32  n_ep_bufs;       /**< Number of available endpoints */
 
-  /* Number of orphaned endpoints after all the applications have gone.
-   * Must be modified in atomic way, because locking is not always trusted
-   * in the context it is used.
+#if CI_CFG_UL_INTERRUPT_HELPER
+  /* Number of orphaned sockets which prevent the stack from destroying.
+   *
+   * In case of non-ulhelper build profile we use a similar field in the
+   * tcp helper resource, so that it can be trusted.
    */
   ci_uint32 n_ep_orphaned;
+#endif
 
 #if CI_CFG_EPOLL3
   ci_int32              ready_list_pid[CI_CFG_N_READY_LISTS];

src/include/ci/internal/ip_types.h
@@ -159,6 +159,15 @@ struct ci_netif_s {
   struct tcp_helper_endpoint_s**  ep_tbl;
   ci_uint32                       ep_tbl_n;
   unsigned                        ep_tbl_max;
+
+#if ! CI_CFG_UL_INTERRUPT_HELPER
+  /* Number of orphaned sockets which prevent the stack from destroying.
+   *
+   * In case of ulhelper build profile we use a similar field in the netif
+   * state.
+   */
+  ci_uint32 n_ep_orphaned;
+#endif
 #endif
 
   /* This is pointer to the shared state of packet sets */

src/include/onload/netif_dtor.h
@@ -1,3 +1,5 @@
+#ifndef __ONLOAD_NETIF_DTOR_H__
+#define __ONLOAD_NETIF_DTOR_H__
 
 #include <ci/internal/ip.h>
 
@@ -6,14 +8,28 @@
 #define OO_DO_STACK_DTOR 1
 
 
+#if CI_CFG_UL_INTERRUPT_HELPER
+/* n_ep_orphaned is protected by the shared stack lock */
+#define n_ep_orphaned(ni) (ni)->state->n_ep_orphaned
+#else
+/* n_ep_orphaned must be changed atomically */
+#define n_ep_orphaned(ni) (ni)->n_ep_orphaned
+#endif
+
 /* Release all the deferred packets */
 void oo_deferred_free(ci_netif *ni);
 
 /* Get all RX and TX complete events and check for packet leaks. */
 void oo_netif_dtor_pkts(ci_netif* ni);
 
-int/*bool*/ oo_netif_apps_gone(ci_netif* netif);
+ci_uint32 oo_netif_apps_gone(ci_netif* netif);
 
 #else
 #define OO_DO_STACK_DTOR 0
 #endif
+
+#if CI_CFG_UL_INTERRUPT_HELPER || defined(__KERNEL__)
+#define OO_N_EP_ORPHANED_INIT ((ci_uint32)-1)
+#endif
+
+#endif /* __ONLOAD_NETIF_DTOR_H__ */

src/lib/transport/ip/netif_debug.c
@@ -14,6 +14,7 @@
 #include "uk_intf_ver.h"
 #include <onload/version.h>
 #include <onload/sleep.h>
+#include <onload/netif_dtor.h>
 #include <etherfabric/vi.h>
 
 #ifdef NDEBUG
@@ -876,7 +877,6 @@ void ci_netif_dump_to_logger(ci_netif* ni, oo_dump_log_fn_t logger,
   long diff;
   int intf_i;
   int i;
-  ci_uint32 n_ep_orphaned;
 
   logger(log_arg, "%s: stack=%d name=%s",
          __FUNCTION__, NI_ID(ni), ni->state->name);
@@ -905,9 +905,14 @@ void ci_netif_dump_to_logger(ci_netif* ni, oo_dump_log_fn_t logger,
           ? " USE_ALIEN_LADDRS" : ""
       );
 
-  n_ep_orphaned = ns->n_ep_orphaned;
-  if( n_ep_orphaned != 0 )
-    logger(log_arg, "  orphaned sockets %d", n_ep_orphaned);
+#if OO_DO_STACK_DTOR
+  {
+    ci_uint32 n_ep_orphaned;
+    n_ep_orphaned = n_ep_orphaned(ni);
+    if( n_ep_orphaned != 0 )
+      logger(log_arg, "  orphaned sockets %d", n_ep_orphaned);
+  }
+#endif
 
 #ifdef __KERNEL__
   logger(log_arg, "  creation_time=%u (delta=%usecs)", ns->creation_time_sec,

src/lib/transport/ip/netif_dtor.c
@@ -81,13 +81,13 @@ void oo_netif_dtor_pkts(ci_netif* ni)
 }
 
 /* Called when all the user applications have gone.
- * Initialises netif->state->n_ep_orphaned.
- * Returns 1 if this value is non-zero.
+ * Returns the number of orphaned sockets which can't be dropped yet
+ * (TIME-WAIT and so on).
  */
-int/*bool*/ oo_netif_apps_gone(ci_netif* netif)
+ci_uint32 oo_netif_apps_gone(ci_netif* netif)
 {
   unsigned i;
-  int orphaned;
+  ci_uint32 orphaned;
 
   ci_assert(ci_netif_is_locked(netif));
 
@@ -130,7 +130,7 @@ int/*bool*/ oo_netif_apps_gone(ci_netif* netif)
 
     /* All user files are closed; all FINs should be sent.
      * There are some cases when we fail to send FIN to passively-opened
-     * connection: reset such connections. */
+     * connection (see ON-2108): reset such connections. */
     if( w->state & CI_TCP_STATE_TCP_CONN && wo->sock.tx_errno == 0 ) {
       if( OO_SP_IS_NULL(wo->tcp.local_peer) ||
           (~w->sb_aflags & CI_SB_AFLAG_TCP_IN_ACCEPTQ) ) {
@@ -172,13 +172,7 @@ int/*bool*/ oo_netif_apps_gone(ci_netif* netif)
 
   LOG_NC(ci_log("%s: [%u] %d socket(s) closing", __FUNCTION__,
                 NI_ID(netif), orphaned));
-  if( ci_cas32u_fail(&netif->state->n_ep_orphaned, 0, orphaned) ) {
-    LOG_E(ci_log("%s: ERROR: nonzero n_ep_orphaned value: %d when "
-                 "setting it to %d",
-                 __func__, netif->state->n_ep_orphaned, orphaned));
-    ci_assert(0);
-  }
-  return orphaned > 0;
+  return orphaned;
 }
 
 #endif /* OO_DO_STACK_DTOR */

src/lib/transport/ip/netif_init.c
@@ -134,7 +134,6 @@ void ci_netif_state_init(ci_netif* ni, int cpu_khz, const char* name)
   nis->deferred_free_eps_head = CI_ILL_END;
   assert_zero(nis->n_ep_bufs);
   nis->max_ep_bufs = NI_OPTS(ni).max_ep_bufs;
-  nis->n_ep_orphaned = 0;
 
   assert_zero(ni->packets->sets_n);
   ni->packets->sets_max = ni->pkt_sets_max;

src/lib/transport/ip/waitable.c
@@ -117,10 +117,10 @@ static void ci_drop_orphan(ci_netif * ni)
    *    they've all gone away.
    */
   do {
-    ci_uint32 val = ni->state->n_ep_orphaned;
-    if( val == 0 )
+    ci_uint32 val = n_ep_orphaned(ni);
+    if( val == OO_N_EP_ORPHANED_INIT )
       return;
-    if( ci_cas32u_succeed(&ni->state->n_ep_orphaned, val, val - 1) ) {
+    if( ci_cas32u_succeed(&n_ep_orphaned(ni), val, val - 1) ) {
 #ifdef __KERNEL__
       if( val == 1 )
         oo_thr_ref_drop(netif2tcp_helper_resource(ni)->ref, OO_THR_REF_BASE);

commit ecb356a56673794f40b25b94c3c256017f0d8f93

ON-12122: fix stack dtor with wedged lock
Fixes: 7243d3d5c73f ON-12053: fix race between kill_stack() and oo_netif_apps_gone()

src/lib/efthrm/tcp_helper_resource.c
@@ -4963,6 +4963,9 @@ static void
 efab_tcp_helper_rm_free_locked(tcp_helper_resource_t* trs)
 {
   ci_netif* netif;
+#if ! CI_CFG_UL_INTERRUPT_HELPER
+  ci_uint32 n_orphaned = 0;
+#endif
 
   ci_assert(NULL != trs);
   ci_assert(trs->trusted_lock == (OO_TRUSTED_LOCK_LOCKED |
@@ -5061,39 +5064,37 @@ efab_tcp_helper_rm_free_locked(tcp_helper_resource_t* trs)
    * the fds will be released.
    */
 #if CI_CFG_DESTROY_WEDGED
-  if( netif->flags & CI_NETIF_FLAG_WEDGED )
-    goto closeall;
-#endif /*CI_CFG_DESTROY_WEDGED*/
+  if( ! (netif->flags & CI_NETIF_FLAG_WEDGED) )
+#endif
+  {
 
 #if CI_CFG_FD_CACHING
-  ci_tcp_active_cache_drop_cache(netif);
-  ci_tcp_passive_scalable_cache_drop_cache(netif);
+    ci_tcp_active_cache_drop_cache(netif);
+    ci_tcp_passive_scalable_cache_drop_cache(netif);
 #endif
 
-  /* purge list of connections waiting to be closed
-   *   - ones where we couldn't continue in fop_close because
-   *     we didn't have the netif lock
-   */
-  tcp_helper_close_pending_endpoints(trs);
+    /* purge list of connections waiting to be closed
+     *   - ones where we couldn't continue in fop_close because
+     *     we didn't have the netif lock
+     */
+    tcp_helper_close_pending_endpoints(trs);
 
-  {
     /* Do we have any endpoints which can't be dropped yet? */
-    ci_uint32 n_orphaned = 0;
     n_orphaned = oo_netif_apps_gone(netif);
-    if( ci_cas32u_succeed(&netif->n_ep_orphaned, OO_N_EP_ORPHANED_INIT,
-                          n_orphaned) && n_orphaned == 0 ) {
-      /* Who sets n_ep_orphaned to zero, the same code should drop the
-       * associated BASE refcount. */
-      oo_thr_ref_drop(trs->ref, OO_THR_REF_BASE);
-    }
   }
 
-  /* Drop lock so that sockets can proceed towards close. */
-  ci_netif_unlock(&trs->netif);
+  /* Who sets n_ep_orphaned to zero, the same code should drop the
+   * associated BASE refcount. */
+  if( ci_cas32u_succeed(&netif->n_ep_orphaned, OO_N_EP_ORPHANED_INIT,
+                        n_orphaned) && n_orphaned == 0 )
+    oo_thr_ref_drop(trs->ref, OO_THR_REF_BASE);
 
+  /* Drop lock so that sockets can proceed towards close. */
 #if CI_CFG_DESTROY_WEDGED
- closeall:
+  if( ! (netif->flags & CI_NETIF_FLAG_WEDGED) )
 #endif
+    ci_netif_unlock(&trs->netif);
+
 #endif /* CI_CFG_UL_INTERRUPT_HELPER*/
 
   /* Don't need atomics here, because only we are permitted to touch