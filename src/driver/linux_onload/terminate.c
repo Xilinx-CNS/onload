Xilinx-CNS/onload

commit 4401f6864af6fd81a18be0aa4a4ab34d300529b6

ON-12093: purge old compat code around do_coredump()

src/driver/linux_onload/terminate.c
@@ -386,15 +386,9 @@ efab_linux_trampoline_exit_group(int status)
   }
 }
 
-#if defined EFRM_DO_COREDUMP_BINFMTS_SIGNR || \
-    defined EFRM_DO_COREDUMP_COREDUMP_SIGNR
-/* RHEL6, etc. */
-#define EFRM_DO_COREDUMP_SIGNR
-static void (*efab_coredump)(long signr, int exit_core, struct pt_regs*);
-#else
-/* pt_regs parameter was only present for a month in 2012, but it's easy to
- * keep */
-static void (*efab_coredump)(const siginfo_t *siginfo, struct pt_regs*);
+#if defined(CONFIG_COREDUMP) && defined(ERFM_HAVE_NEW_KALLSYMS)
+#define OO_DO_COREDUMP
+static void (*efab_do_coredump)(const siginfo_t *siginfo);
 #endif
 
 
@@ -418,20 +412,18 @@ efab_signal_die(ci_private_t *priv_unused, void *arg)
 
     /* lock all the stacks */
     efab_terminate_lock_all_stacks(stacks, stacks_num);
+#ifdef OO_DO_COREDUMP
     /* kill all threads while they do not have netif locks (we have them!),
      * dump the core with all the threads. */
-    if( sig_kernel_coredump(sig) && efab_coredump != NULL ) {
+    if( sig_kernel_coredump(sig) && efab_do_coredump != NULL ) {
       siginfo_t siginfo;
       struct pt_regs regs;
       memset(&regs, 0, sizeof(regs));
       siginfo.si_signo = sig;
-#ifdef EFRM_DO_COREDUMP_SIGNR
-      efab_coredump(siginfo.si_signo, sig, &regs);
-#else
-      efab_coredump(&siginfo, &regs);
-#endif
+      efab_do_coredump(&siginfo);
     }
     else
+#endif
       efab_exit_group(&status);
     /* now when everybody is dead we can release netifs */
     efab_terminate_unlock_all_stacks(stacks, stacks_num);
@@ -453,18 +445,20 @@ void efab_linux_termination_ctor(void)
 {
 #ifdef ERFM_HAVE_NEW_KALLSYMS
   efab_my_zap_other_threads = efrm_find_ksym("zap_other_threads");
-  efab_coredump = efrm_find_ksym("do_coredump");
-  TERM_DEBUG("Find zap_other_threads,do_coredump via kallsyms at %p,%p",
-             efab_my_zap_other_threads, efab_coredump);
-
+  TERM_DEBUG("Find zap_other_threads via kallsyms at %px",
+             efab_my_zap_other_threads);
   if( efab_my_zap_other_threads == NULL )
 #endif
     efab_my_zap_other_threads = efab_zap_other_threads;
 
-  if( efab_coredump == NULL ) {
+#ifdef OO_DO_COREDUMP
+  efab_do_coredump = efrm_find_ksym("do_coredump");
+  TERM_DEBUG("Find do_coredump via kallsyms at %px", do_coredump);
+  if( efab_do_coredump == NULL )
+#endif
     ci_log("WARNING: failed to find do_coredump() symbol.  Use "
-           "module parameter safe_signals_and_exit=0 if you want SIGSEGV "
-           "to produce core dump.");
-  }
+           "module parameter safe_signals_and_exit=0 or add SIGSEGV "
+           "to EF_SIGNALS_NOPOSTPONE environment variable "
+           "if you want SIGSEGV to produce core dump.");
 }
 

src/driver/linux_affinity/kernel_compat.sh
@@ -93,8 +93,6 @@ EFRM_HAVE_WRITE_ONCE                    symbol WRITE_ONCE include/linux/compiler
 EFRM_HAVE_INIT_LIST_HEAD_RCU            symbol INIT_LIST_HEAD_RCU include/linux/rculist.h
 EFRM_HAVE_S_MIN_MAX                     symbol S32_MIN include/linux/kernel.h include/linux/limits.h
 
-EFRM_DO_COREDUMP_BINFMTS_SIGNR          symtype	do_coredump	include/linux/binfmts.h	void(long, int, struct pt_regs*)
-EFRM_DO_COREDUMP_COREDUMP_SIGNR         symtype	do_coredump	include/linux/coredump.h	void(long, int, struct pt_regs*)
 EFRM_RTNL_LINK_OPS_HAS_GET_LINK_NET	member	struct_rtnl_link_ops	get_link_net	include/net/rtnetlink.h
 
 EFRM_ACCESS_OK_HAS_2_ARGS    custom

commit 783cb13c5d05c997199e7fa636634e0818a0e676

ON-12093: build fix with TERM_DEBUG
Fixes: 4401f6864af ON-12093: purge old compat code around do_coredump()

src/driver/linux_onload/terminate.c
@@ -459,7 +459,7 @@ void efab_linux_termination_ctor(void)
 
 #ifdef OO_DO_COREDUMP
   efab_do_coredump = efrm_find_ksym("do_coredump");
-  TERM_DEBUG("Find do_coredump via kallsyms at %px", do_coredump);
+  TERM_DEBUG("Find do_coredump via kallsyms at %px", efab_do_coredump);
   if( efab_do_coredump == NULL )
 #endif
     ci_log("WARNING: failed to find do_coredump() symbol.  Use "