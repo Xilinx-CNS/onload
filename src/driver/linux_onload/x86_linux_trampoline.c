Xilinx-CNS/onload

commit bd8c15c8a62421b7d11cce17c6c4ae0fb872bfbb

ON-12158: ON-12093: call oo_entry_SYSCALL_64 at ctor time
oo_entry_SYSCALL_64 can sleep and should not be called at random point.
With this patch we call it once from ctor function.

Fixes: 939283736a3 ON-12093: cleanup and comments for oo_entry_SYSCALL_64()

src/driver/linux_onload/x86_linux_trampoline.c
@@ -207,6 +207,9 @@
  */
 static void **syscall_table = 0;
 
+/* The address of entry_SYSCALL_64() */
+static void *oo_entry_SYSCALL_64_addr = NULL;
+
 #ifdef CONFIG_COMPAT
 
 /* The address of the 32-bit compatibility system call table.
@@ -234,14 +237,9 @@ static SYSCALL_PTR_DEF(saved_sys_exit_group32, (int));
 
 atomic_t efab_syscall_used;
 
-
 static void* oo_entry_SYSCALL_64(void)
 {
-  static void *oo_entry_SYSCALL_64_addr = NULL;
   unsigned long result = 0;
-
-  if( oo_entry_SYSCALL_64_addr != NULL )
-    return oo_entry_SYSCALL_64_addr;
 
   /* linux<4.2:
    *   MSR_LSTAR points to system_call(); we are returning this address.
@@ -256,14 +254,13 @@ static void* oo_entry_SYSCALL_64(void)
    *   MSR_LSTAR points to entry_SYSCALL_64().
    */
 #ifdef ERFM_HAVE_NEW_KALLSYMS
-  oo_entry_SYSCALL_64_addr = efrm_find_ksym("entry_SYSCALL_64");
-  if( oo_entry_SYSCALL_64_addr != NULL )
-    return oo_entry_SYSCALL_64_addr;
+  void *ret = efrm_find_ksym("entry_SYSCALL_64");
+  if( ret != NULL )
+    return ret;
 #endif
 
   rdmsrl(MSR_LSTAR, result);
-  oo_entry_SYSCALL_64_addr = (void*)result;
-  return oo_entry_SYSCALL_64_addr;
+  return (void*)result;
 }
 
 static void **find_syscall_table(void)
@@ -285,8 +282,7 @@ static void **find_syscall_table(void)
   /* If kallsyms lookup failed, fall back to looking at some assembly
    * code that we know references the syscall table.
    */
-  p = oo_entry_SYSCALL_64();
-  if( p == NULL )
+  if( oo_entry_SYSCALL_64_addr == NULL )
     return NULL;
 
   TRAMP_DEBUG("entry_SYSCALL_64=%px", p);
@@ -724,7 +720,7 @@ ci_inline unsigned long *get_oldrsp_addr(void)
     unsigned long kernel_stack_p = (unsigned long) percpu_p(OO_KERNEL_STACK);
     unsigned char *p_end;
 
-    p = oo_entry_SYSCALL_64();
+    p = oo_entry_SYSCALL_64_addr;
 #ifndef NDEBUG
     ptr = p;
 #define OOPS(msg) { \
@@ -1316,6 +1312,7 @@ patch_syscall_table (void **table, unsigned entry, void *func,
  */
 int efab_linux_trampoline_ctor(int no_sct)
 {
+  oo_entry_SYSCALL_64_addr = oo_entry_SYSCALL_64();
   syscall_table = find_syscall_table();
 
   atomic_set(&efab_syscall_used, 0);

commit 44519db4359f9455fc2e58a9afb8f0473b6ab351

ON-12093: fix trampoline for linux-5.7 - tested
Repair linux-5.7 which was broken by the fix for ON-12158.

Fixes: bd8c15c ON-12158: ON-12093: call oo_entry_SYSCALL_64 at ctor time
Fixes: 9392837 ON-12093: cleanup and comments for oo_entry_SYSCALL_64()

src/driver/linux_onload/x86_linux_trampoline.c
@@ -284,6 +284,7 @@ static void **find_syscall_table(void)
    */
   if( oo_entry_SYSCALL_64_addr == NULL )
     return NULL;
+  p = oo_entry_SYSCALL_64_addr;
 
   TRAMP_DEBUG("entry_SYSCALL_64=%px", p);
   /* linux>=4.17 has following layout: