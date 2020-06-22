57bc4e4aaf9c5acf042cd0442db43e3e45094fef
Alexandra Kossovsky <Alexandra.Kossovsky@oktetlabs.ru>
2020-06-22 11:20:58 +0300

    ON-12125: move efab_ipp_get_locked_thr_from_tcp_handle content into caller

--- src/lib/efthrm/ip_protocols.c
+++ src/lib/efthrm/ip_protocols.c
@@ -522,54 +522,6 @@ efab_ipp_icmp_qpkt(tcp_helper_resource_t* thr,
 }
 
 
-/*--------------------------------------------------------------------
- *!
- * This function is intended to safely decode a TCP helper resource
- * handle that we passed to the NET driver back into a locked netif
- *
- * Given a TCP helper resource handle, this function attempts to
- * return a locked netif. If it succeeds its up to the callee to drop
- * the netif lock by calling efab_tcp_helper_netif_unlock, and drop a
- * reference by calling oo_thr_ref_drop().  This function can fail
- * because the handle is no longer valid OR its not possible to get
- * the netif lock at this time
- *
- * \param nic           nic object the handle relates to
- * \param tcp_id        TCP helper resource id
- *
- * \returns pointer to locked TCP helper resource or NULL
- *
- *--------------------------------------------------------------------*/
-
-static tcp_helper_resource_t *
-efab_ipp_get_locked_thr_from_tcp_handle(unsigned tcp_id)
-{
-  tcp_helper_resource_t * thr = NULL;
-  int rc;
-
-  /* ask resource manager to decode to a resource */
-  rc = efab_thr_table_lookup(NULL, NULL, tcp_id,
-                             EFAB_THR_TABLE_LOOKUP_NO_CHECK_USER,
-                             OO_THR_REF_BASE, &thr);
-  if (rc < 0) {
-    OO_DEBUG_IPP( ci_log("%s: Invalid TCP helper resource handle %u", 
-                         __FUNCTION__, tcp_id) );
-  }
-  else {
-    /* so we have found the resource in the table and have incremented
-    ** its reference count  - now lets try and lock the associated netif 
-    */
-    if ( !efab_tcp_helper_netif_try_lock(thr, 1) ) {
-      OO_DEBUG_IPP( ci_log("%s: Failed to lock TCP helper", __FUNCTION__) );
-      oo_thr_ref_drop(thr->ref, OO_THR_REF_BASE);
-      thr = NULL;
-    }
-  }
-  return thr;
-}
-
-
-
 /* NOTE: the current implementation discards data when it
  * cannot get a lock.  As the mechanism is for protocols that
  * do not guarantee data delivery this is considered acceptible.
@@ -577,15 +529,29 @@ efab_ipp_get_locked_thr_from_tcp_handle(unsigned tcp_id)
 int efab_handle_ipp_pkt_task(int thr_id, efab_ipp_addr* addr)
 {
   tcp_helper_resource_t* thr;
+  int rc;
+  ci_sock_cmn* s;
 
-  if( (thr = efab_ipp_get_locked_thr_from_tcp_handle(thr_id))) {
-    ci_sock_cmn* s;
-
-    s = efab_ipp_icmp_for_thr( thr, addr );
-    if( s )  efab_ipp_icmp_qpkt( thr, s, addr );
-    efab_tcp_helper_netif_unlock( thr, 1 );
-    oo_thr_ref_drop(thr->ref, OO_THR_REF_BASE);
+  /* ask resource manager to decode to a resource */
+  rc = efab_thr_table_lookup(NULL, NULL, thr_id,
+                             EFAB_THR_TABLE_LOOKUP_NO_CHECK_USER,
+                             OO_THR_REF_BASE, &thr);
+  if (rc < 0) {
+    OO_DEBUG_IPP( ci_log("%s: Invalid TCP helper resource handle %u", 
+                         __FUNCTION__, thr_id) );
+    return -ENOENT;
   }
+
+  if ( !efab_tcp_helper_netif_try_lock(thr, 1) ) {
+    OO_DEBUG_IPP( ci_log("%s: Failed to lock TCP helper", __FUNCTION__) );
+    oo_thr_ref_drop(thr->ref, OO_THR_REF_BASE);
+    return -EAGAIN;
+  }
+
+  s = efab_ipp_icmp_for_thr( thr, addr );
+  if( s )  efab_ipp_icmp_qpkt( thr, s, addr );
+  efab_tcp_helper_netif_unlock( thr, 1 );
+  oo_thr_ref_drop(thr->ref, OO_THR_REF_BASE);
 
   return 0;
 }
