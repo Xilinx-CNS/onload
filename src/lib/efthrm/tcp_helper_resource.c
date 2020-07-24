Xilinx-CNS/onload

commit ec5b03a79c0bd98e4a072bf0510196b6f72993c2

ON-12125: always handle ICMP
Use efab_tcp_helper_netif_lock_or_set_flags() to handle incoming ICMP.

src/lib/efthrm/tcp_helper_resource.c
@@ -300,6 +300,10 @@ oo_trusted_lock_drop(tcp_helper_resource_t* trs, int in_dl_context)
   if( l & OO_TRUSTED_LOCK_XDP_CHANGE )
     sl_flags |= CI_EPLOCK_NETIF_XDP_CHANGE;
 #endif
+#if CI_CFG_HANDLE_ICMP
+  if( l & OO_TRUSTED_LOCK_HANDLE_ICMP )
+    sl_flags |= CI_EPLOCK_NETIF_HANDLE_ICMP;
+#endif
   ci_assert(sl_flags != 0);
   if( ci_cas32_succeed(&trs->trusted_lock, l, OO_TRUSTED_LOCK_LOCKED) &&
       ef_eplock_trylock_and_set_flags(&trs->netif.state->lock, sl_flags) ) {
@@ -3861,6 +3865,9 @@ int tcp_helper_rm_alloc(ci_resource_onload_alloc_t* alloc,
   ci_sllist_init(&rs->ep_tobe_closed);
   ci_irqlock_ctor(&rs->lock);
   init_completion(&rs->complete);
+#if CI_CFG_HANDLE_ICMP
+  rs->icmp_msg = NULL;
+#endif
 
 #ifdef EFRM_DO_NAMESPACES
   /* Initialise namespaces */
@@ -7354,6 +7361,12 @@ efab_tcp_helper_netif_lock_callback(eplock_helper_t* epl, ci_uint64 lock_val,
      * it can cause us to take an early exit from the function. */
     ci_assert_nflags(flags_set & ~all_after_unlock_flags,
                      ~CI_EPLOCK_NETIF_CLOSE_ENDPOINT);
+
+#if CI_CFG_HANDLE_ICMP
+    if( flags_set & CI_EPLOCK_NETIF_HANDLE_ICMP ) {
+      oo_icmp_handle(thr);
+    }
+#endif
 
 #if ! CI_CFG_UL_INTERRUPT_HELPER
     if( flags_set & CI_EPLOCK_NETIF_CLOSE_ENDPOINT ) {

src/include/ci/internal/ip_shared_types.h
@@ -923,9 +923,11 @@ typedef struct {
 #define CI_EPLOCK_NETIF_HAS_DEFERRED_PKTS  0x0004000000000000ULL
   /* need to handle postponed XDP prog update */
 # define CI_EPLOCK_NETIF_XDP_CHANGE        0x0002000000000000ULL
+  /* Have ICMP message to handle */
+#define CI_EPLOCK_NETIF_HANDLE_ICMP        0x0001000000000000ULL
 
   /* mask for the above flags that must be handled before dropping lock */
-# define CI_EPLOCK_NETIF_UNLOCK_FLAGS      0xff3a000000000000ULL
+# define CI_EPLOCK_NETIF_UNLOCK_FLAGS      0xff3b000000000000ULL
 
   /* these flags can be handled in UL */
 #define CI_EPLOCK_NETIF_UL_COMMON_MASK \

src/include/onload/linux_ip_protocols.h
@@ -34,6 +34,9 @@
 extern int 
 efab_handle_ipp_pkt_task(int thr_id, efab_ipp_addr* addr, ci_icmp_hdr* icmp);
 
+/* Handle all ICMP messages queued for this stack.
+ * Called with stack lock held. */
+void oo_icmp_handle(tcp_helper_resource_t* thr);
 #endif
 
 #endif

src/include/onload/tcp_helper.h
@@ -194,7 +194,7 @@ typedef struct tcp_helper_resource_s {
 #define OO_TRUSTED_LOCK_CLOSE_ENDPOINT    0x8
 #define OO_TRUSTED_LOCK_OS_READY          0x10
 #define OO_TRUSTED_LOCK_NEED_PRIME        0x20
-/* 0x40 is available for reuse; was OO_TRUSTED_LOCK_DONT_BLOCK_SHARED. */
+#define OO_TRUSTED_LOCK_HANDLE_ICMP       0x40
 #define OO_TRUSTED_LOCK_SWF_UPDATE        0x80
 #define OO_TRUSTED_LOCK_PURGE_TXQS        0x100
 #if CI_CFG_WANT_BPF_NATIVE && CI_HAVE_BPF_NATIVE
@@ -300,6 +300,7 @@ typedef struct tcp_helper_resource_s {
    *    - wakeup_list
    *    - intfs_to_reset 
    *    - intfs_to_xdp_update
+   *    - icmp_msg
    */
   ci_irqlock_t          lock;
 
@@ -382,6 +383,8 @@ typedef struct tcp_helper_resource_s {
   struct mutex usermem_mutex;
   uint64_t usermem_prev_id;
   struct tcp_helper_usermem* usermem;
+
+  struct oo_icmp_msg* icmp_msg;
 } tcp_helper_resource_t;
 
 

src/lib/efthrm/ip_protocols.c
@@ -521,6 +521,45 @@ efab_ipp_icmp_qpkt(tcp_helper_resource_t* thr, ci_sock_cmn* s,
 #endif
 }
 
+struct oo_icmp_msg {
+  struct oo_icmp_msg* next;
+  efab_ipp_addr addr;
+  ci_icmp_too_big_t icmp;
+};
+
+void oo_icmp_handle(tcp_helper_resource_t* thr)
+{
+  ci_irqlock_state_t lock_flags;
+  ci_sock_cmn* s;
+  struct oo_icmp_msg* msg;
+  struct oo_icmp_msg* p;
+  struct oo_icmp_msg* n;
+
+  ci_irqlock_lock(&thr->lock, &lock_flags);
+  msg = thr->icmp_msg;
+  thr->icmp_msg = NULL;
+  ci_irqlock_unlock(&thr->lock, &lock_flags);
+  if( msg == NULL )
+    return;
+
+  /* This list is ordered as "last first".  Let's reorder the messages
+   * back. */
+  n = NULL;
+  while( msg->next != NULL ) {
+    p = msg->next;
+    msg->next = n;
+    n = msg;
+    msg = p;
+  }
+
+  /* And now handle them all */
+ do {
+    s = efab_ipp_icmp_for_thr(thr, &msg->addr);
+    if( s )  efab_ipp_icmp_qpkt(thr, s, &msg->addr, &msg->icmp.hdr);
+    n = msg->next;
+    kfree(msg);
+  } while( (msg = n) != NULL );
+}
 
 /* NOTE: the current implementation discards data when it
  * cannot get a lock.  As the mechanism is for protocols that
@@ -529,9 +568,10 @@ efab_ipp_icmp_qpkt(tcp_helper_resource_t* thr, ci_sock_cmn* s,
 int efab_handle_ipp_pkt_task(int thr_id, efab_ipp_addr* addr,
                              ci_icmp_hdr* icmp)
 {
+  ci_irqlock_state_t lock_flags;
   tcp_helper_resource_t* thr;
   int rc;
-  ci_sock_cmn* s;
+  struct oo_icmp_msg* msg;
 
   /* ask resource manager to decode to a resource */
   rc = efab_thr_table_lookup(NULL, NULL, thr_id,
@@ -543,15 +583,28 @@ int efab_handle_ipp_pkt_task(int thr_id, efab_ipp_addr* addr,
     return -ENOENT;
   }
 
-  if ( !efab_tcp_helper_netif_try_lock(thr, 1) ) {
-    OO_DEBUG_IPP( ci_log("%s: Failed to lock TCP helper", __FUNCTION__) );
+  msg = kmalloc(sizeof(struct oo_icmp_msg), GFP_ATOMIC);
+  if( msg == NULL ) {
     oo_thr_ref_drop(thr->ref, OO_THR_REF_BASE);
-    return -EAGAIN;
+    return -ENOMEM;
   }
 
-  s = efab_ipp_icmp_for_thr( thr, addr );
-  if( s )  efab_ipp_icmp_qpkt( thr, s, addr, icmp );
-  efab_tcp_helper_netif_unlock( thr, 1 );
+  memcpy(&msg->addr, addr, sizeof(msg->addr));
+  memcpy(&msg->icmp, icmp, sizeof(msg->icmp));
+
+  ci_irqlock_lock(&thr->lock, &lock_flags);
+  msg->next = thr->icmp_msg;
+  thr->icmp_msg = msg;
+  ci_irqlock_unlock(&thr->lock, &lock_flags);
+
+  if( efab_tcp_helper_netif_lock_or_set_flags(thr,
+                                              OO_TRUSTED_LOCK_HANDLE_ICMP,
+                                              CI_EPLOCK_NETIF_HANDLE_ICMP,
+                                              1) ) {
+    oo_icmp_handle(thr);
+    efab_tcp_helper_netif_unlock(thr, 1);
+  }
+
   oo_thr_ref_drop(thr->ref, OO_THR_REF_BASE);
 
   return 0;

commit 557fe575c13f7e70f713a53ce035cbe0589d31dc

ON-12344: fix regression in ICMP handling
Fixes: ec5b03a79c0b ON-12125: always handle ICMP

src/lib/efthrm/tcp_helper_resource.c
@@ -7357,17 +7357,18 @@ efab_tcp_helper_netif_lock_callback(eplock_helper_t* epl, ci_uint64 lock_val,
     }
 #endif
 
-    /* CI_EPLOCK_NETIF_CLOSE_ENDPOINT must be the last flag handled, because
-     * it can cause us to take an early exit from the function. */
-    ci_assert_nflags(flags_set & ~all_after_unlock_flags,
-                     ~CI_EPLOCK_NETIF_CLOSE_ENDPOINT);
-
 #if CI_CFG_HANDLE_ICMP
     if( flags_set & CI_EPLOCK_NETIF_HANDLE_ICMP ) {
       CITP_STATS_NETIF(++ni->state->stats.rx_icmp_hooked);
       oo_icmp_handle(thr);
+      flags_set &= ~CI_EPLOCK_NETIF_HANDLE_ICMP;
     }
 #endif
+
+    /* CI_EPLOCK_NETIF_CLOSE_ENDPOINT must be the last flag handled, because
+     * it can cause us to take an early exit from the function. */
+    ci_assert_nflags(flags_set & ~all_after_unlock_flags,
+                     ~CI_EPLOCK_NETIF_CLOSE_ENDPOINT);
 
 #if ! CI_CFG_UL_INTERRUPT_HELPER
     if( flags_set & CI_EPLOCK_NETIF_CLOSE_ENDPOINT ) {