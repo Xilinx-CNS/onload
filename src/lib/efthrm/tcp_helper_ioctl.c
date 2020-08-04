Xilinx-CNS/onload

commit a03780e4da1d29177adc64a7758baf155a956990

ON-11940: ulhelper: use ringbuffer for closed_eps
It allows to avoid ioctls when handling stack lock hooks.

src/lib/efthrm/tcp_helper_ioctl.c
@@ -1615,7 +1615,6 @@ oo_operations_table_t oo_operations[] = {
 
 #if CI_CFG_UL_INTERRUPT_HELPER
   op(OO_IOC_WAIT_FOR_INTERRUPT, oo_wait_for_interrupt),
-  op(OO_IOC_GET_CLOSING_EP,     oo_get_closing_ep),
   op(OO_IOC_WAKEUP_WAITERS,     oo_wakeup_waiters),
   op(OO_IOC_SWF_UPDATE,         oo_swf_update_rsop),
 #endif

src/include/ci/internal/ip_shared_types.h
@@ -1057,6 +1057,9 @@ struct ci_netif_state_s {
 #define OO_ACTION_SWF_UPDATE 0x0002
 #endif
 
+#define OO_CLOSED_EPS_RING_SIZE 512
+  struct oo_ringbuffer_state closed_eps;
+
   ci_uint32  flags;
 # define CI_NETIF_FLAG_DEBUG              0x1 /* driver is debug build   */
 # define CI_NETIF_FLAG_ONLOAD_UNSUPPORTED 0x2 /* OOL unsupported on this h/w */
@@ -1185,6 +1188,9 @@ struct ci_netif_state_s {
   CI_ULCONST ci_uint32  table_ext_ofs;   /**< offset of slow s/w filter state */
 #if CI_CFG_IPV6
   CI_ULCONST ci_uint32  ip6_table_ofs;   /**< offset of IPv6 s/w filter table */
+#endif
+#if CI_CFG_UL_INTERRUPT_HELPER
+  CI_ULCONST ci_uint32  closed_eps_ofs; /**< offset of colsed eps ringbuffer */
 #endif
   CI_ULCONST ci_uint32  seq_table_ofs;   /**< offset of seq no table */
   CI_ULCONST ci_uint32  deferred_pkts_ofs; /**< offset of deferred pkts array */

src/include/ci/internal/ip_types.h
@@ -189,6 +189,10 @@ struct ci_netif_s {
 #endif
 #endif
 
+#if CI_CFG_UL_INTERRUPT_HELPER
+  struct oo_ringbuffer closed_eps;
+#endif
+
   /* This is pointer to the shared state of packet sets */
   oo_pktbuf_manager*    packets;
   /* And this is non-shared array for UL- or kernel- specific data

src/include/onload/ioctl.h
@@ -232,8 +232,6 @@ enum {
   /* Wait for an interrupt */
   OO_OP_WAIT_FOR_INTERRUPT,
 #define OO_IOC_WAIT_FOR_INTERRUPT OO_IOC_RW(WAIT_FOR_INTERRUPT, struct oo_ulh_waiter)
-  OO_OP_GET_CLOSING_EP,
-#define OO_IOC_GET_CLOSING_EP     OO_IOC_R(GET_CLOSING_EP, oo_sp)
   OO_OP_WAKEUP_WAITERS,
 #define OO_IOC_WAKEUP_WAITERS     OO_IOC_W(WAKEUP_WAITERS, struct oo_wakeup_eps)
   OO_OP_SWF_UPDATE,

src/include/onload/tcp_helper.h
@@ -290,7 +290,7 @@ typedef struct tcp_helper_resource_s {
 # define OO_THR_AFLAG_DONT_BLOCK_SHARED   0x10
 
   /*! Spinlock.  Protects:
-   *    - ep_tobe_closed
+   *    - ep_tobe_closed / closed_eps
    *    - non_atomic_list
    *    - wakeup_list
    *    - intfs_to_reset 
@@ -424,8 +424,10 @@ struct tcp_helper_endpoint_s {
    */
   struct file* os_port_keeper;
 
+#if ! CI_CFG_UL_INTERRUPT_HELPER
   /*! link so we can be in the list of endpoints to be closed in the future */
   ci_sllink tobe_closed;
+#endif
 
   /* Link field when queued for non-atomic work. */
   ci_sllink non_atomic_link;

src/lib/efthrm/tcp_helper_resource.c
@@ -2083,6 +2083,11 @@ allocate_netif_resources(ci_resource_onload_alloc_t* alloc,
   sz += ip6_filter_table_size;
 #endif
 
+#if CI_CFG_UL_INTERRUPT_HELPER
+  sz = CI_ROUND_UP(sz, __alignof__(oo_sp));
+  sz += sizeof(oo_sp) * OO_CLOSED_EPS_RING_SIZE;
+#endif
+
 #if CI_CFG_PIO
   /* Allocate shmbuf for pio regions.  We haven't tried to allocate
    * PIOs yet and we don't know how many ef10s we have.  So just
@@ -2190,7 +2195,17 @@ allocate_netif_resources(ci_resource_onload_alloc_t* alloc,
 #if CI_CFG_IPV6
   ns_ofs = CI_ROUND_UP(ns_ofs, __alignof__(ci_ip6_netif_filter_table));
   ns->ip6_table_ofs = ns_ofs;
+  ns_ofs += ip6_filter_table_size;
 #endif
+
+#if CI_CFG_UL_INTERRUPT_HELPER
+  ns_ofs = CI_ROUND_UP(ns_ofs, __alignof__(oo_sp));
+  ns->closed_eps_ofs = ns_ofs;
+  ns_ofs += sizeof(oo_sp) * OO_CLOSED_EPS_RING_SIZE;
+#endif
+
+  /* The last addition to ns_ofs is not really used */
+  (void)ns_ofs;
 
   ns->vi_state_bytes = vi_state_bytes;
 
@@ -2207,6 +2222,13 @@ allocate_netif_resources(ci_resource_onload_alloc_t* alloc,
 
 #if CI_CFG_IPV6
   ni->ip6_filter_table = (void*) ((char*) ns + ns->ip6_table_ofs);
+#endif
+
+#if CI_CFG_UL_INTERRUPT_HELPER
+  oo_ringbuffer_state_init(&ns->closed_eps, OO_CLOSED_EPS_RING_SIZE,
+                           sizeof(oo_sp));
+  oo_ringbuffer_init(&ni->closed_eps, &ns->closed_eps,
+                     (void*)((char*) ns + ns->closed_eps_ofs));
 #endif
 
   ni->packets->sets_max = ni->pkt_sets_max;
@@ -3860,8 +3882,8 @@ int tcp_helper_rm_alloc(ci_resource_onload_alloc_t* alloc,
   INIT_WORK(&rs->work_item_dtor, tcp_helper_destroy_work);
   INIT_WORK(&rs->non_atomic_work, tcp_helper_do_non_atomic);
   ci_sllist_init(&rs->non_atomic_list);
-#endif
   ci_sllist_init(&rs->ep_tobe_closed);
+#endif
   ci_irqlock_ctor(&rs->lock);
   init_completion(&rs->complete);
 #if CI_CFG_HANDLE_ICMP
@@ -4899,36 +4921,6 @@ tcp_helper_close_pending_endpoints(tcp_helper_resource_t* trs)
   }
 }
 #else
-
-int oo_get_closing_ep(ci_private_t* priv, void* arg)
-{
-  oo_sp* p_id = arg;
-  ci_irqlock_state_t lock_flags;
-  tcp_helper_resource_t* trs = priv->thr;
-  tcp_helper_endpoint_t* ep;
-  ci_sllink* link;
-
-  ci_assert(ci_netif_is_locked(&trs->netif));
-
-  if( ci_sllist_is_empty(&trs->ep_tobe_closed) ) {
-    *p_id = OO_SP_NULL;
-    return 0;
-  }
-  ci_irqlock_lock(&trs->lock, &lock_flags);
-  if( ci_sllist_is_empty(&trs->ep_tobe_closed) ) {
-    ci_irqlock_unlock(&trs->lock, &lock_flags);
-    *p_id = OO_SP_NULL;
-    return 0;
-  }
-  link = ci_sllist_pop(&trs->ep_tobe_closed);
-  ci_irqlock_unlock(&trs->lock, &lock_flags);
-
-  ep = CI_CONTAINER(tcp_helper_endpoint_t, tobe_closed , link);
-  tcp_helper_close_cleanup_ep(trs, ep);
-  *p_id = ep->id;
-
-  return 0;
-}
 
 int oo_wakeup_waiters(ci_private_t* priv, void* arg)
 {
@@ -6838,6 +6830,7 @@ efab_tcp_helper_close_endpoint(tcp_helper_resource_t* trs, oo_sp ep_id,
     * the application exits i.e. crashes (even if its holding the netif lock)
     */
   ci_irqlock_lock(&trs->lock, &lock_flags);
+#if ! CI_CFG_UL_INTERRUPT_HELPER
   if( ! ci_sllink_busy(&tep_p->tobe_closed) )
     ci_sllist_push(&trs->ep_tobe_closed, &tep_p->tobe_closed);
   else {
@@ -6846,6 +6839,10 @@ efab_tcp_helper_close_endpoint(tcp_helper_resource_t* trs, oo_sp ep_id,
            trs->id, OO_SP_FMT(ep_id));
     return;
   }
+#else
+  tcp_helper_close_cleanup_ep(trs, tep_p);
+  oo_ringbuffer_write(&trs->netif.closed_eps, &tep_p->id);
+#endif
   ci_irqlock_unlock(&trs->lock, &lock_flags);
 
 #if ! CI_CFG_UL_INTERRUPT_HELPER

src/lib/transport/ip/netif.c
@@ -1946,19 +1946,21 @@ void ci_netif_handle_actions(ci_netif* ni)
   }
 }
 
+static void close_cb(void* arg, void* data)
+{
+  ci_netif* ni = arg;
+  oo_sp* id_p = data;
+
+  ci_assert(ci_netif_is_locked(ni));
+  ci_assert(IS_VALID_SOCK_P(ni, *id_p));
+
+  citp_waitable_all_fds_gone(ni, *id_p);
+}
+
 /* Ask kernel for any sockets to be closed and really close them */
 void ci_netif_close_pending(ci_netif* ni)
 {
-  oo_sp id;
-  do {
-    int saved_errno = errno;
-    oo_resource_op(ci_netif_get_driver_handle(ni), OO_IOC_GET_CLOSING_EP,
-                   &id);
-    errno = saved_errno;
-    if( OO_SP_IS_NULL(id) )
-      break;
-    citp_waitable_all_fds_gone(ni, id);
-  } while(1);
+  oo_ringbuffer_iterate(&ni->closed_eps, close_cb, ni);
 }
 #endif
 /*! \cidoxg_end */

src/lib/transport/ip/netif_init.c
@@ -1750,6 +1750,11 @@ static void netif_tcp_helper_build2(ci_netif* ni)
 #endif
   ni->packets = (oo_pktbuf_manager*) ((char*) ni->state + ni->state->buf_ofs);
   ni->dma_addrs = (ef_addr*) ((char*) ni->state + ni->state->dma_ofs);
+
+#if CI_CFG_UL_INTERRUPT_HELPER
+  oo_ringbuffer_init(&ni->closed_eps, &ni->state->closed_eps, "closed_eps",
+                     (void*)((char*) ni->state + ni->state->closed_eps_ofs));
+#endif
 }
 
 

commit 0b788d45c880e93bdcaa461217eccd064f0c1364

ON-12427: fix pipe closing
Do not call efab_tcp_helper_close_endpoint() directly when it can be called
via fput().  The new code is simpler, and it does not assume synchronous
closure of the shared and kernel-only structure.

Fixes: a03780e4da1 ON-11940: ulhelper: use ringbuffer for closed_eps

src/lib/efthrm/tcp_helper_ioctl.c
@@ -600,8 +600,8 @@ efab_tcp_helper_pipe_attach(ci_private_t* priv, void *arg)
   if( rc < 0 ) {
     LOG_E(ci_log("%s: ERROR: failed to bind writer [%d:%d] to fd",
                  __func__, trs->id, ep->id));
+    tcp_helper_endpoint_set_aflags(ep, OO_THR_EP_AFLAG_PEER_CLOSED);
     efab_linux_sys_close(op->rfd);
-    efab_tcp_helper_close_endpoint(trs, ep->id, 0);
     return rc;
   }
   op->wfd = rc;