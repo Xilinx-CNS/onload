Xilinx-CNS/onload

commit a470774e111e27922f86403e906fda4df9f9932f

ON-12667: use timeout when locking stacks from on_exit hook
Add timeout value to efab_eplock_lock_wait() function and
OO_IOC_EPLOCK_LOCK_WAIT() ioctl.  Use it from oo_exit_hook()
to avoid hang during exit.

src/lib/transport/common/netif_init.c
@@ -743,8 +743,18 @@ void oo_exit_hook(void)
    * (see oo_fop_stack_flush_unlock()) and unlock the stack from here.
    */
   CI_DLLIST_FOR_EACH2( ci_netif, ni, link, &citp_active_netifs ) {
-    ci_netif_lock(ni);
-    ni->state->exiting_pid = pid;
+    int rc = 0;
+    /* It's a copy of ci_netif_lock() without spinning and with timeout */
+    if( ! ci_netif_trylock(ni) ) {
+      ci_int32 op = 1000; /* 1sec */
+      rc = oo_resource_op(ci_netif_get_driver_handle(ni),
+                          OO_IOC_EPLOCK_LOCK_WAIT, &op);
+    }
+    if( rc == 0 )
+      ni->state->exiting_pid = pid;
+    else
+      NI_LOG(ni, RESOURCE_WARNINGS, "Failed to lock the Onload stack "
+             "for gracious application exit");
   }
 
   CITP_FDTABLE_UNLOCK();

src/include/onload/eplock_resource.h
@@ -58,7 +58,7 @@ extern int efab_eplock_wake_and_do(ci_netif *ni, ci_uint64 l);
 #endif
 
 /*! Comment? */
-extern int efab_eplock_lock_wait(ci_netif* ni, int maybe_wedged);
+extern int efab_eplock_lock_wait(ci_netif* ni, int maybe_wedged, long jiffies_timeout);
 
 extern int
 efab_eplock_lock_timeout(ci_netif* ni, signed long timeout_jiffies);

src/include/onload/ioctl.h
@@ -178,7 +178,7 @@ enum {
 #define OO_IOC_EPLOCK_WAKE_AND_DO   OO_IOC_W(EPLOCK_WAKE_AND_DO, ci_uint64)
 #endif
   OO_OP_EPLOCK_LOCK_WAIT,
-#define OO_IOC_EPLOCK_LOCK_WAIT     OO_IOC_NONE(EPLOCK_LOCK_WAIT)
+#define OO_IOC_EPLOCK_LOCK_WAIT     OO_IOC_W(EPLOCK_LOCK_WAIT, ci_int32)
   
   OO_OP_INSTALL_STACK,
 #define OO_IOC_INSTALL_STACK        OO_IOC_W(INSTALL_STACK,             \

src/lib/efthrm/eplock_resource_manager.c
@@ -148,7 +148,7 @@ static int efab_eplock_is_unlocked_or_request_wake(ci_eplock_t* epl)
 
 
 int
-efab_eplock_lock_wait(ci_netif* ni, int maybe_wedged)
+efab_eplock_lock_wait(ci_netif* ni, int maybe_wedged, long jiffies_timeout)
 {
   wait_queue_entry_t wait;
   int rc;
@@ -165,7 +165,11 @@ efab_eplock_lock_wait(ci_netif* ni, int maybe_wedged)
     rc = efab_eplock_is_unlocked_or_request_wake(&ni->state->lock);
     if( rc <= 0 )
       break;
-    schedule();
+    jiffies_timeout = schedule_timeout(jiffies_timeout);
+    if( jiffies_timeout == 0 ) {
+      rc = -ETIMEDOUT;
+      break;
+    }
     if(CI_UNLIKELY( signal_pending(current) )) {
       rc = -ERESTARTSYS;
       break;

src/lib/efthrm/tcp_helper_ioctl.c
@@ -993,11 +993,15 @@ efab_eplock_wake_and_do_rsop(ci_private_t *priv, void *arg)
 }
 #endif
 static int
-efab_eplock_lock_wait_rsop(ci_private_t *priv, void *unused)
+efab_eplock_lock_wait_rsop(ci_private_t *priv, void *arg)
 {
+  ci_int32* timeout_p = arg;
+  long timeout = msecs_to_jiffies(*timeout_p);
   if (priv->thr == NULL)
     return -EINVAL;
-  return efab_eplock_lock_wait(&priv->thr->netif, 0);
+  if( timeout < 0 || timeout > MAX_SCHEDULE_TIMEOUT )
+    timeout = MAX_SCHEDULE_TIMEOUT;
+  return efab_eplock_lock_wait(&priv->thr->netif, 0, timeout);
 }
 static int
 efab_install_stack(ci_private_t *priv, void *arg)

src/lib/efthrm/tcp_helper_sleep.c
@@ -32,7 +32,7 @@ sock_sleep__on_wakeup(ci_waiter_t* waiter, void* opaque_trs,
 
   if( rc == 0 && (op->lock_flags & CI_SLEEP_NETIF_RQ) )
     if( ! (trs->netif.state->lock.lock & CI_EPLOCK_UNLOCKED) ) {
-      rc = efab_eplock_lock_wait(&trs->netif, 0);
+      rc = efab_eplock_lock_wait(&trs->netif, 0, MAX_SCHEDULE_TIMEOUT);
       rc = CI_WAITER_CONVERT_REENTRANT(rc);
     }
 

src/lib/transport/ip/eplock_slow.c
@@ -27,11 +27,12 @@ CI_BUILD_ASSERT( (CI_EPLOCK_LOCK_FLAGS & CI_EPLOCK_CALLBACK_FLAGS) == 0 );
 static int __ef_eplock_lock_wait(ci_netif *ni, int maybe_wedged)
 {
 #ifndef __KERNEL__
+  ci_int32 op = -1; /* wait forever */
   ci_assert_equal(maybe_wedged, 0);
   return oo_resource_op(ci_netif_get_driver_handle(ni), OO_IOC_EPLOCK_LOCK_WAIT,
-                        NULL);
+                        &op);
 #else
-  return efab_eplock_lock_wait(ni, maybe_wedged);
+  return efab_eplock_lock_wait(ni, maybe_wedged, MAX_SCHEDULE_TIMEOUT);
 #endif
 }
 

commit cedbd6ad6eb08c7630f6e9a5d727885f9c10513f

ON-12679: ON-12667: really lock stack when exiting
Call ci_netif_trylock() after sleeping on the stack lock when locking
stack at exit.

Fixes: a470774e111e ON-12667: use timeout when locking stacks from on_exit hook

src/lib/transport/common/netif_init.c
@@ -749,6 +749,11 @@ void oo_exit_hook(void)
       ci_int32 op = 1000; /* 1sec */
       rc = oo_resource_op(ci_netif_get_driver_handle(ni),
                           OO_IOC_EPLOCK_LOCK_WAIT, &op);
+      /* We were woken up by timeout or by a signal.  In any case we do not
+       * want to sleep again; let's exit!
+       */
+      if( ! ci_netif_trylock(ni) )
+        rc = -EBUSY;
     }
     if( rc == 0 )
       ni->state->exiting_pid = pid;