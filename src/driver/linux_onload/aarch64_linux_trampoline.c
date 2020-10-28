aac057e7da25465ae79dff4d3e641ae5101348ce
Alexandra Kossovsky <Alexandra.Kossovsky@oktetlabs.ru>
2020-10-28 09:34:18 +0000

    ON-12585: fix aarch64 syscall table access

--- src/driver/linux_onload/aarch64_linux_trampoline.c
+++ src/driver/linux_onload/aarch64_linux_trampoline.c
@@ -14,47 +14,47 @@
 #include <asm/esr.h>
 #include <asm/insn.h>
 #include <asm/ptrace.h>
-#include <linux/stop_machine.h>
 
 /* On 4.17+ on ARM64 the system calls are taking a single
    ptregs argument.
    (The user-space calling convention is the same as before, though).
 */
 #ifdef EFRM_SYSCALL_PTREGS
-#  define SYSCALL_PTR_DEF(_name)                        \
-  asmlinkage long (*saved_##_name)(const struct pt_regs *regs)
-#  define PASS_SYSCALL1(_name, _arg)                    \
-  ((saved_##_name)(&(struct pt_regs){.regs[0] = (u64)(_arg)}))
-#  define PASS_SYSCALL2(_name, _arg1, _arg2)                  \
-  ((saved_##_name)(&(struct pt_regs){.regs[0] = (u64)(_arg1), \
+#  define SYSCALL_PTR_DEF(_name)                                \
+  asmlinkage long (*syscall_fn)(const struct pt_regs *regs) =   \
+    efrm_syscall_table[__NR_##_name];
+#  define PASS_SYSCALL1(_arg)                                   \
+  (syscall_fn(&(struct pt_regs){.regs[0] = (u64)(_arg)}))
+#  define PASS_SYSCALL2(_arg1, _arg2)                           \
+  (syscall_fn(&(struct pt_regs){.regs[0] = (u64)(_arg1),        \
         .regs[1] = (u64)(_arg2)}))
-#  define PASS_SYSCALL3(_name, _arg1, _arg2, _arg3)                     \
-  ((saved_##_name)(&(struct pt_regs){.regs[0] = (u64)(_arg1),           \
-        .regs[1] = (unsigned long)(_arg2),                              \
+#  define PASS_SYSCALL3(_arg1, _arg2, _arg3)                        \
+  (syscall_fn(&(struct pt_regs){.regs[0] = (u64)(_arg1),            \
+        .regs[1] = (unsigned long)(_arg2),                          \
         .regs[2] = (unsigned long)(_arg3)}))
-#  define PASS_SYSCALL4(_name, _arg1, _arg2, _arg3, _arg4)              \
-  ((saved_##_name)(&(struct pt_regs){.regs[0] = (u64)(_arg1),           \
-        .regs[1] = (u64)(_arg2),                                        \
-        .regs[2] = (u64)(_arg3),                                        \
+#  define PASS_SYSCALL4(_arg1, _arg2, _arg3, _arg4)                 \
+  (syscall_fn(&(struct pt_regs){.regs[0] = (u64)(_arg1),            \
+        .regs[1] = (u64)(_arg2),                                    \
+        .regs[2] = (u64)(_arg3),                                    \
         .regs[3] = (u64)(_arg4)}))
-#  define PASS_SYSCALL6(_name, _arg1, _arg2, _arg3, _arg4, _arg5, _arg6) \
-  ((saved_##_name)(&(struct pt_regs){.regs[0] = (u64)(_arg1),           \
-        .regs[1] = (u64)(_arg2),                                        \
-        .regs[2] = (u64)(_arg3),                                        \
-        .regs[3] = (u64)(_arg4),                                        \
-        .regs[4] = (u64)(_arg5),                                        \
+#  define PASS_SYSCALL6(_arg1, _arg2, _arg3, _arg4, _arg5, _arg6)   \
+  (syscall_fn(&(struct pt_regs){.regs[0] = (u64)(_arg1),            \
+        .regs[1] = (u64)(_arg2),                                    \
+        .regs[2] = (u64)(_arg3),                                    \
+        .regs[3] = (u64)(_arg4),                                    \
+        .regs[4] = (u64)(_arg5),                                    \
         .regs[5] = (u64)(_arg6)}))
 #else
 #  define SYSCALL_PTR_DEF(_name)                \
-    asmlinkage typeof(_name) *saved_##_name
-#  define PASS_SYSCALL1(_name, _arg) ((saved_##_name)(_arg))
-#  define PASS_SYSCALL2(_name, _arg1, _arg2) ((saved_##_name)(_arg1, _arg2))
-#  define PASS_SYSCALL3(_name, _arg1, _arg2, _arg3) \
-  ((saved_##_name)(_arg1, _arg2, _arg3))
-#  define PASS_SYSCALL4(_name, _arg1, _arg2, _arg3, _arg4)    \
-  ((saved_##_name)(_arg1, _arg2, _arg3, _arg4))
-#  define PASS_SYSCALL6(_name, _arg1, _arg2, _arg3, _arg4, _arg5, _arg6)    \
-  ((saved_##_name)(_arg1, _arg2, _arg3, _arg4, _arg5, _arg6))
+    asmlinkage typeof(_name) *syscall_fn = efrm_syscall_table[__NR_##name];
+#  define PASS_SYSCALL1(_arg) (syscall_fn(_arg))
+#  define PASS_SYSCALL2(_arg1, _arg2) (syscall_fn(_arg1, _arg2))
+#  define PASS_SYSCALL3(_arg1, _arg2, _arg3) \
+  (syscall_fn(_arg1, _arg2, _arg3))
+#  define PASS_SYSCALL4(_arg1, _arg2, _arg3, _arg4)    \
+  (syscall_fn(_arg1, _arg2, _arg3, _arg4))
+#  define PASS_SYSCALL6(_arg1, _arg2, _arg3, _arg4, _arg5, _arg6)    \
+  (syscall_fn(_arg1, _arg2, _arg3, _arg4, _arg5, _arg6))
 #endif
 
 /*
@@ -73,59 +73,54 @@
 #define SET_SYSCALL_NO(_sc) \
   asm volatile("mov x8, %0" :: "i" (__NR_##_sc))
 
-static SYSCALL_PTR_DEF(sys_close);
 
 asmlinkage int efab_linux_sys_close(int fd)
 {
   int rc;
+  SYSCALL_PTR_DEF(close);
   SET_SYSCALL_NO(close);
-  rc = (int)PASS_SYSCALL1(sys_close, fd);
+  rc = (int)PASS_SYSCALL1(fd);
   return rc;
 }
-
-static SYSCALL_PTR_DEF(sys_epoll_create1);
 
 int efab_linux_sys_epoll_create1(int flags)
 {
   int rc;
+  SYSCALL_PTR_DEF(epoll_create1);
   SET_SYSCALL_NO(epoll_create1);
-  rc = (int)PASS_SYSCALL1(sys_epoll_create1, flags);
+  rc = (int)PASS_SYSCALL1(flags);
   return rc;
 }
-
-static SYSCALL_PTR_DEF(sys_epoll_ctl);
 
 int efab_linux_sys_epoll_ctl(int epfd, int op, int fd,
                              struct epoll_event *event)
 {
   int rc;
+  SYSCALL_PTR_DEF(epoll_ctl);
   SET_SYSCALL_NO(epoll_ctl);
-  rc = (int)PASS_SYSCALL4(sys_epoll_ctl, epfd, op, fd, event);
+  rc = (int)PASS_SYSCALL4(epfd, op, fd, event);
   return rc;
 }
-
-static SYSCALL_PTR_DEF(sys_epoll_pwait);
 
 int efab_linux_sys_epoll_wait(int epfd, struct epoll_event *events,
                               int maxevents, int timeout)
 {
   int rc;
+  SYSCALL_PTR_DEF(epoll_pwait);
   SET_SYSCALL_NO(epoll_pwait);
-  rc = (int)PASS_SYSCALL6(sys_epoll_pwait,
-                          epfd, events, maxevents, timeout,
+  rc = (int)PASS_SYSCALL6(epfd, events, maxevents, timeout,
                           NULL, sizeof(sigset_t));
   return rc;
 }
-
-static SYSCALL_PTR_DEF(sys_sendmsg);
 
 int efab_linux_sys_sendmsg(int fd, struct msghdr __user* msg,
                            unsigned long __user* socketcall_args,
                            unsigned flags)
 {
   int rc;
+  SYSCALL_PTR_DEF(sendmsg);
   SET_SYSCALL_NO(sendmsg);
-  rc = (int)PASS_SYSCALL3(sys_sendmsg, fd, (struct user_msghdr __user *)msg, flags);
+  rc = (int)PASS_SYSCALL3(fd, (struct user_msghdr __user *)msg, flags);
   return rc;
 }
 
