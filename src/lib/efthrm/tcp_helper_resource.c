Xilinx-CNS/onload

commit 74ee5ef484d80a05aa45b3b3118c2cbe4b4fcf5f

ON-12646: fixup assert in efab_tcp_helper_netif_lock_callback introduced by b9a66dd72d
Change-Id: I620a9bea264854ff4da8447b07b600ab7477ac73

src/lib/efthrm/tcp_helper_resource.c
@@ -7629,7 +7629,7 @@ efab_tcp_helper_netif_lock_callback(eplock_helper_t* epl, ci_uint64 lock_val,
 #endif
 
     /* we have handled all the flags */
-    ci_assert_equal(flags_set, all_after_unlock_flags);
+    ci_assert_equal(flags_set, after_unlock_flags);
 
     /* We can free some packets while closing endpoints, etc.  Check this
      * condition again, but do it only once. */

commit 2e2b7dde634189ca108b327f801fd7b29524ab2e

ON-12646: 2nd fixup assert in efab_tcp_helper_netif_lock_callback introduced by 74ee5ef484
Change-Id: I668f6f409e5d2c39f2c4b7131789cec9e77a884b

src/lib/efthrm/tcp_helper_resource.c
@@ -7628,8 +7628,8 @@ efab_tcp_helper_netif_lock_callback(eplock_helper_t* epl, ci_uint64 lock_val,
     }
 #endif
 
-    /* we have handled all the flags */
-    ci_assert_equal(flags_set, after_unlock_flags);
+    /* we have handled all the flags (that get handled before unlock) */
+    ci_assert_nflags(flags_set, ~all_after_unlock_flags);
 
     /* We can free some packets while closing endpoints, etc.  Check this
      * condition again, but do it only once. */