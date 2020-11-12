Xilinx-CNS/onload

commit e5fdafb90479a36af063cf0454a3341d949e1714

ON-12736: use vmalloc() for temporary storage in efab_tcp_helper_more_bufs()

src/lib/efthrm/tcp_helper_resource.c
@@ -6339,8 +6339,8 @@ efab_tcp_helper_more_bufs(tcp_helper_resource_t* trs)
     return -EBUSY;
   }
 
-  hw_addrs = ci_alloc(sizeof(uint64_t) * (1 << HW_PAGES_PER_SET_S) *
-                      CI_CFG_MAX_INTERFACES);
+  hw_addrs = ci_vmalloc(sizeof(uint64_t) * (1 << HW_PAGES_PER_SET_S) *
+                        CI_CFG_MAX_INTERFACES);
   if( hw_addrs == NULL ) {
     ci_log("%s: [%d] out of memory", __func__, trs->id);
     return -ENOMEM;
@@ -6378,7 +6378,7 @@ efab_tcp_helper_more_bufs(tcp_helper_resource_t* trs)
                                   CI_EPLOCK_NETIF_NEED_PKT_SET);
       }
     }
-    ci_free(hw_addrs);
+    ci_vfree(hw_addrs);
     return rc;
   }
   /* check we get the size we are expecting */
@@ -6395,7 +6395,7 @@ efab_tcp_helper_more_bufs(tcp_helper_resource_t* trs)
     OO_STACK_FOR_EACH_INTF_I(ni, intf_i)
       oo_iobufset_resource_release(iobrs[intf_i], 0);
     oo_iobufset_pages_release(pages);
-    ci_free(hw_addrs);
+    ci_vfree(hw_addrs);
     return -ENOSPC;
   }
   bufset_id = ni->pkt_sets_n;
@@ -6464,7 +6464,7 @@ efab_tcp_helper_more_bufs(tcp_helper_resource_t* trs)
     set_pkt_bufset_hwaddrs(ni, bufset_id, intf_i,
                            hw_addrs + intf_i * (1 << HW_PAGES_PER_SET_S));
   }
-  ci_free(hw_addrs);
+  ci_vfree(hw_addrs);
 
   trs->netif.state->packet_alloc_numa_nodes |= 1 << numa_node_id();
   CHECK_FREEPKTS(ni);

commit 2b4198ecb51629c390161766b5d86151b473ec84

ON-12736: avoid pkt allocations from driverlink context
Purge CI_NETIF_FLAG_AVOID_ATOMIC_ALLOCATION flag and always do as if
it is present.

It may result in some performance regressions in the cases when the flag
was not present: EF_PACKET_BUFFER_MODE=2 or Siena/Falcon.

Fixes: e5fdafb9047 ON-12736: use vmalloc() for temporary storage in efab_tcp_helper_more_bufs()

src/include/ci/internal/ip_types.h
@@ -268,8 +268,6 @@ struct ci_netif_s {
 # define CI_NETIF_FLAGS_IS_TRUSTED       0x100
   /* Currently being used from a driverlink context */
 # define CI_NETIF_FLAG_IN_DL_CONTEXT     0x400
-  /* Should not allocate packets in atomic/driverlink context */
-# define CI_NETIF_FLAG_AVOID_ATOMIC_ALLOCATION 0x800
 #if CI_CFG_PKTS_AS_HUGE_PAGES
   /* Huge pages packet allocation have failed */
 #define CI_NETIF_FLAG_HUGE_PAGES_FAILED  0x2000

src/lib/efthrm/tcp_helper_resource.c
@@ -1123,8 +1123,6 @@ static int allocate_pd(ci_netif* ni, struct vi_allocate_info* info,
 
   switch( NI_OPTS(ni).packet_buffer_mode ) {
   case 0:
-    if( nic->devtype.arch == EFHW_ARCH_EF10 )
-      ni->flags |= CI_NETIF_FLAG_AVOID_ATOMIC_ALLOCATION;
     break;
 
   case CITP_PKTBUF_MODE_PHYS:
@@ -6328,10 +6326,7 @@ efab_tcp_helper_more_bufs(tcp_helper_resource_t* trs)
   if( ni->pkt_sets_n == ni->pkt_sets_max )
     return -ENOSPC;
 
-  if( (ni->flags & (CI_NETIF_FLAG_IN_DL_CONTEXT |
-                    CI_NETIF_FLAG_AVOID_ATOMIC_ALLOCATION) ) ==
-       (CI_NETIF_FLAG_IN_DL_CONTEXT |
-        CI_NETIF_FLAG_AVOID_ATOMIC_ALLOCATION) ) {
+  if( ni->flags & CI_NETIF_FLAG_IN_DL_CONTEXT ) {
     ef_eplock_holder_set_flag(&ni->state->lock,
                               CI_EPLOCK_NETIF_NEED_PKT_SET);
     return -EBUSY;
@@ -6360,21 +6355,6 @@ efab_tcp_helper_more_bufs(tcp_helper_resource_t* trs)
       NI_LOG(ni, RESOURCE_WARNINGS,
              FN_FMT "Failed to allocate packet buffers (%d)",
              FN_PRI_ARGS(&trs->netif), rc);
-
-      /* We've got ENOMEM.  If we are in atomic context, it is possible
-       * that memory is available in non-atomic mode.
-       * efab_tcp_helper_netif_lock_callback() will kick off packet
-       * allocation via workqueue without
-       * CI_NETIF_FLAG_AVOID_ATOMIC_ALLOCATION flag check, so we'll
-       * retry from non-atomic context immediately.
-       *
-       * For all other errors (ENOMEM & non-atomic or other rc values),
-       * there is no obvious benefit in re-trying the same operation
-       * immediately. */
-      if( rc == -ENOMEM && ni->flags & CI_NETIF_FLAG_IN_DL_CONTEXT ) {
-        ef_eplock_holder_set_flag(&ni->state->lock,
-                                  CI_EPLOCK_NETIF_NEED_PKT_SET);
-      }
     }
     ci_vfree(hw_addrs);
     return rc;