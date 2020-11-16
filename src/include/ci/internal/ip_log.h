Xilinx-CNS/onload

commit ecaca1e04f39803ea0f51e39a10acbb3ff3d0575

ON-12737: OOF_IPCACHE_DETAIL logging: print verinfo

src/include/ci/internal/ip_log.h
@@ -190,9 +190,12 @@
 #define OOFA_IPCACHE_STATE(ni, ipc)  OOFA_IPCACHE_STATUS((ipc)->status), \
                                      OOFA_IPCACHE_VALID((ni), (ipc))
 
-#define OOF_IPCACHE_DETAIL        "if=%d mtu=%d intf_i=%d vlan=%d encap=%x"
-#define OOFA_IPCACHE_DETAIL(ipc)  (ipc)->ifindex, (ipc)->mtu, (ipc)->intf_i, \
-                                  (ipc)->encap.vlan_id, (ipc)->encap.type
+#define OOF_IPCACHE_DETAIL \
+  "if=%d mtu=%d intf_i=%d vlan=%d encap=%x verinfo %x-%x"
+#define OOFA_IPCACHE_DETAIL(ipc) \
+  (ipc)->ifindex, (ipc)->mtu, (ipc)->intf_i, \
+  (ipc)->encap.vlan_id, (ipc)->encap.type, \
+  (ipc)->mac_integrity.id, (ipc)->mac_integrity.version
 
 
 /**********************************************************************

commit 0913bf16a692fe3117dfa1278203bd45aed01cb7

ON-12737: fix up for master branch
Fixes: ecaca1e04f ON-12737: OOF_IPCACHE_DETAIL logging: print verinfo

src/include/ci/internal/ip_log.h
@@ -195,7 +195,7 @@
 #define OOFA_IPCACHE_DETAIL(ipc) \
   (ipc)->ifindex, (ipc)->mtu, (ipc)->intf_i, \
   (ipc)->encap.vlan_id, (ipc)->encap.type, \
-  (ipc)->mac_integrity.id, (ipc)->mac_integrity.version
+  (ipc)->fwd_ver.id, (ipc)->fwd_ver.version
 
 
 /**********************************************************************