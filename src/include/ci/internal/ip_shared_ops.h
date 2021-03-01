Xilinx-CNS/onload

commit 80008c514b9816e5ff9848cc071fe9155baedb13

ON-12686: use oo_shmbuf for socket states

src/include/ci/internal/ip_shared_ops.h
@@ -182,7 +182,11 @@ extern int no_shared_state_panic;
 */
 ci_inline oo_p oo_ptr_to_statep(const ci_netif* ni, void* ptr) {
   oo_p sp;
+#ifdef __KERNEL__
+  OO_P_INIT(sp, ni, (ci_uint32) oo_shmbuf_ptr2off(&ni->shmbuf, ptr));
+#else
   OO_P_INIT(sp, ni, (ci_uint32) ((char*) ptr - (char*) ni->state));
+#endif
   return sp;
 }
 
@@ -200,11 +204,11 @@ ci_inline oo_p oo_ptr_to_statep(const ci_netif* ni, void* ptr) {
  * state. */
 ci_inline char* oo_state_off_to_ptr(ci_netif* ni, unsigned off)
 {
-  char* ptr = (char*) ni->state + off;
-#ifdef __ci_driver__
-  ci_assert_equal(ptr, ci_shmbuf_ptr(&ni->pages_buf, off));
+#ifndef __KERNEL__
+  return (char*) ni->state + off;;
+#else
+  return oo_shmbuf_off2ptr(&ni->shmbuf, off);
 #endif
-  return ptr;
 }
 
 

src/lib/efthrm/tcp_helper_resource.c
@@ -2224,29 +2224,32 @@ allocate_netif_resources(ci_resource_onload_alloc_t* alloc,
   }
 #endif
 
-  sz = CI_ROUND_UP(sz, CI_PAGE_SIZE);
+  sz = CI_ROUND_UP(sz, OO_SHARED_BUFFER_CHUNK_SIZE);
 
-  /* [pages_buf] backs the shared stack state and the socket buffers.  First,
+  /* [shmbuf] backs the shared stack state and the socket buffers.  First,
    * count the pages required for the latter. */
-  i = (NI_OPTS(ni).max_ep_bufs + EP_BUF_PER_PAGE - 1) / EP_BUF_PER_PAGE;
+  i = (NI_OPTS(ni).max_ep_bufs +
+       (EP_BUF_PER_PAGE << OO_SHARED_BUFFER_CHUNK_ORDER) - 1) /
+      (EP_BUF_PER_PAGE << OO_SHARED_BUFFER_CHUNK_ORDER);
   /* Now add in the pages for the shared state. */
-  i += sz / CI_PAGE_SIZE;
+  i += sz / OO_SHARED_BUFFER_CHUNK_SIZE;
 
   /* Allocate the shmbuf, and fault in the pages for the shared state (but not
    * for the sockets).  These pages get zeroed, so all fields in the shared
    * state can be assumed to have been zero-initialised. */
-  rc = ci_shmbuf_alloc(&ni->pages_buf, i, sz / CI_PAGE_SIZE);
+  rc = oo_shmbuf_alloc(&ni->shmbuf, OO_SHARED_BUFFER_CHUNK_ORDER, i,
+                       sz / OO_SHARED_BUFFER_CHUNK_SIZE);
   if( rc < 0 ) {
     OO_DEBUG_ERR(ci_log("%s: failed to alloc shmbuf for shared state and "
                         "socket buffers (%d)", __FUNCTION__, rc));
     goto fail1;
   }
 
-  ns = ni->state = (ci_netif_state*) ci_shmbuf_ptr(&ni->pages_buf, 0);
+  ns = ni->state = (ci_netif_state*) oo_shmbuf_off2ptr(&ni->shmbuf, 0);
 
   CI_DEBUG(ns->flags |= CI_NETIF_FLAG_DEBUG);
 
-  ns->netif_mmap_bytes = ci_shmbuf_size(&ni->pages_buf);
+  ns->netif_mmap_bytes = oo_shmbuf_size(&ni->shmbuf);
 
   ns->stack_id = trs->id;
   ns->ep_ofs = ni->ep_ofs = sz;
@@ -2422,7 +2425,7 @@ allocate_netif_resources(ci_resource_onload_alloc_t* alloc,
   return 0;
 
  fail2:
-  ci_shmbuf_free(&ni->pages_buf);
+  oo_shmbuf_free(&ni->shmbuf);
  fail1:
   LOG_NC(ci_log("failed to allocate tcp_helper resources (%d)", rc));
   return rc;
@@ -2678,7 +2681,7 @@ release_netif_resources(tcp_helper_resource_t* trs)
   for( i = 0; i < CI_CFG_N_READY_LISTS; i++ )
     ci_waitable_dtor(&trs->ready_list_waitqs[i]);
 
-  ci_shmbuf_free(&ni->pages_buf);
+  oo_shmbuf_free(&ni->shmbuf);
 }
 static void
 release_netif_hw_resources(tcp_helper_resource_t* trs)
@@ -5763,11 +5766,13 @@ add_ep(tcp_helper_resource_t* trs, unsigned id, tcp_helper_endpoint_t* ep)
 static int
 install_socks(tcp_helper_resource_t* trs, unsigned id, int num)
 {
-  tcp_helper_endpoint_t* eps[EP_BUF_PER_PAGE];
+  tcp_helper_endpoint_t** eps;
   ci_irqlock_state_t lock_flags;
   int i;
 
-  ci_assert_equal(num, EP_BUF_PER_PAGE);
+  eps = vmalloc(sizeof(void*) * num);
+  if( eps == NULL )
+    return -ENOMEM;
 
   /* Allocate the kernel state for each socket. */
   for( i = 0; i < num; ++i ) {
@@ -5775,12 +5780,13 @@ install_socks(tcp_helper_resource_t* trs, unsigned id, int num)
     if( ! eps[i] ) {
       OO_DEBUG_ERR(ci_log("%s: allocation failed", __FUNCTION__));
       while( i-- )  ci_free(eps[i]);
+      vfree(eps);
       return -ENOMEM;
     }
   }
 
   ci_irqlock_lock(&THR_TABLE.lock, &lock_flags);
-  for( i = 0; i < EP_BUF_PER_PAGE; ++i, ++id ){
+  for( i = 0; i < num; ++i, ++id ){
     OO_DEBUG_SHM(ci_log("%s: add ep %d", __FUNCTION__, id));
     if( add_ep(trs, id, eps[i]) == 0 )
       eps[i] = NULL;
@@ -5788,9 +5794,10 @@ install_socks(tcp_helper_resource_t* trs, unsigned id, int num)
   ci_irqlock_unlock(&THR_TABLE.lock, &lock_flags);
 
   /* Prevents leaks! */
-  for( i = 0; i < EP_BUF_PER_PAGE; ++i )
+  for( i = 0; i < num; ++i )
     if( eps[i] )
       ci_free(eps[i]);
+  vfree(eps);
 
   trs->netif.state->sock_alloc_numa_nodes |= 1 << numa_node_id();
   return 0;
@@ -5801,18 +5808,17 @@ int efab_tcp_helper_more_socks(tcp_helper_resource_t* trs)
 {
   ci_netif* ni = &trs->netif;
   int rc;
-  unsigned page_i;
 
   if( ni->ep_tbl_n >= ni->ep_tbl_max )  return -ENOSPC;
 
-  page_i = oo_sockid_to_state_off(ni, ni->ep_tbl_n) >> CI_PAGE_SHIFT;
-  rc = ci_shmbuf_demand_page(&ni->pages_buf, page_i);
+  rc = oo_shmbuf_add(&ni->shmbuf);
   if( rc < 0 ) {
     OO_DEBUG_ERR(ci_log("%s: demand failed (%d)", __FUNCTION__, rc));
     return rc;
   }
 
-  return install_socks(trs, ni->ep_tbl_n, EP_BUF_PER_PAGE);
+  return install_socks(trs, ni->ep_tbl_n,
+                       EP_BUF_PER_PAGE << OO_SHARED_BUFFER_CHUNK_ORDER);
 }
 
 

src/driver/linux_onload/mmap.c
@@ -46,11 +46,12 @@
  ****************************************************************************/
 
 /*! map offset in shared data to physical page frame number */
-static struct page*
+static vm_fault_t
 tcp_helper_rm_nopage_mem(tcp_helper_resource_t* trs,
                          struct vm_area_struct *vma, unsigned long offset)
 {
   ci_netif* ni = &trs->netif;
+  int rc;
 
   OO_DEBUG_SHM(ci_log("%s: %u", __FUNCTION__, trs->id));
 
@@ -60,12 +61,13 @@ tcp_helper_rm_nopage_mem(tcp_helper_resource_t* trs,
          region.
   */
 
-  if( offset < ci_shmbuf_size(&ni->pages_buf) )
-    return ci_shmbuf_page(&ni->pages_buf, offset);
+  rc = oo_shmbuf_fault(&ni->shmbuf, vma, offset);
+  if( rc == 0 )
+    return VM_FAULT_NOPAGE;
 
   OO_DEBUG_SHM(ci_log("%s: offset %lx out of range", __FUNCTION__, offset));
   ci_assert(0);
-  return NULL;
+  return VM_FAULT_SIGBUS;
 }
 
 
@@ -135,8 +137,7 @@ tcp_helper_rm_nopage(tcp_helper_resource_t* trs, struct vm_area_struct *vma,
 
   switch( map_id ) {
     case CI_NETIF_MMAP_ID_STATE:
-      *page_out = tcp_helper_rm_nopage_mem(trs, vma, offset);
-      return *page_out == NULL ? VM_FAULT_SIGBUS : 0;
+      return tcp_helper_rm_nopage_mem(trs, vma, offset);
     case CI_NETIF_MMAP_ID_TIMESYNC:
       *page_out = tcp_helper_rm_nopage_timesync(trs, vma, offset);
       return *page_out == NULL ? VM_FAULT_SIGBUS : 0;
@@ -239,21 +240,15 @@ static int tcp_helper_rm_mmap_mem(tcp_helper_resource_t* trs,
                                   unsigned long bytes,
                                   struct vm_area_struct* vma)
 {
-  int rc = 0;
-  int map_num = 0;
-  unsigned long offset = 0;
-
   OO_DEBUG_VM(ci_log("%s: %u bytes=0x%lx", __func__, trs->id, bytes));
 
-  rc = ci_shmbuf_mmap(&trs->netif.pages_buf, 0, &bytes, vma,
-                           &map_num, &offset);
-  if( rc < 0 )  goto out;
-  OO_DEBUG_MEMSIZE(ci_log("after mapping page buf have %ld", bytes));
-
-  ci_assert_equal(bytes, 0);
-
- out:
-  return rc;
+  /* Let's fault in the first chunk right now, and defer the socket states
+   * to the fault handler.
+   */
+  if( bytes == oo_shmbuf_size(&trs->netif.shmbuf) )
+    return oo_shmbuf_fault(&trs->netif.shmbuf, vma, 0);
+  else
+    return -EFAULT;
 }
 
 

src/driver/linux_onload/oo_shmbuf.c
@@ -63,8 +63,9 @@ unsigned long oo_shmbuf_ptr2off(const struct oo_shmbuf* sh, char* ptr)
 {
   int i;
 
-  ci_assert_lt(sh->num, 10);
-  /* Fixme implement hash table */
+  /* Fixme implement hash table, it is really necessary with 2^21 sockets
+   * => 2^11=2048 chunks.  See the comment at OO_SHARED_BUFFER_CHUNK_SIZE.
+   */
 
   for(i = 0; i < sh->num; i++) {
     unsigned long off = ptr - oo_shmbuf_idx2ptr(sh, i);

src/include/ci/internal/ip_types.h
@@ -147,7 +147,14 @@ struct ci_netif_s {
   /* These uid_t are in the kernel init namespace */
   uid_t                kuid;
   uid_t                keuid;
-  ci_shmbuf_t          pages_buf;
+
+  struct oo_shmbuf     shmbuf; /* shared state storage */
+  /* Size of continuous chunks: */
+#define OO_SHARED_BUFFER_CHUNK_ORDER (PMD_SHIFT - PAGE_SHIFT)
+#define OO_SHARED_BUFFER_CHUNK_SIZE  (1ULL << PMD_SHIFT)
+  /* With CI_CFG_NETIF_MAX_ENDPOINTS_MAX = 2^21,
+   * there can't be more than 2^11 chunks of socket buffers
+   * (+ a few chunks for the stack state itsefl). */
 #endif
 
   struct oo_cplane_handle *cplane;

commit 1988b122e1358f858df9880633bcad67b3374791

ON-12989: ON-12686: ensure that max_ep_bufs is divisable by 2048
Endpoint buffers are allocated in chunks, 2048 buffers in each chunk.
This patch ensures that EF_MAX_ENDPOINTS/max_ep_bufs value is rounded up
before use.

Fixes: 80008c514b9 ON-12686: use oo_shmbuf for socket states

src/include/ci/internal/ip_shared_ops.h
@@ -50,31 +50,6 @@ ci_netif_unlock_slow_common(ci_netif*, ci_uint64 lock_val,
                             ci_uint64 flags_to_handle) CI_HF;
 
 
-/* Threshhold for proactive socket allocation is half of the shmbuf chunk
- * capability: 2^21 / 2^10 / 2 = 1024.  It can't guarantee that one driverlink
- * poll does not exhaust all the spare socket buffers, but probably gives
- * a good chance that a listener can accept all incoming connections and
- * create new sockets.
- *
- * Driverlink budget is usually 64, but with RX event merging one poll can
- * accept some hundreds of packets.
- */
-#define __OO_SOCK_ALLOC_PROACTIVE_THRESH 1024
-#ifdef __KERNEL__
-#define OO_SOCK_ALLOC_PROACTIVE_THRESH \
-  (OO_SHARED_BUFFER_CHUNK_SIZE / CI_CFG_EP_BUF_SIZE / 2)
-#if OO_SOCK_ALLOC_PROACTIVE_THRESH != __OO_SOCK_ALLOC_PROACTIVE_THRESH
-#error Please fix __OO_SOCK_ALLOC_PROACTIVE_THRESH value
-#endif
-#else
-#define OO_SOCK_ALLOC_PROACTIVE_THRESH __OO_SOCK_ALLOC_PROACTIVE_THRESH
-#endif
-static inline int
-oo_want_proactive_socket_allocation(ci_netif* ni)
-{
-  return ni->state->free_eps_num < OO_SOCK_ALLOC_PROACTIVE_THRESH;
-}
-
 /*! Blocking calls that grab the stack lock return 0 on success.  When
  * called at userlevel, this is the only possible outcome.  In the kernel,
  * they return -EINTR if interrupted by a signal.
@@ -258,6 +233,27 @@ ci_inline char* oo_state_off_to_ptr(ci_netif* ni, unsigned off)
 */
 #define EP_BUF_SIZE        CI_CFG_EP_BUF_SIZE
 #define EP_BUF_PER_PAGE    (CI_PAGE_SIZE / EP_BUF_SIZE)
+
+#define EP_BUF_PER_CHUNK    2048
+#ifdef __KERNEL__
+  CI_BUILD_ASSERT(OO_SHARED_BUFFER_CHUNK_SIZE / CI_CFG_EP_BUF_SIZE ==
+                  EP_BUF_PER_CHUNK);
+#endif
+
+/* Threshhold for proactive socket allocation is half of the shmbuf chunk
+ * capability: 2^21 / 2^10 / 2 = 1024.  It can't guarantee that one driverlink
+ * poll does not exhaust all the spare socket buffers, but probably gives
+ * a good chance that a listener can accept all incoming connections and
+ * create new sockets.
+ *
+ * Driverlink budget is usually 64, but with RX event merging one poll can
+ * accept some hundreds of packets.
+ */
+static inline int
+oo_want_proactive_socket_allocation(ci_netif* ni)
+{
+  return ni->state->free_eps_num < EP_BUF_PER_CHUNK / 2;
+}
 
 /* Aux buffers are sub-buffers of EP buffers.  Header at beginning,
  * and 7 aux buffer per 1024 bytes. */

src/lib/efthrm/tcp_helper_resource.c
@@ -2229,9 +2229,8 @@ allocate_netif_resources(ci_resource_onload_alloc_t* alloc,
 
   /* [shmbuf] backs the shared stack state and the socket buffers.  First,
    * count the pages required for the latter. */
-  i = (NI_OPTS(ni).max_ep_bufs +
-       (EP_BUF_PER_PAGE << OO_SHARED_BUFFER_CHUNK_ORDER) - 1) /
-      (EP_BUF_PER_PAGE << OO_SHARED_BUFFER_CHUNK_ORDER);
+  i = (NI_OPTS(ni).max_ep_bufs / EP_BUF_PER_PAGE) >>
+                                    OO_SHARED_BUFFER_CHUNK_ORDER;
   /* Now add in the pages for the shared state. */
   i += sz / OO_SHARED_BUFFER_CHUNK_SIZE;
 

src/lib/transport/ip/netif_init.c
@@ -654,6 +654,14 @@ void ci_netif_config_opts_rangecheck(ci_netif_config_opts* opts)
 }                                               
 
 # include <ci/internal/opts_netif_def.h>
+
+  /* EF_MAX_ENDPOINTS should must be divisible by 2048 */
+  if( opts->max_ep_bufs % EP_BUF_PER_CHUNK != 0 ) {
+    unsigned new_max = opts->max_ep_bufs;
+    new_max = CI_ROUND_UP(new_max, EP_BUF_PER_CHUNK);
+    ci_log("config: EF_MAX_ENDPOINTS is rounded up from %d to %d", opts->max_ep_bufs, new_max);
+    opts->max_ep_bufs = new_max;
+  }
 }
 
 