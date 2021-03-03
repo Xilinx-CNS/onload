Xilinx-CNS/onload

commit efcdd86986263d9c70847ccb9b0f2b53d0a1103b

ON-12686: reader-friendly oo_p_dllink instead of ci_ni_dllist_t

src/lib/transport/ip/netif.c
@@ -1111,7 +1111,9 @@ ci_uint64 ci_netif_unlock_slow_common(ci_netif* ni, ci_uint64 lock_val,
 
   if( test_val & CI_EPLOCK_NETIF_NEED_WAKE ) {
     /* Tell kernel to wake up endpoints */
-    ci_ni_dllist_link* lnk;
+    struct oo_p_dllink_state post_poll_list =
+                             oo_p_dllink_ptr(ni, &ni->state->post_poll_list);
+    struct oo_p_dllink_state lnk, tmp_lnk;
     citp_waitable* w;
     struct oo_wakeup_eps op;
     oo_sp eps[64];
@@ -1119,10 +1121,10 @@ ci_uint64 ci_netif_unlock_slow_common(ci_netif* ni, ci_uint64 lock_val,
     op.eps_num = 0;
     CI_USER_PTR_SET(op.eps, eps);
 
-    while( ci_ni_dllist_not_empty(ni, &ni->state->post_poll_list) ) {
-      lnk = ci_ni_dllist_head(ni, &ni->state->post_poll_list);
+    oo_p_dllink_for_each_safe(ni, lnk, tmp_lnk, post_poll_list) {
+      oo_p_dllink_del_init(ni, lnk);
+
       w = CI_CONTAINER(citp_waitable, post_poll_link, lnk);
-      ci_ni_dllist_remove_safe(ni, &w->post_poll_link);
       eps[op.eps_num++] = w->bufid;
 
       /* Todo: we'd better allocate larger eps and wake up all

src/include/ci/internal/ip.h
@@ -4784,8 +4784,12 @@ citp_waitable_remove_from_epoll(ci_netif* ni, citp_waitable* w, int do_free)
 
 ci_inline void ci_netif_put_on_post_poll(ci_netif* ni, citp_waitable* sb)
 {
-  ci_ni_dllist_remove(ni, &sb->post_poll_link);
-  ci_ni_dllist_put(ni, &ni->state->post_poll_list, &sb->post_poll_link);
+  struct oo_p_dllink_state link = oo_p_dllink_sb(ni, sb, &sb->post_poll_link);
+
+  oo_p_dllink_del(ni, link);
+  oo_p_dllink_add_tail(ni, oo_p_dllink_ptr(ni, &ni->state->post_poll_list),
+                       link);
+
 #if CI_CFG_EPOLL3
   if( sb->ready_lists_in_use != 0 )
     ci_netif_put_on_post_poll_epoll(ni, sb);

src/include/ci/internal/ip_shared_ops.h
@@ -295,6 +295,20 @@ ci_inline unsigned __TRUSTED_SOCK_ID(ci_netif* ni, unsigned id,
 ci_inline unsigned oo_sockid_to_state_off(ci_netif* ni, unsigned sock_id)
 { return ci_netif_ep_ofs(ni) + sock_id * EP_BUF_SIZE; }
 
+/* Convert pointer to oo_p.
+ * When in-kernel, it works for for the main state area only;
+ * it should not be used for socket buffer area in kernel mode.
+ */
+ci_inline oo_p oo_state_ptr_to_statep(const ci_netif* ni, const void* ptr)
+{
+  uintptr_t off = (uintptr_t)ptr - (uintptr_t)ni->state;
+  oo_p sp;
+#ifdef __KERNEL__
+  ci_assert_lt(off, ci_netif_ep_ofs(ni));
+#endif
+  OO_P_INIT(sp, ni, off);
+  return sp;
+}
 
 /* oo_sockp_to_statep(ni, oo_sp)
 **

src/include/ci/internal/ip_shared_types.h
@@ -129,6 +129,10 @@
 #define CI_ILL_NO_CODE
 #include <ci/internal/ni_dllist_tmpl_instantiate.h>
 
+#define OO_P_DLLIST_NO_CODE
+#include <onload/oo_p_dllist.h>
+
+
 
 
 /*********************************************************************
@@ -1359,7 +1363,7 @@ struct ci_netif_state_s {
 
   /* are we in the poll loop? */
   ci_int32              in_poll;
-  ci_ni_dllist_t        post_poll_list;
+  struct oo_p_dllink    post_poll_list;
 
   oo_pkt_p              rx_defrag_head;       /*  rx buffers re-assembly */
   oo_pkt_p              rx_defrag_tail;
@@ -1649,7 +1653,7 @@ typedef struct {
 #endif
 
   /* Link field for [ci_netif_state::post_poll_list]. */
-  ci_ni_dllist_link     post_poll_link;
+  struct oo_p_dllink      post_poll_link;
 
   /* Per-socket lock. */
   struct oo_waitable_lock lock;

src/include/ci/internal/iptimer.h
@@ -7,6 +7,7 @@
 #include <ci/internal/ip_shared_types.h>
 #include <ci/internal/ip_types.h>
 #include <ci/internal/ni_dllist.h>
+#include <onload/oo_p_dllist.h>
 
 /*********************************************************************
 ********************** Timers and management *************************

src/include/onload/oo_p_dllist.h
@@ -1,0 +1,178 @@
+/* There is no standard
+#ifndef __ONLOAD_OO_P_DLLIST_H__
+#define __ONLOAD_OO_P_DLLIST_H__
+ * guard, because this file can be included with OO_P_DLLIST_NO_CODE and
+ * without it.  We rely on the guard of the includer.
+ *
+ * We really should remove this crazy data / code separation in the include
+ * files...
+ */
+
+
+#ifdef OO_P_DLLIST_NO_CODE
+
+/* Structure to use for list head and members.
+ *
+ * This structure is designed for double-linked lists in the Onload stack
+ * shared state.
+ *
+ * The most of these double-linked lists share common semantics:
+ * - Circular linked list, with the head/tail at the main stack state
+ *   and members scattered in socket states.
+ * - Empty/unused link is self-linked.
+ */
+struct oo_p_dllink {
+ oo_p next;
+ oo_p prev;
+};
+
+#undef OO_P_DLLIST_NO_CODE
+#else /* OO_P_DLLIST_NO_CODE */
+
+/* Structure to declare locally and use in the code. */
+struct oo_p_dllink_state {
+  struct oo_p_dllink* l;
+  oo_p p;
+};
+
+static inline struct oo_p_dllink* oo_p_dllink_from_p(ci_netif* ni, oo_p p)
+{
+  return (void*)CI_NETIF_PTR(ni, p);
+}
+
+
+/* Create the state link structure from a pointer in the stack state.
+ * For pointers in socket state please use _sb() version below.
+ */
+static inline struct oo_p_dllink_state
+oo_p_dllink_ptr(ci_netif* ni, struct oo_p_dllink* l)
+{
+  struct oo_p_dllink_state ret = {
+    .l = l,
+    .p = oo_state_ptr_to_statep(ni, l),
+  };
+  return ret;
+}
+
+/* Create the link state structure from statep */
+static inline struct oo_p_dllink_state
+oo_p_dllink_statep(ci_netif* ni, oo_p p)
+{
+  struct oo_p_dllink_state ret = {
+    .l = oo_p_dllink_from_p(ni, p),
+    .p = p,
+  };
+  return ret;
+}
+
+/* Create the state link structure from a pointer in a socket state */
+#ifdef __KERNEL__
+static inline oo_p
+oo_p_dllink_sb_to_p(ci_netif* ni, citp_waitable* sb, struct oo_p_dllink* l)
+{
+  oo_p p = oo_sockp_to_statep(ni, sb->bufid);
+  OO_P_ADD(p, (uintptr_t)l - (uintptr_t)sb);
+  return p;
+}
+
+static inline struct oo_p_dllink_state
+oo_p_dllink_sb(ci_netif* ni, citp_waitable* sb, struct oo_p_dllink* l)
+{
+  struct oo_p_dllink_state ret = {
+    .l = l,
+    .p = oo_p_dllink_sb_to_p(ni, sb, l),
+  };
+  return ret;
+}
+#else /* __KERNEL__ */
+static inline struct oo_p_dllink_state
+oo_p_dllink_sb(ci_netif* ni, citp_waitable* sb, struct oo_p_dllink* l)
+{
+  return oo_p_dllink_ptr(ni, l);
+}
+#endif /* __KERNEL__ */
+
+
+/* Initialise a link or a head of a list */
+static inline void
+oo_p_dllink_init(ci_netif* ni, struct oo_p_dllink_state state)
+{
+  state.l->next = state.l->prev = state.p;
+}
+
+/* Delete a link from any list it is linked in.
+ * In the most cases the caller should call oo_p_dllink_init()
+ * after removal, unless the link is immediately reused.
+ */
+static inline void
+oo_p_dllink_del(ci_netif* ni, struct oo_p_dllink_state link)
+{
+  oo_p_dllink_from_p(ni, link.l->prev)->next = link.l->next;
+  oo_p_dllink_from_p(ni, link.l->next)->prev = link.l->prev;
+}
+
+static inline void
+oo_p_dllink_del_init(ci_netif* ni, struct oo_p_dllink_state link)
+{
+  oo_p_dllink_del(ni, link);
+  oo_p_dllink_init(ni, link);
+}
+
+static inline void
+__oo_p_dllink_add(ci_netif* ni, struct oo_p_dllink_state prev,
+                  struct oo_p_dllink_state next,
+                  struct oo_p_dllink_state new)
+{
+  next.l->prev = new.p;
+  new.l->next = next.p;
+  new.l->prev = prev.p;
+  prev.l->next = new.p;
+}
+
+static inline void
+oo_p_dllink_add(ci_netif* ni, struct oo_p_dllink_state list,
+                struct oo_p_dllink_state link)
+{
+  __oo_p_dllink_add(ni, list, oo_p_dllink_statep(ni, list.l->next), link);
+}
+
+static inline void
+oo_p_dllink_add_tail(ci_netif* ni, struct oo_p_dllink_state list,
+                struct oo_p_dllink_state link)
+{
+  __oo_p_dllink_add(ni, oo_p_dllink_statep(ni, list.l->prev), list, link);
+}
+
+static inline bool
+oo_p_dllink_is_empty(ci_netif* ni, struct oo_p_dllink_state list)
+{
+  return list.l->next == list.p;
+}
+
+static inline void
+oo_p_dllink_assert_empty(ci_netif* ni, struct oo_p_dllink_state l,
+                         const char* file, int line)
+{
+  _ci_assert_equal(l.l->next, l.p, file, line);
+  _ci_assert_equal(l.l->prev, l.p, file, line);
+}
+#define OO_P_DLLINK_ASSERT_EMPTY(ni, l) \
+  oo_p_dllink_assert_empty(ni, l, __FILE__, __LINE__)
+#define OO_P_DLLINK_ASSERT_EMPTY_SB(ni, sb, l) \
+  OO_P_DLLINK_ASSERT_EMPTY(ni, oo_p_dllink_sb(ni, sb, l))
+
+
+/* Iterate through the list */
+#define oo_p_dllink_for_each(ni, link, list) \
+  for( link = oo_p_dllink_statep(ni, list.l->next);  \
+       link.l != list.l;                            \
+       link = oo_p_dllink_statep(ni, link.l->next) )
+
+/* Iterate through the list, protecting against removal */
+#define oo_p_dllink_for_each_safe(ni, link, n, list) \
+  for( link = oo_p_dllink_statep(ni, list.l->next),  \
+       n = oo_p_dllink_statep(ni, link.l->next);     \
+       link.l != list.l;                            \
+       link = n, n = oo_p_dllink_statep(ni, link.l->next) )
+
+#endif /* OO_P_DLLIST_NO_CODE */

src/lib/efthrm/tcp_helper_endpoint_move.c
@@ -241,6 +241,7 @@ int efab_file_move_to_alien_stack(ci_private_t *priv, ci_netif *alien_ni,
 #if CI_CFG_FD_CACHING
   oo_p sp;
 #endif
+  struct oo_p_dllink_state link;
 
   OO_DEBUG_TCPH(ci_log("%s: move %d:%d to %d", __func__,
                        old_thr->id, priv->sock_id, new_thr->id));
@@ -520,7 +521,9 @@ int efab_file_move_to_alien_stack(ci_private_t *priv, ci_netif *alien_ni,
    * we update all file descriptors which reference it. */
   old_s->b.sb_flags |= CI_SB_FLAG_MOVED;
   ci_ni_dllist_remove(&old_thr->netif, &old_s->reap_link);
-  ci_ni_dllist_remove_safe(&old_thr->netif, &old_s->b.post_poll_link);
+
+  link = oo_p_dllink_sb(&old_thr->netif, &old_s->b, &old_s->b.post_poll_link);
+  oo_p_dllink_del_init(&old_thr->netif, link);
 
   /* Old stack can be unlocked */
   ci_netif_unlock(&old_thr->netif);

src/lib/efthrm/tcp_helper_resource.c
@@ -7347,22 +7347,25 @@ wakeup_post_poll_list(tcp_helper_resource_t* thr)
   ci_netif* ni = &thr->netif;
   tcp_helper_endpoint_t* ep;
   int n = ni->ep_tbl_n;
-  ci_ni_dllist_link* lnk;
+  struct oo_p_dllink_state post_poll_list =
+                           oo_p_dllink_ptr(ni, &ni->state->post_poll_list);
   citp_waitable* w;
 #if CI_CFG_EPOLL3
   int tmp;
 #endif
 
-  LOG_TV(if( ci_ni_dllist_is_empty(ni, &ni->state->post_poll_list) )
+  LOG_TV(if( oo_p_dllink_is_empty(ni, post_poll_list) )
            ci_log("netif_lock_callback: need_wake but empty"));
 
   /* [n] ensures the loop will terminate in reasonable time no matter how
   ** badly u/l behaves.
   */
-  while( n-- > 0 && ci_ni_dllist_not_empty(ni, &ni->state->post_poll_list) ) {
-    lnk = ci_ni_dllist_head(ni, &ni->state->post_poll_list);
-    w = CI_CONTAINER(citp_waitable, post_poll_link, lnk);
-    ci_ni_dllist_remove_safe(ni, &w->post_poll_link);
+  while( n-- > 0 && ! oo_p_dllink_is_empty(ni, post_poll_list) ) {
+    struct oo_p_dllink_state lnk =
+                            oo_p_dllink_statep(ni, post_poll_list.l->next);
+    oo_p_dllink_del_init(ni, lnk);
+    w = CI_CONTAINER(citp_waitable, post_poll_link, lnk.l);
+
     ep = ci_netif_get_valid_ep(ni, W_SP(w));
     tcp_helper_endpoint_wakeup(thr, ep);
   }

commit 30b3b0d9a2baf5601610543d47fa9ca3194fbe1c

ON-12686: fix ulhelper build
Ulhelper build is runtime-broken, but let's keep it buildable.

Fixes: efcdd869862 ON-12686: reader-friendly oo_p_dllink instead of ci_ni_dllist_t

src/lib/transport/ip/netif.c
@@ -1126,7 +1126,7 @@ ci_uint64 ci_netif_unlock_slow_common(ci_netif* ni, ci_uint64 lock_val,
     oo_p_dllink_for_each_safe(ni, lnk, tmp_lnk, post_poll_list) {
       oo_p_dllink_del_init(ni, lnk);
 
-      w = CI_CONTAINER(citp_waitable, post_poll_link, lnk);
+      w = CI_CONTAINER(citp_waitable, post_poll_link, lnk.l);
       eps[op.eps_num++] = w->bufid;
 
       /* Todo: we'd better allocate larger eps and wake up all