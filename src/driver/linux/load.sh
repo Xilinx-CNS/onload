Xilinx-CNS/onload

commit 264f287198859067bfb6ea2e2df76fc622696eec

ON-12686: load.sh: do not set cplane_track_xdp for rhel7

src/driver/linux/load.sh
@@ -196,7 +196,13 @@ doonload () {
   echo "ONLOAD_OPT is $ONLOAD_OPT"
   $PROBE_CP_SERVER_PATH && \
     O_MOD_ARGS="cplane_server_path=$(get_cp_server_path) ${O_MOD_ARGS}"
-  O_MOD_ARGS="cplane_track_xdp=yes ${O_MOD_ARGS}"
+
+  # For developers we'd like to set cplane_track_xdp on all the kernels
+  # which support it.  Unfortunately RHEL7 (linux-3.10) is unable to ignore
+  # an unknown parameter.
+  if nm onload.ko |grep -q -w cplane_track_xdp; then
+    O_MOD_ARGS="cplane_track_xdp=yes ${O_MOD_ARGS}"
+  fi
   loadmod onload $O_MOD_ARGS
   $LOAD_CONFIG && doonloadconfig
 }

commit 7a482029287b0a4e0383682adabe54a7d1d755cd

ON-12686: load.sh: fixup for running from another directory
Fixes: 264f2871988 ON-12686: load.sh: do not set cplane_track_xdp for rhel7

src/driver/linux/load.sh
@@ -200,7 +200,7 @@ doonload () {
   # For developers we'd like to set cplane_track_xdp on all the kernels
   # which support it.  Unfortunately RHEL7 (linux-3.10) is unable to ignore
   # an unknown parameter.
-  if nm onload.ko |grep -q -w cplane_track_xdp; then
+  if nm $DIR/onload.ko |grep -q -w cplane_track_xdp; then
     O_MOD_ARGS="cplane_track_xdp=yes ${O_MOD_ARGS}"
   fi
   loadmod onload $O_MOD_ARGS