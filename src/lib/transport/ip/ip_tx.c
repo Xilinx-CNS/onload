Xilinx-CNS/onload

commit 1db3b135009457691ec7c0bd951aed69680ed3d9

ON-12686: convert deferred packets to oo_p_dllink

src/lib/transport/ip/ip_tx.c
@@ -60,7 +60,9 @@ void ci_ip_send_pkt_defer(ci_netif* ni, const struct oo_sock_cplane* sock_cp,
                           const ci_ip_cached_hdrs *ipcache)
 {
   struct oo_deferred_pkt* dpkt;
-  ci_ni_dllist_link* lnk;
+  struct oo_p_dllink_state free_list =
+                oo_p_dllink_ptr(ni, &ni->state->deferred_list_free);
+  struct oo_p_dllink_state lnk;
   ci_assert_equal(retrieve_rc, retrrc_nomac);
 
   /* The upper layers think that the packet is in-flight and the NIC owns
@@ -71,18 +73,19 @@ void ci_ip_send_pkt_defer(ci_netif* ni, const struct oo_sock_cplane* sock_cp,
    * for TCP code which tries to retransmit packets. */
   pkt->flags |= CI_PKT_FLAG_TX_PENDING;
 
-  if( ci_ni_dllist_is_empty(ni, &ni->state->deferred_list_free) ) {
+  if( oo_p_dllink_is_empty(ni, free_list) ) {
     CITP_STATS_NETIF_INC(ni, tx_defer_pkt_drop_limited);
     cicp_pkt_complete_fake(ni, pkt);
     return;
   }
-  lnk = ci_ni_dllist_pop(ni, &ni->state->deferred_list_free);
+  lnk = oo_p_dllink_statep(ni, free_list.l->next);
+  oo_p_dllink_del(ni, lnk);
 
   /* Ensure the pkt is ready to send */
   ci_assert_equal(pkt->intf_i, ipcache->intf_i);
 
   /* Store all the data in the deferred packets queue */
-  dpkt = CI_CONTAINER(struct oo_deferred_pkt, link, lnk);
+  dpkt = CI_CONTAINER(struct oo_deferred_pkt, link, lnk.l);
   dpkt->pkt_id = pkt->pp;
   dpkt->src = ipcache_laddr(ipcache);
   if( ni->cplane_init_net != NULL && sock_cp != NULL &&
@@ -114,12 +117,12 @@ void ci_ip_send_pkt_defer(ci_netif* ni, const struct oo_sock_cplane* sock_cp,
   /* We do not have the MAC table available for Onload, so we use the FWD
    * cache instead.  Kick off next hop resolution. */
   if( oo_deferred_send_one(ni, dpkt) ) {
-    ci_ni_dllist_put(ni, &ni->state->deferred_list_free, &dpkt->link);
+    oo_p_dllink_add(ni, free_list, lnk);
     CITP_STATS_NETIF_INC(ni, tx_defer_pkt_fast);
     return;
   }
 
-  ci_ni_dllist_put(ni, &ni->state->deferred_list, &dpkt->link);
+  oo_p_dllink_add(ni, oo_p_dllink_ptr(ni, &ni->state->deferred_list), lnk);
   ef_eplock_holder_set_flag(&ni->state->lock,
                             CI_EPLOCK_NETIF_HAS_DEFERRED_PKTS);
   CITP_STATS_NETIF_INC(ni, tx_defer_pkt);

src/include/ci/internal/ip_shared_types.h
@@ -890,7 +890,7 @@ struct oo_deferred_pkt {
 #define OO_DEFERRED_FLAG_FIRST        2
 
   ci_iptime_t ts;    /* Timestamp when added to the queue */
-  ci_ni_dllist_link link; /* Link */
+  struct oo_p_dllink link; /* Link */
 };
 
 
@@ -1476,8 +1476,8 @@ struct ci_netif_state_s {
   ci_int32              reserved_pktbufs;
 
   /* Deferred packets, because of nomac. */
-  ci_ni_dllist_t deferred_list;
-  ci_ni_dllist_t deferred_list_free;
+  struct oo_p_dllink deferred_list;
+  struct oo_p_dllink deferred_list_free;
 
 
   /* events are in shared state for poll_in_kernel mode,

src/lib/transport/ip/cplane_ops.c
@@ -609,15 +609,16 @@ int oo_deferred_send_one(ci_netif *ni, struct oo_deferred_pkt* dpkt)
 int oo_deferred_send(ci_netif *ni)
 {
   int ret = 1;
-  ci_ni_dllist_link* l = ci_ni_dllist_start(ni, &ni->state->deferred_list);
+  struct oo_p_dllink_state deferred_list =
+                    oo_p_dllink_ptr(ni, &ni->state->deferred_list);
+  struct oo_p_dllink_state l, tmp;
 
   ci_assert(ci_netif_is_locked(ni));
 
-  while( l != ci_ni_dllist_end(ni, &ni->state->deferred_list) ) {
+  oo_p_dllink_for_each_safe(ni, l, tmp, deferred_list) {
     struct oo_deferred_pkt* dpkt = CI_CONTAINER(struct oo_deferred_pkt,
-                                                link, l);
+                                                link, l.l);
     int handled = oo_deferred_send_one(ni, dpkt);
-    ci_ni_dllist_iter(ni, l);
 
     if( handled ||
         TIME_GT(ci_ip_time_now(ni),
@@ -627,8 +628,9 @@ int oo_deferred_send(ci_netif *ni)
         CITP_STATS_NETIF_INC(ni, tx_defer_pkt_drop_timeout);
         cicp_pkt_complete_fake(ni, PKT_CHK(ni, dpkt->pkt_id));
       }
-      ci_ni_dllist_remove(ni, &dpkt->link);
-      ci_ni_dllist_put(ni, &ni->state->deferred_list_free, &dpkt->link);
+      oo_p_dllink_del(ni, l);
+      oo_p_dllink_add(ni, oo_p_dllink_ptr(ni,
+                                &ni->state->deferred_list_free), l);
     }
     else {
       ret = 0;

src/lib/transport/ip/netif_dtor.c
@@ -9,12 +9,12 @@
 void oo_deferred_free(ci_netif *ni)
 {
   CI_DEBUG(int n = 0;)
-  ci_ni_dllist_link* l = ci_ni_dllist_start(ni, &ni->state->deferred_list);
+  struct oo_p_dllink_state l;
 
-  while( l != ci_ni_dllist_end(ni, &ni->state->deferred_list) ) {
+  oo_p_dllink_for_each(ni, l,
+                       oo_p_dllink_ptr(ni, &ni->state->deferred_list)) {
     struct oo_deferred_pkt* dpkt = CI_CONTAINER(struct oo_deferred_pkt,
-                                                link, l);
-    ci_ni_dllist_iter(ni, l);
+                                                link, l.l);
 
     cicp_pkt_complete_fake(ni, PKT_CHK(ni, dpkt->pkt_id));
     CI_DEBUG(n++;)

src/lib/transport/ip/netif_init.c
@@ -88,15 +88,15 @@ void ci_netif_state_init(ci_netif* ni, int cpu_khz, const char* name)
   nis->nonb_pkt_pool = CI_ILL_END;
 
   /* Deferred packets */
-  ci_ni_dllist_init(ni, &nis->deferred_list,
-                    oo_ptr_to_statep(ni, &nis->deferred_list), "dfp");
-  ci_ni_dllist_init(ni, &nis->deferred_list_free,
-                    oo_ptr_to_statep(ni, &nis->deferred_list_free), "dfp");
+  list = oo_p_dllink_ptr(ni, &nis->deferred_list);
+  oo_p_dllink_init(ni, list);
+  list = oo_p_dllink_ptr(ni, &nis->deferred_list_free);
+  oo_p_dllink_init(ni, list);
   for( i = 0; i < NI_OPTS(ni).defer_arp_pkts; i++ ) {
-    ci_ni_dllist_link_init(ni, &ni->deferred_pkts[i].link,
-                           oo_ptr_to_statep(ni, &ni->deferred_pkts[i].link),
-                           "dfp");
-    ci_ni_dllist_put(ni, &nis->deferred_list_free, &ni->deferred_pkts[i].link);
+    struct oo_p_dllink_state link =
+                oo_p_dllink_ptr(ni, &ni->deferred_pkts[i].link);
+    oo_p_dllink_init(ni, link);
+    oo_p_dllink_add(ni, list, link);
   }
 
   ci_netif_filter_init(ni, ci_log2_le(ci_netif_filter_table_size(ni)));

src/tools/onload_remote_monitor/ftl_defs.h
@@ -519,8 +519,8 @@ typedef struct oo_p_dllink oo_p_dllink_t;
   FTL_TFIELD_INT(ctx, ci_int32, mem_pressure_pkt_pool_n, ORM_OUTPUT_STACK) \
   FTL_TFIELD_INT(ctx, ci_int32, n_async_pkts, ORM_OUTPUT_STACK)           \
   FTL_TFIELD_INT(ctx, ci_int32, reserved_pktbufs, ORM_OUTPUT_STACK)       \
-  FTL_TFIELD_STRUCT(ctx, ci_ni_dllist_t, deferred_list, ORM_OUTPUT_EXTRA) \
-  FTL_TFIELD_STRUCT(ctx, ci_ni_dllist_t, deferred_list_free, ORM_OUTPUT_EXTRA) \
+  FTL_TFIELD_STRUCT(ctx, oo_p_dllink_t, deferred_list, ORM_OUTPUT_EXTRA) \
+  FTL_TFIELD_STRUCT(ctx, oo_p_dllink_t, deferred_list_free, ORM_OUTPUT_EXTRA) \
   FTL_TFIELD_INT(ctx, ci_uint64, nonb_pkt_pool, ORM_OUTPUT_STACK)         \
   FTL_TFIELD_STRUCT(ctx, ci_netif_ipid_cb_t, ipid, ORM_OUTPUT_EXTRA) \
   ON_CI_CFG_TCP_SHARED_LOCAL_PORTS(                                       \

commit ca90adc0f07a7b662fb4c6f3131eec817d027f5d

ON-12686: send_pkt_defer: keep the order of deferred packets
Fixes: 1db3b135009 ON-12686: convert deferred packets to oo_p_dllink

src/lib/transport/ip/ip_tx.c
@@ -122,7 +122,7 @@ void ci_ip_send_pkt_defer(ci_netif* ni, const struct oo_sock_cplane* sock_cp,
     return;
   }
 
-  oo_p_dllink_add(ni, oo_p_dllink_ptr(ni, &ni->state->deferred_list), lnk);
+  oo_p_dllink_add_tail(ni, oo_p_dllink_ptr(ni, &ni->state->deferred_list), lnk);
   ef_eplock_holder_set_flag(&ni->state->lock,
                             CI_EPLOCK_NETIF_HAS_DEFERRED_PKTS);
   CITP_STATS_NETIF_INC(ni, tx_defer_pkt);
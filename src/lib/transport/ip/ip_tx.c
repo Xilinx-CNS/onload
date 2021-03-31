ca90adc0f07a7b662fb4c6f3131eec817d027f5d
Alexandra Kossovsky <Alexandra.Kossovsky@oktetlabs.ru>
2021-03-31 17:27:51 +0300

    ON-12686: send_pkt_defer: keep the order of deferred packets
    Fixes: 1db3b135009 ON-12686: convert deferred packets to oo_p_dllink

--- src/lib/transport/ip/ip_tx.c
+++ src/lib/transport/ip/ip_tx.c
@@ -122,7 +122,7 @@ void ci_ip_send_pkt_defer(ci_netif* ni, const struct oo_sock_cplane* sock_cp,
     return;
   }
 
-  oo_p_dllink_add(ni, oo_p_dllink_ptr(ni, &ni->state->deferred_list), lnk);
+  oo_p_dllink_add_tail(ni, oo_p_dllink_ptr(ni, &ni->state->deferred_list), lnk);
   ef_eplock_holder_set_flag(&ni->state->lock,
                             CI_EPLOCK_NETIF_HAS_DEFERRED_PKTS);
   CITP_STATS_NETIF_INC(ni, tx_defer_pkt);
