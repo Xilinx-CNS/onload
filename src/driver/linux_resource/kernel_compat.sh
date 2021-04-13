Xilinx-CNS/onload

commit 8c25635cadca394bf20d850a0fa5ad86644b9d19

ON-13089: compat for RHEL8.4 with half of sockptr_t

src/driver/linux_resource/kernel_compat.sh
@@ -126,7 +126,7 @@ EFRM_HAVE_MMAP_LOCK_WRAPPERS		file	include/linux/mmap_lock.h
 EFRM_HAVE_SOCK_BINDTOINDEX		symbol	sock_bindtoindex	include/net/sock.h
 
 EFRM_MSGHDR_HAS_MSG_CONTROL_USER	member	struct_msghdr	msg_control_user	include/linux/socket.h
-EFRM_HAS_SOCKPTR		symbol	KERNEL_SOCKPTR	include/linux/sockptr.h
+EFRM_HAS_SOCKPTR		memtype	struct_proto_ops	setsockopt	include/linux/net.h	int(*)((struct socket *sock, int level, int optname, sockptr_t optval, unsigned int optlen)
 
 EFRM_SYS_CLOSE_EXPORTED 	export	sys_close	include/linux/syscalls.h	fs/open.c
 EFRM_CLOSE_FD_EXPORTED		export	close_fd	include/linux/fdtable.h	fs/file.c

src/include/ci/driver/kernel_compat.h
@@ -53,6 +53,7 @@
 #include <linux/syscalls.h>
 #include <linux/fdtable.h>
 #include <asm/syscall.h>
+#include <net/sock.h>
 
 #include <driver/linux_resource/autocompat.h>
 

commit aa2baf218207d569ef128f18932a023085c3ba00

ON-13089: typo fixup in kernel_compat.sh
Fixes: 8c25635cadc ON-13089: compat for RHEL8.4 with half of sockptr_t

src/driver/linux_resource/kernel_compat.sh
@@ -126,7 +126,7 @@ EFRM_HAVE_MMAP_LOCK_WRAPPERS		file	include/linux/mmap_lock.h
 EFRM_HAVE_SOCK_BINDTOINDEX		symbol	sock_bindtoindex	include/net/sock.h
 
 EFRM_MSGHDR_HAS_MSG_CONTROL_USER	member	struct_msghdr	msg_control_user	include/linux/socket.h
-EFRM_HAS_SOCKPTR		memtype	struct_proto_ops	setsockopt	include/linux/net.h	int(*)((struct socket *sock, int level, int optname, sockptr_t optval, unsigned int optlen)
+EFRM_HAS_SOCKPTR		memtype	struct_proto_ops	setsockopt	include/linux/net.h	int(*)(struct socket *sock, int level, int optname, sockptr_t optval, unsigned int optlen)
 
 EFRM_SYS_CLOSE_EXPORTED 	export	sys_close	include/linux/syscalls.h	fs/open.c
 EFRM_CLOSE_FD_EXPORTED		export	close_fd	include/linux/fdtable.h	fs/file.c