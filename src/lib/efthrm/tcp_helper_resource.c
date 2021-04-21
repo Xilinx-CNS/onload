Xilinx-CNS/onload

commit 7db428778e69bbd879c87bd70a210654fb471415

ON-12686: convert active_wild buffers to oo_p_dllink

src/lib/efthrm/tcp_helper_resource.c
@@ -2185,9 +2185,9 @@ allocate_netif_resources(ci_resource_onload_alloc_t* alloc,
                     PKTS_PER_SET / (PAGE_SIZE / CI_CFG_PKT_BUF_SIZE) *
                     sizeof(ef_addr);
   sz += dma_addrs_bytes;
-  sz = CI_ROUND_UP(sz, __alignof__(ci_ni_dllist_t));
+  sz = CI_ROUND_UP(sz, __alignof__(struct oo_p_dllink));
 #if CI_CFG_TCP_SHARED_LOCAL_PORTS
-  sz += sizeof(ci_ni_dllist_t) * no_active_wild_table_entries *
+  sz += sizeof(struct oo_p_dllink) * no_active_wild_table_entries *
         no_active_wild_pools;
 #endif
   sz = CI_ROUND_UP(sz, __alignof__(ci_tcp_prev_seq_t));
@@ -2292,12 +2292,12 @@ allocate_netif_resources(ci_resource_onload_alloc_t* alloc,
   ns_ofs += dma_addrs_bytes;
 
 #if CI_CFG_TCP_SHARED_LOCAL_PORTS
-  ns_ofs = CI_ROUND_UP(ns_ofs, __alignof__(ci_ni_dllist_t));
+  ns_ofs = CI_ROUND_UP(ns_ofs, __alignof__(struct oo_p_dllink));
   ns->active_wild_ofs = ns_ofs;
   ns->active_wild_table_entries_n = no_active_wild_table_entries;
   ns->active_wild_pools_n = no_active_wild_pools;
 
-  ns_ofs += (sizeof(ci_ni_dllist_t) * ns->active_wild_table_entries_n *
+  ns_ofs += (sizeof(struct oo_p_dllink) * ns->active_wild_table_entries_n *
             ns->active_wild_pools_n);
 #endif
 
@@ -3442,11 +3442,10 @@ tcp_helper_alloc_to_aw_pool(tcp_helper_resource_t* rs,
                             ci_addr_t laddr,
                             struct efab_ephemeral_port_keeper* port)
 {
-  int rc;
   ci_netif* ni = &rs->netif;
   int idx;
   ci_active_wild* aw = tcp_helper_alloc_active_wild(rs, port);
-  ci_ni_dllist_t* list;
+  struct oo_p_dllink_state list;
 
   OO_DEBUG_TCPH(ci_log("%s [%u]", __FUNCTION__, rs->id));
 
@@ -3471,15 +3470,15 @@ tcp_helper_alloc_to_aw_pool(tcp_helper_resource_t* rs,
                                       addr_any, 0) : 0;
   idx &= ni->state->active_wild_pools_n - 1;
 
-  rc = ci_netif_get_active_wild_list(ni, idx, laddr, &list);
-  if( rc < 0 ) {
+  list = ci_netif_get_active_wild_list(ni, idx, laddr);
+  if( list.p == OO_P_NULL ) {
     aw->s.b.sb_aflags |= CI_SB_AFLAG_ORPHAN;
     efab_tcp_helper_drop_os_socket(rs, ci_netif_ep_get(ni, W_SP(&aw->s.b)));
     citp_waitable_obj_free(ni, &aw->s.b);
-    return rc;
+    return 0;
   }
 
-  ci_ni_dllist_push(ni, list, &aw->pool_link);
+  oo_p_dllink_add(ni, list, oo_p_dllink_sb(ni, &aw->s.b, &aw->pool_link));
   ni->state->active_wild_n++;
 
   return 0;
@@ -3531,9 +3530,9 @@ tcp_helper_alloc_list_to_aw_pool(tcp_helper_resource_t* rs,
 
   if( ! NI_OPTS(ni).tcp_shared_local_ports_per_ip )
     for( i = 0; i < ni->state->active_wild_pools_n; i++ ) {
-      ci_ni_dllist_t* list;
-      if( ci_netif_get_active_wild_list(ni, i, addr_any, &list) >= 0 &&
-          ci_ni_dllist_is_empty(ni, list) ) {
+      struct oo_p_dllink_state list =
+                    ci_netif_get_active_wild_list(ni, i, addr_any);
+      if( list.p != OO_P_NULL && oo_p_dllink_is_empty(ni, list) ) {
         NI_LOG(&rs->netif, RESOURCE_WARNINGS, "%s: Current shared local ports "
                "don't provide coverage of all possible connections.  Allocate "
                "more to improve coverage.", __FUNCTION__);

src/include/ci/internal/ip.h
@@ -1853,9 +1853,8 @@ extern int ci_netif_active_wild_nic_hash(ci_netif *ni,
                                          ci_addr_t laddr, ci_uint16 lport,
                                          ci_addr_t raddr, ci_uint16 rport);
 
-extern int
-ci_netif_get_active_wild_list(ci_netif* ni, int aw_pool,
-                              ci_addr_t laddr, ci_ni_dllist_t** list_out);
+extern struct oo_p_dllink_state
+ci_netif_get_active_wild_list(ci_netif* ni, int aw_pool, ci_addr_t laddr);
 #endif
 
 /* Bind RX of socket to given interface.  Used by implementation of

src/include/ci/internal/ip_shared_types.h
@@ -2363,7 +2363,7 @@ struct ci_active_wild_s {
   ci_sock_cmn           s;
   ci_addr_t             last_laddr;
   ci_addr_t             last_raddr;
-  ci_ni_dllist_link     pool_link;
+  struct oo_p_dllink    pool_link;
   ci_iptime_t           expiry;
   ci_uint32             last_rport;
 };

src/include/ci/internal/ip_types.h
@@ -181,7 +181,7 @@ struct ci_netif_s {
   ci_ip6_netif_filter_table* ip6_filter_table;
 #endif
 #if CI_CFG_TCP_SHARED_LOCAL_PORTS
-  ci_ni_dllist_t*      active_wild_table;
+  struct oo_p_dllink* active_wild_table;
 #endif
   ci_tcp_prev_seq_t*   seq_table;
 

src/lib/transport/ip/active_wild.c
@@ -8,8 +8,6 @@
 #if CI_CFG_TCP_SHARED_LOCAL_PORTS
 static void ci_active_wild_state_init(ci_netif* netif, ci_active_wild* aw)
 {
-  oo_p p;
-
   ci_sock_cmn_init(netif, &aw->s, 1);
   aw->s.b.state = CI_TCP_STATE_ACTIVE_WILD;
   aw->s.b.sb_aflags = 0;
@@ -21,10 +19,7 @@ static void ci_active_wild_state_init(ci_netif* netif, ci_active_wild* aw)
   ci_sock_set_laddr_port(&aw->s, addr_any, 0);
   ci_sock_set_raddr_port(&aw->s, addr_any, 0);
 
-  p = TS_OFF(netif, aw);
-  OO_P_ADD(p, CI_MEMBER_OFFSET(ci_active_wild, pool_link));
-  ci_ni_dllist_link_init(netif, &aw->pool_link, p, "pool");
-  ci_ni_dllist_self_link(netif, &aw->pool_link);
+  oo_p_dllink_init(netif, oo_p_dllink_sb(netif, &aw->s.b, &aw->pool_link));
 
   aw->expiry = ci_ip_time_now(netif);
   aw->last_laddr = addr_any;

src/lib/transport/ip/netif.c
@@ -1537,7 +1537,7 @@ int ci_netif_active_wild_nic_hash(ci_netif *ni,
 
 
 /* Returns the hash table of active wilds for the specified pool. */
-ci_inline ci_ni_dllist_t*
+ci_inline struct oo_p_dllink*
 ci_netif_active_wild_pool_table(ci_netif* ni, int aw_pool)
 {
   ci_assert(ci_netif_is_locked(ni));
@@ -1554,11 +1554,13 @@ ci_netif_active_wild_pool_table(ci_netif* ni, int aw_pool)
  * not become 'owned' by that address until it becomes non-empty.  This means
  * that the stack lock must not be dropped between retrieving the address of a
  * list using this function and ceasing to use the returned pointer to that
- * list. */
-int ci_netif_get_active_wild_list(ci_netif* ni, int aw_pool, ci_addr_t laddr,
-                                  ci_ni_dllist_t** list_out)
+ * list.
+ * Returns link state with .p==OO_P_NULL in case of failure. */
+struct oo_p_dllink_state
+ci_netif_get_active_wild_list(ci_netif* ni, int aw_pool, ci_addr_t laddr)
 {
-  ci_ni_dllist_t* table = ci_netif_active_wild_pool_table(ni, aw_pool);
+  struct oo_p_dllink* table = ci_netif_active_wild_pool_table(ni, aw_pool);
+  struct oo_p_dllink_state list;
   ci_uint32 bucket, hash1, hash2;
 
   ci_assert(ci_netif_is_locked(ni));
@@ -1566,26 +1568,27 @@ int ci_netif_get_active_wild_list(ci_netif* ni, int aw_pool, ci_addr_t laddr,
   ci_addr_simple_hash(laddr, ni->state->active_wild_table_entries_n,
                       &hash1, &hash2);
   bucket = hash1;
+  list.p = OO_P_NULL;
 
   do {
     ci_active_wild* aw;
-    ci_ni_dllist_link* link;
+    struct oo_p_dllink_state link;
 
-    *list_out = &table[bucket];
+    list = oo_p_dllink_ptr(ni, &table[bucket]);
 
     /* If we've found an empty list, it means there's no entry in the table for
      * the specified IP address.  This empty list is also at the correct
      * location for the insertion of a new list for that IP address, and so we
      * return it. */
-    if( ci_ni_dllist_is_empty(ni, *list_out) )
-      return 0;
+    if( oo_p_dllink_is_empty(ni, list) )
+      return list;
 
     /* The list is non-empty, so look at one of the active wilds contained in
      * it in order to determine and check the local address. */
-    link = ci_ni_dllist_head(ni, *list_out);
-    aw = CI_CONTAINER(ci_active_wild, pool_link, link);
+    link = oo_p_dllink_statep(ni, list.l->next);
+    aw = CI_CONTAINER(ci_active_wild, pool_link, link.l);
     if( CI_IPX_ADDR_EQ(sock_ipx_laddr(&aw->s), laddr) )
-      return 0;
+      return list;
 
     /* This list is for the wrong IP address, so advance to the next bucket. */
     bucket = (bucket + hash2) & (ni->state->active_wild_table_entries_n - 1);
@@ -1595,7 +1598,7 @@ int ci_netif_get_active_wild_list(ci_netif* ni, int aw_pool, ci_addr_t laddr,
               "No space in active wild table %d for local address "
               IPX_FMT, aw_pool, IPX_ARG(AF_IP_L3(laddr)));
 
-  return -ENOSPC;
+  return list;
 }
 
 
@@ -1689,12 +1692,10 @@ static oo_sp __ci_netif_active_wild_pool_get(ci_netif* ni, int aw_pool,
   ci_uint16 lport;
   ci_addr_t laddr_aw =
             NI_OPTS(ni).tcp_shared_local_ports_per_ip ? laddr : addr_any;
-  int rc;
   int af_space = AF_SPACE_FLAG_IP4;
   oo_sp sp;
-  ci_ni_dllist_t* list;
-  ci_ni_dllist_link* link = NULL;
-  ci_ni_dllist_link* tail;
+  struct oo_p_dllink_state list;
+  struct oo_p_dllink_state link, tmp;
 
   ci_assert(ci_netif_is_locked(ni));
 
@@ -1707,23 +1708,22 @@ static oo_sp __ci_netif_active_wild_pool_get(ci_netif* ni, int aw_pool,
 
   *prev_seq_out = 0;
 
-  rc = ci_netif_get_active_wild_list(ni, aw_pool, laddr_aw, &list);
-  if( rc < 0 )
+  list = ci_netif_get_active_wild_list(ni, aw_pool, laddr_aw);
+  if( list.p == OO_P_NULL )
     return OO_SP_NULL;
 
   /* This can happen if active wilds are configured, but we failed to allocate
    * any at stack creation time, for example because there were no filters
    * available, or if none of them give a valid hash for this 4-tuple.
    */
-  if( ci_ni_dllist_is_empty(ni, list) )
+  if( oo_p_dllink_is_empty(ni, list) )
     return OO_SP_NULL;
 
-  tail = ci_ni_dllist_tail(ni, list);
-  while( link != tail ) {
-    link = ci_ni_dllist_pop(ni, list);
-    ci_ni_dllist_push_tail(ni, list, link);
+  oo_p_dllink_for_each_safe(ni, link, tmp, list) {
+    oo_p_dllink_del(ni, link);
+    oo_p_dllink_add(ni, list, link);
 
-    aw = CI_CONTAINER(ci_active_wild, pool_link, link);
+    aw = CI_CONTAINER(ci_active_wild, pool_link, link.l);
 
     lport = sock_lport_be16(&aw->s);
 

src/lib/transport/ip/netif_init.c
@@ -216,9 +216,7 @@ void ci_netif_state_init(ci_netif* ni, int cpu_khz, const char* name)
   for( i = 0;
        i < nis->active_wild_table_entries_n * nis->active_wild_pools_n;
        ++i ) {
-    ci_ni_dllist_init(ni, &ni->active_wild_table[i],
-                      oo_ptr_to_statep(ni, &ni->active_wild_table[i]),
-                      "active_wild");
+    oo_p_dllink_init(ni, oo_p_dllink_ptr(ni, &ni->active_wild_table[i]));
   }
   nis->active_wild_n = 0;
 #endif
@@ -1698,7 +1696,7 @@ static void netif_tcp_helper_build2(ci_netif* ni)
 {
 #if CI_CFG_TCP_SHARED_LOCAL_PORTS
   ni->active_wild_table =
-    (ci_ni_dllist_t*) ((char*) ni->state + ni->state->active_wild_ofs);
+    (struct oo_p_dllink*) ((char*) ni->state + ni->state->active_wild_ofs);
 #endif
   ni->seq_table =
     (ci_tcp_prev_seq_t*) ((char*) ni->state + ni->state->seq_table_ofs);

commit 7bfcc837293a6bf74931e50705f30d29e99713da

ON-12686: fix return value of tcp_helper_alloc_to_aw_pool()
Fixes: 7db428778e ON-12686: convert active_wild buffers to oo_p_dllink

src/lib/efthrm/tcp_helper_resource.c
@@ -3455,7 +3455,7 @@ tcp_helper_alloc_to_aw_pool(tcp_helper_resource_t* rs,
     aw->s.b.sb_aflags |= CI_SB_AFLAG_ORPHAN;
     efab_tcp_helper_drop_os_socket(rs, ci_netif_ep_get(ni, W_SP(&aw->s.b)));
     citp_waitable_obj_free(ni, &aw->s.b);
-    return 0;
+    return -ENOSPC;
   }
 
   oo_p_dllink_add(ni, list, oo_p_dllink_sb(ni, &aw->s.b, &aw->pool_link));