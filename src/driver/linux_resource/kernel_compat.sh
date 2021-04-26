Xilinx-CNS/onload

commit 899999533b48926addca4e7e0b977e10f151fdaa

ON-13121: Use-after-free reading /proc/interrupts

src/lib/efrm/vi_resource_alloc.c
@@ -88,6 +88,7 @@ union vi_attr_u {
 /*** Data definitions ****************************************************/
 
 static const char *q_names[EFHW_N_Q_TYPES] = { "TXQ", "RXQ", "EVQ" };
+static const char default_irq_name[] = "Onload";
 
 struct vi_resource_manager *efrm_vi_manager;
 
@@ -261,9 +262,12 @@ int efrm_vi_rm_alloc_instance(struct efrm_pd *pd,
 static void
 efrm_vi_irq_free(struct efrm_vi *vi)
 {
+	char *name;
 	EFRM_ASSERT(vi);
 
-	free_irq(vi->irq, vi);
+	name = (char*)free_irq(vi->irq, vi);
+	if (name != default_irq_name)
+		kfree(name);
 	tasklet_kill(&vi->tasklet);
 }
 
@@ -1101,16 +1105,22 @@ static int
 efrm_vi_irq_setup(struct efrm_vi *vi, const char *vi_name, unsigned int irq)
 {
 	int rc;
+	const char *name;
 
 	spin_lock_init(&vi->evq_callback_lock);
 
 	/* Enable interrupts */
 	tasklet_init(&vi->tasklet, &efrm_vi_tasklet, (unsigned long)vi);
+	name = kstrdup(vi_name, GFP_KERNEL);
+	if (!name)
+		name = default_irq_name;
 	rc = request_irq(irq, vi_interrupt,
-			 IRQF_SAMPLE_RANDOM, vi_name, vi);
+			 IRQF_SAMPLE_RANDOM, name, vi);
 	if (rc != 0) {
 		EFRM_ERR("failed to request IRQ %d for VI %d",
 			 irq, vi->rs.rs_instance);
+		if (name != default_irq_name)
+			kfree(name);
 	}
 
 	return rc;

commit 6fa880734d135bf3bb7395b71602827785a7e77e

ON-13125: ON-13121: compat for free_irq() for linux<=4.12
Fixes: 899999533b ON-13121: Use-after-free reading /proc/interrupts

src/driver/linux_resource/kernel_compat.sh
@@ -138,6 +138,9 @@ EFRM_FILE_HAS_F_EP	member	struct_file	f_ep	include/linux/fs.h
 EFRM_HAS_LOOKUP_FD_RCU	symbol	lookup_fd_rcu	include/linux/fdtable.h
 
 EFRM_HAS_FLUSH_DELAYED_FPUT	export	flush_delayed_fput	include/linux/file.h	fs/file_table.c
+
+EFRM_IRQ_FREE_RETURNS_NAME	symtype	free_irq	include/linux/interrupt.h void *(unsigned int, void *)
+
 # TODO move onload-related stuff from net kernel_compat
 " | egrep -v -e '^#' -e '^$' | sed 's/[ \t][ \t]*/:/g'
 }

src/include/ci/efrm/vi_resource_manager.h
@@ -162,6 +162,10 @@ struct efrm_vi {
 	unsigned tx_alt_cp;
 	int      tx_alt_num;
 	unsigned tx_alt_ids[EFRM_VI_TX_ALTERNATIVES_MAX];
+
+#ifndef EFRM_IRQ_FREE_RETURNS_NAME
+	const char *irq_name;
+#endif
 };
 
 

src/lib/efrm/vi_resource_alloc.c
@@ -262,10 +262,17 @@ int efrm_vi_rm_alloc_instance(struct efrm_pd *pd,
 static void
 efrm_vi_irq_free(struct efrm_vi *vi)
 {
-	char *name;
+	const char *name;
 	EFRM_ASSERT(vi);
 
-	name = (char*)free_irq(vi->irq, vi);
+	/* linux>=4.13: free_irq() returns name */
+#ifdef EFRM_IRQ_FREE_RETURNS_NAME
+	name = free_irq(vi->irq, vi);
+#else
+	free_irq(vi->irq, vi);
+	name = vi->irq_name;
+	vi->irq_name = NULL;
+#endif
 	if (name != default_irq_name)
 		kfree(name);
 	tasklet_kill(&vi->tasklet);
@@ -1122,6 +1129,9 @@ efrm_vi_irq_setup(struct efrm_vi *vi, const char *vi_name, unsigned int irq)
 		if (name != default_irq_name)
 			kfree(name);
 	}
+#ifndef EFRM_IRQ_FREE_RETURNS_NAME
+	vi->irq_name = name;
+#endif
 
 	return rc;
 }