Xilinx-CNS/onload

commit 4ee25b47847c679a70ea19418ffcf7fbf5a3bf49

VIRTBLK-1079: user_mark field not specified when packets are sent for recycling

src/lib/transport/ip/tcp_rx.c
@@ -1764,6 +1764,10 @@ static bool ci_tcp_rx_plugin_recycle(ci_netif* netif, ci_tcp_state* ts,
                                      ci_ip_pkt_fmt* pkt)
 {
   bool rc;
+  struct ef_vi_tx_extra extra = {
+    .flags = EF_VI_TX_EXTRA_MARK,
+    .mark = ts->plugin_stream_id,
+  };
   /* Smarter retry logic would be better here, in particular we shouldn't
    * keep trying if we reckon the plugin's blocked because of insufficient
    * FPGA DDR. This smarter logic probably belongs near the top of
@@ -1785,7 +1789,7 @@ static bool ci_tcp_rx_plugin_recycle(ci_netif* netif, ci_tcp_state* ts,
   /* Need to use the immediate version to avoid the possibility of putting the
    * packet on the dmaq - we can't do that because we're already using
    * pkt::next for the rob. */
-  rc = ci_netif_send_immediate(netif, pkt);
+  rc = ci_netif_send_immediate(netif, pkt, &extra);
   if( ! rc ) {
     pkt->flags &=~ CI_PKT_FLAG_TX_PENDING;
     ci_netif_pkt_release(netif, pkt);

src/include/ci/internal/ip.h
@@ -509,7 +509,8 @@ ci_inline void ci_netif_send(ci_netif* ni, ci_ip_pkt_fmt* pkt)
   pkt->flags |= CI_PKT_FLAG_TX_PENDING;
   __ci_netif_send(ni, pkt);
 }
-extern bool ci_netif_send_immediate(ci_netif* netif, ci_ip_pkt_fmt* pkt) CI_HF;
+extern bool ci_netif_send_immediate(ci_netif* netif, ci_ip_pkt_fmt* pkt,
+                                    const struct ef_vi_tx_extra* extra) CI_HF;
 extern int ci_netif_rx_post(ci_netif* netif, int nic_index, ef_vi* vi) CI_HF;
 #ifdef __KERNEL__
 extern int  ci_netif_set_rxq_limit(ci_netif*) CI_HF;

src/lib/transport/ip/netif_tx.c
@@ -350,7 +350,8 @@ void __ci_netif_send(ci_netif* netif, ci_ip_pkt_fmt* pkt)
  * low-level function used by VIs which are used for communicating with
  * plugins, where the caller typically has their own reliability policy and
  * hence they don't want automatic handling of it behind the scenes. */
-bool ci_netif_send_immediate(ci_netif* netif, ci_ip_pkt_fmt* pkt)
+bool ci_netif_send_immediate(ci_netif* netif, ci_ip_pkt_fmt* pkt,
+                             const struct ef_vi_tx_extra* extra)
 {
   int intf_i;
   ef_vi* vi;
@@ -377,8 +378,26 @@ bool ci_netif_send_immediate(ci_netif* netif, ci_ip_pkt_fmt* pkt)
 
   iov_len = ci_netif_pkt_to_iovec(netif, pkt, iov,
                                   sizeof(iov) / sizeof(iov[0]));
-  if( ef_vi_transmitv(vi, iov, iov_len, OO_PKT_ID(pkt)) != 0 )
-    return false;
+  if( extra ) {
+    int i;
+    ef_remote_iovec riov[CI_IP_PKT_SEGMENTS_MAX];
+    for( i = 0; i < iov_len; ++i) {
+      riov[i] = (ef_remote_iovec){
+        .iov_base = iov[i].iov_base,
+        .iov_len = iov[i].iov_len,
+        .flags = 0,
+        .addrspace = EF_ADDRSPACE_LOCAL,
+      };
+    }
+    if( ef_vi_transmitv_init_extra(vi, extra, riov, iov_len,
+                                   OO_PKT_ID(pkt)) != 0 )
+      return false;
+    ef_vi_transmit_push(vi);
+  }
+  else {
+    if( ef_vi_transmitv(vi, iov, iov_len, OO_PKT_ID(pkt)) != 0 )
+      return false;
+  }
 
   ___ci_netif_dmaq_insert_prep_pkt(netif, pkt);
   CITP_STATS_NETIF_INC(netif, tx_dma_doorbells);

commit a464b0efb25fb0124fb8ce41f6e6dd4952094891

VIRTBLK-1079: Fix non-cloud build
Broken by 4ee25b4784

src/lib/transport/ip/tcp_rx.c
@@ -1763,7 +1763,8 @@ static int ci_tcp_check_ooo_stripe(ci_netif* netif, ci_tcp_state* ts)
 static bool ci_tcp_rx_plugin_recycle(ci_netif* netif, ci_tcp_state* ts,
                                      ci_ip_pkt_fmt* pkt)
 {
-  bool rc;
+  bool rc = false;
+#if CI_CFG_TCP_OFFLOAD_RECYCLER
   struct ef_vi_tx_extra extra = {
     .flags = EF_VI_TX_EXTRA_MARK,
     .mark = ts->plugin_stream_id,
@@ -1794,6 +1795,7 @@ static bool ci_tcp_rx_plugin_recycle(ci_netif* netif, ci_tcp_state* ts,
     pkt->flags &=~ CI_PKT_FLAG_TX_PENDING;
     ci_netif_pkt_release(netif, pkt);
   }
+#endif
   return rc;
 }
 