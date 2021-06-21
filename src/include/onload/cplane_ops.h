Xilinx-CNS/onload

commit 3d682270f955193396adabd2b4c94ef8639b3566

ON-13275: do not print scary error when prefsrc_as_local
Do not print scary message "laddr not local" when cplane_use_prefsrc_as_local
is set.  This conditon happens in such a mode, and it is handled properly.

src/include/onload/cplane_ops.h
@@ -12,6 +12,8 @@
 
 #ifdef __KERNEL__
 
+#include <onload/cplane_moparam.h>
+
 #define CICP_HANDLE(netif) (&CI_GLOBAL_CPLANE)
 
 #else
@@ -185,13 +187,4 @@ extern int cicp_raw_ip_send(struct oo_cplane_handle* cp, int af,
 #endif
 
 #endif
-
-#ifdef __KERNEL__
-/* Tell Onload what kind of addresses are considered to be "local" */
-extern bool cplane_use_prefsrc_as_local;
-
-/* Do we track XDP programs? */
-extern bool cplane_track_xdp;
-#endif
-
 #endif /* __ONLOAD_CPLANE_OPS_H__ */

src/lib/efthrm/oof_filters_deps.h
@@ -1,0 +1,7 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/* X-SPDX-Copyright-Text: (c) Copyright 2021 Xilinx, Inc. */
+
+#ifndef __OOF_FILTERS_DEPS_H__
+#define __OOF_FILTERS_DEPS_H__
+#include <onload/cplane_moparam.h>
+#endif

src/lib/efthrm/tcp_helper_resource.c
@@ -36,7 +36,7 @@
 #include <onload/oof_onload.h>
 #include <onload/oof_interface.h>
 #include <onload/nic.h>
-#include <onload/cplane_ops.h>
+#include <onload/cplane_moparam.h>
 #include <onload/netif_dtor.h>
 #include <ci/internal/pio_buddy.h>
 #include <onload/tmpl.h>

src/tests/onload/oof/oof_filters_deps.h
@@ -1,0 +1,9 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/* X-SPDX-Copyright-Text: (c) Copyright 2021 Xilinx, Inc. */
+
+#ifndef __OOF_TEST_OOF_FILTERS_DEPS_H__
+#define __OOF_TEST_OOF_FILTERS_DEPS_H__
+
+extern bool cplane_use_prefsrc_as_local;
+
+#endif

src/driver/linux_onload/mmake.mk
@@ -30,7 +30,7 @@ EFTHRM_SRCS	:= eplock_resource_manager.c \
 
 EFTHRM_HDRS	:= oo_hw_filter.h oof_impl.h tcp_filters_internal.h \
 		tcp_helper_resource.h tcp_filters_deps.h oof_tproxy_ipproto.h \
-		oof_onload_types.h tcp_helper_stats_dump.h
+		oof_onload_types.h tcp_helper_stats_dump.h oof_filters_deps.h
 
 ifeq ($(LINUX),1)
 EFTHRM_SRCS	+= tcp_helper_linux.c

src/include/onload/cplane_moparam.h
@@ -1,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* X-SPDX-Copyright-Text: (c) Copyright 2021 Xilinx, Inc. */
+#ifndef __ONLOAD_CPLANE_MODPARAM_H__
+#define __ONLOAD_CPLANE_MODPARAM_H__
+
+/* Tell Onload what kind of addresses are considered to be "local" */
+extern bool cplane_use_prefsrc_as_local;
+
+/* Do we track XDP programs? */
+extern bool cplane_track_xdp;
+
+#endif

src/lib/efthrm/oof_filters.c
@@ -31,6 +31,7 @@
 #include <onload/debug.h>
 #include "oo_hw_filter.h"
 #include "tcp_filters_internal.h"
+#include "oof_filters_deps.h"
 
 #define OOF_SRC_FLAGS_DEFAULT 0
 #define OOF_SRC_FLAGS_DEFAULT_MCAST (OO_HW_SRC_FLAG_LOOPBACK)
@@ -2627,8 +2628,9 @@ __oof_socket_add(struct oof_manager* fm, struct oof_socket* skf, int do_arm_only
         ci_dllist_push(&fm->fm_mcast_laddr_socks, &skf->sf_lp_link);
         return 0;
       }
-      ERR_LOG(FSK_FMT "ERROR: laddr=" IPX_FMT " not local",
-              FSK_PRI_ARGS(skf), IPX_ARG(AF_IP_L3(skf->sf_laddr)));
+      if( ! cplane_use_prefsrc_as_local )
+        ERR_LOG(FSK_FMT "ERROR: laddr=" IPX_FMT " not local",
+                FSK_PRI_ARGS(skf), IPX_ARG(AF_IP_L3(skf->sf_laddr)));
       return -ENOENT;
     }
 

src/tests/onload/oof/cplane.c
@@ -9,6 +9,8 @@
 #include <onload/oof_onload.h>
 #include <arpa/inet.h>
 
+bool cplane_use_prefsrc_as_local = false;
+
 struct ooft_hwport* ooft_alloc_hwport(struct ooft_cplane* cp, struct net* ns,
                                       int vlans, int mcast_replication)
 {

commit a7a9d57ca0923a1ab12adbc2a46a4def37899a9c

ON-13275: rename cplane_moparam.h to cplane_modparam.h
Fixes: 3d682270f9 ON-13275: do not print scary error when prefsrc_as_local

src/include/onload/cplane_ops.h
@@ -12,7 +12,7 @@
 
 #ifdef __KERNEL__
 
-#include <onload/cplane_moparam.h>
+#include <onload/cplane_modparam.h>
 
 #define CICP_HANDLE(netif) (&CI_GLOBAL_CPLANE)
 

src/lib/efthrm/oof_filters_deps.h
@@ -3,5 +3,5 @@
 
 #ifndef __OOF_FILTERS_DEPS_H__
 #define __OOF_FILTERS_DEPS_H__
-#include <onload/cplane_moparam.h>
+#include <onload/cplane_modparam.h>
 #endif

src/lib/efthrm/tcp_helper_resource.c
@@ -36,7 +36,7 @@
 #include <onload/oof_onload.h>
 #include <onload/oof_interface.h>
 #include <onload/nic.h>
-#include <onload/cplane_moparam.h>
+#include <onload/cplane_modparam.h>
 #include <onload/netif_dtor.h>
 #include <ci/internal/pio_buddy.h>
 #include <onload/tmpl.h>