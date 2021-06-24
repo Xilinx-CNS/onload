Xilinx-CNS/onload

commit d56cb83fe31ff6f75a341581d22c09beeb4365d1

ON-13290: ON-13307: dup2: interrupt recv() in other threads
SIGONLOAD is not deferred any more, because it must not be deferred when
it is used for close().  At the same time this signal is used for dup()
to interrupt other threads which are using the same file descriptor, see
bug 28123 / ON-1646.  In such a case it is important to avoid SA_RESTART effect.

This patch ensures that NEED_RESTART flag is removed, and fixes a regression.

Fixes: d02d472e69 ON-13290: do not postpone SIGONLOAD

src/lib/transport/unix/fdtable.c
@@ -105,6 +105,9 @@ static void sighandler_sigonload(int sig, siginfo_t* info, void* context)
    * tgkill() syscall, which uses SI_TKILL code. */
   if( info->si_code < 0 && info->si_code != SI_ONLOAD ) {
     ci_assert_equal(info->si_code, SI_TKILL);
+    /* Ensure that onload_recv() and similar functions are not restarted: */
+    ci_atomic32_and(&citp_signal_get_specific_inited()->c.aflags,
+                    ~OO_SIGNAL_FLAG_NEED_RESTART);
     return;
   }
 

commit 4ebe32c37050b7f203f1186951f8d91352350be4

ON-13290: ON-13307: dup2: interrupt recv() in other threads when spinning
Interrupt spinning thread as well as blocking.

Fixes: d56cb83fe3 ON-13290: ON-13307: dup2: interrupt recv() in other threads
Fixes: d02d472e69 ON-13290: do not postpone SIGONLOAD

src/lib/transport/unix/fdtable.c
@@ -106,6 +106,8 @@ static void sighandler_sigonload(int sig, siginfo_t* info, void* context)
   if( info->si_code < 0 && info->si_code != SI_ONLOAD ) {
     ci_assert_equal(info->si_code, SI_TKILL);
     /* Ensure that onload_recv() and similar functions are not restarted: */
+    ci_atomic32_or(&citp_signal_get_specific_inited()->c.aflags,
+                   OO_SIGNAL_FLAG_HAVE_PENDING);
     ci_atomic32_and(&citp_signal_get_specific_inited()->c.aflags,
                     ~OO_SIGNAL_FLAG_NEED_RESTART);
     return;