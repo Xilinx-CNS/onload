Xilinx-CNS/onload

commit 08b20f01f6d148846cc4db01cb23aaef3b883c6c

#32: avoid efrm_filter_check() for veth interfaces
efrm_filter_check() is part of onload_iptables.  It can't be used with AF_XDP,
so it is safe to skip it if we can't determine the device's name.

src/driver/linux_resource/filter.c
@@ -2202,7 +2202,11 @@ int efrm_filter_insert(struct efrm_client *client,
 	   block it. */
 	net_dev = efhw_nic_get_net_dev(efhw_nic);
 	if( net_dev ) {
-		rc = efrm_filter_check( net_dev->dev.parent, spec );
+		/* AF_XDP does not work with onload_iptables */
+		if( efhw_nic->devtype.arch != EFHW_ARCH_AF_XDP ) {
+			EFRM_ASSERT(net_dev->dev.parent);
+			rc = efrm_filter_check( net_dev->dev.parent, spec );
+		}
 		dev_put(net_dev);
 		if ( rc >= 0 )
 			rc = efhw_nic_filter_insert( efhw_nic, spec, replace );

commit bd85e4f8050aa11ed4fe2f8a9e9a3e726955af6d

#32: fix a warning
Fixes: 08b20f01f6 #32: avoid efrm_filter_check() for veth interfaces

src/driver/linux_resource/filter.c
@@ -2187,7 +2187,7 @@ int efrm_filter_insert(struct efrm_client *client,
 {
 	struct efhw_nic *efhw_nic = efrm_client_get_nic(client);
 	struct net_device* net_dev;
-	int rc;
+	int rc = 0;
 
 #if CI_CFG_IPV6
 	/* FIXME: add IPv6 support to firewall rules (bug 85208) */