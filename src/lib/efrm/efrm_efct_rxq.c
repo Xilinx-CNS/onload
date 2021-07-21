Xilinx-CNS/onload

commit f94526c4738b84cfaf8ef7198c65dd90f92ccb54

ON-13281: mmap support for superbufs themselves

src/lib/efrm/efrm_efct_rxq.c
@@ -5,6 +5,8 @@
 #include <ci/efrm/efct_rxq.h>
 #include <ci/driver/resource/linux_efhw_nic.h>
 #include <ci/efhw/efct.h>
+#include <linux/mman.h>
+#include <ci/driver/ci_efct.h>
 #include "efrm_internal.h"
 
 struct efrm_efct_rxq {
@@ -92,6 +94,93 @@ int efrm_rxq_mmap(struct efrm_efct_rxq* rxq, struct vm_area_struct *vma,
   return remap_vmalloc_range(vma, rxq->hw.shm, 0);
 }
 EXPORT_SYMBOL(efrm_rxq_mmap);
+
+
+#define REFRESH_BATCH_SIZE  8
+#define EFCT_INVALID_PFN   (~0ull)
+#define HUGE_SIZE          (HPAGE_PMD_NR * PAGE_SIZE)
+
+static int fixup_superbuf_mapping(unsigned long addr,
+                                  uint64_t *user,
+                                  const struct xlnx_efct_hugepage *kern)
+{
+	uint64_t pfn = kern->page ? __pa(kern->page) : EFCT_INVALID_PFN;
+	if (*user == pfn)
+		return 0;
+
+	if (pfn == EFCT_INVALID_PFN) {
+		vm_munmap(addr, HUGE_SIZE);
+	}
+	else {
+		unsigned long rc;
+		rc = vm_mmap(kern->file, addr, HUGE_SIZE,
+		             PROT_READ,
+		             MAP_FIXED | MAP_SHARED | MAP_POPULATE |
+		                     MAP_HUGETLB | MAP_HUGE_2MB, 0);
+		if (IS_ERR((void*)rc))
+			return PTR_ERR((void*)rc);
+	}
+	*user = pfn;
+	return 1;
+}
+
+int efrm_rxq_refresh(struct efrm_efct_rxq *rxq, unsigned long superbufs,
+                     uint64_t __user *user_current, unsigned max_superbufs)
+{
+	struct xlnx_efct_hugepage *pages;
+	size_t i;
+	int rc = 0;
+
+	if (max_superbufs != CI_EFCT_MAX_SUPERBUFS) {
+		/* Could be supported without much difficulty, but no need for now */
+		return -EINVAL;
+	}
+
+	pages = kmalloc_array(sizeof(pages[0]), CI_EFCT_MAX_HUGEPAGES,
+	                      GFP_KERNEL);
+	if (!pages)
+		return -ENOMEM;
+
+	rc = efct_get_hugepages(rxq->rs.rs_client->nic,
+	                        &rxq->hw, pages, CI_EFCT_MAX_HUGEPAGES);
+	if (rc < 0) {
+		kfree(pages);
+		return rc;
+	}
+	for (i = 0; i < CI_EFCT_MAX_HUGEPAGES; i += REFRESH_BATCH_SIZE) {
+		uint64_t local_current[REFRESH_BATCH_SIZE];
+		size_t j;
+		size_t n = min((size_t)REFRESH_BATCH_SIZE,
+		               CI_EFCT_MAX_HUGEPAGES - i);
+		bool changes = false;
+
+		if (copy_from_user(local_current, user_current,
+		                   n * sizeof(*local_current)))
+			return -EFAULT;
+
+		for (j = 0; j < n; ++j) {
+			rc = fixup_superbuf_mapping(
+					superbufs + HUGE_SIZE * (i + j),
+					&local_current[j], &pages[i + j]);
+			if (rc < 0)
+				break;
+			if (rc)
+				changes = true;
+		}
+
+		if (changes)
+			if (copy_to_user(user_current, local_current,
+			                 n * sizeof(*local_current)))
+				rc = -EFAULT;
+
+		if (rc)
+			break;
+	}
+
+	kfree(pages);
+	return rc;
+}
+EXPORT_SYMBOL(efrm_rxq_refresh);
 
 
 static void efrm_rxq_rm_dtor(struct efrm_resource_manager *rm)

src/driver/linux_char/efct_rxq.c
@@ -104,7 +104,14 @@ static int
 rxq_rm_rsops(efch_resource_t* rs, ci_resource_table_t* priv_opt,
              ci_resource_op_t* op, int* copy_out)
 {
+  struct efrm_efct_rxq* rxq = efrm_rxq_from_resource(rs->rs_base);
+
   switch (op->op) {
+  case CI_RSOP_RXQ_REFRESH:
+    return efrm_rxq_refresh(rxq, (uintptr_t)op->u.rxq_refresh.superbufs,
+               (uint64_t __user*)(uintptr_t)op->u.rxq_refresh.current_mappings,
+               op->u.rxq_refresh.max_superbufs);
+
   default:
     EFCH_ERR("%s: Invalid op, expected CI_RSOP_RXQ_*", __FUNCTION__);
     return -EINVAL;

src/include/ci/efch/op_types.h
@@ -210,6 +210,7 @@ typedef struct ci_resource_op_s {
 # define                CI_RSOP_VI_GET_TS_FORMAT        0x88
 # define                CI_RSOP_EXT_FREE                0x89
 # define                CI_RSOP_EXT_MSG                 0x8A
+# define                CI_RSOP_RXQ_REFRESH             0x8B
 
   union {
     struct {
@@ -309,6 +310,11 @@ typedef struct ci_resource_op_s {
       uint64_t          payload_len;
       uint32_t          flags;
     } ext_msg;
+    struct {
+      uint64_t          superbufs;
+      uint64_t          current_mappings;
+      uint32_t          max_superbufs;
+    } rxq_refresh;
   } u CI_ALIGN(8);
 } ci_resource_op_t;
 

src/include/ci/efhw/efct.h
@@ -8,6 +8,7 @@
 extern struct efhw_func_ops efct_char_functional_units;
 
 struct efhw_efct_rxq;
+struct xlnx_efct_hugepage;
 typedef void efhw_efct_rxq_free_func_t(struct efhw_efct_rxq*);
 
 struct efhw_efct_rxq {
@@ -48,6 +49,8 @@ int efct_nic_rxq_bind(struct efhw_nic *nic, int qid,
                       size_t n_hugepages, struct efhw_efct_rxq *rxq);
 void efct_nic_rxq_free(struct efhw_nic *nic, struct efhw_efct_rxq *rxq,
                        efhw_efct_rxq_free_func_t *freer);
+int efct_get_hugepages(struct efhw_nic *nic, struct efhw_efct_rxq *rxq,
+                       struct xlnx_efct_hugepage *pages, size_t n_pages);
 
 static inline void efct_app_list_push(struct efhw_efct_rxq **head,
                                       struct efhw_efct_rxq *app)

src/include/ci/efrm/efct_rxq.h
@@ -21,6 +21,9 @@ extern int efrm_rxq_alloc(struct efrm_pd *pd, int qid,
 extern void efrm_rxq_release(struct efrm_efct_rxq *rxq);
 int efrm_rxq_mmap(struct efrm_efct_rxq* rxq, struct vm_area_struct *vma,
                   unsigned long *bytes);
+int efrm_rxq_refresh(struct efrm_efct_rxq *rxq,
+                     unsigned long superbufs, uint64_t __user *user_current,
+                     unsigned max_superbufs);
 
 
 #endif

src/lib/efhw/efct.c
@@ -75,6 +75,22 @@ efct_nic_rxq_free(struct efhw_nic *nic, struct efhw_efct_rxq *rxq,
 }
 
 
+int
+efct_get_hugepages(struct efhw_nic *nic, struct efhw_efct_rxq *rxq,
+                   struct xlnx_efct_hugepage *pages, size_t n_pages)
+{
+  struct device *dev;
+  struct xlnx_efct_device* edev;
+  struct xlnx_efct_client* cli;
+  int rc = 0;
+
+  EFCT_PRE(dev, edev, cli, nic, rc)
+  rc = edev->ops->get_hugepages(cli, rxq->qid, pages, n_pages);
+  EFCT_POST(dev, edev, cli, nic, rc);
+  return rc;
+}
+
+
 /*----------------------------------------------------------------------------
  *
  * Initialisation and configuration discovery

commit e5f7a4039ca7dc0229190eee0564347a5dc78690

ON-13281: Fix !CI_HAVE_EFCT_AUX build (broken by f94526c473)

src/lib/efrm/efrm_efct_rxq.c
@@ -96,6 +96,8 @@ int efrm_rxq_mmap(struct efrm_efct_rxq* rxq, struct vm_area_struct *vma,
 EXPORT_SYMBOL(efrm_rxq_mmap);
 
 
+#if CI_HAVE_EFCT_AUX
+
 #define REFRESH_BATCH_SIZE  8
 #define EFCT_INVALID_PFN   (~0ull)
 #define HUGE_SIZE          (HPAGE_PMD_NR * PAGE_SIZE)
@@ -124,9 +126,12 @@ static int fixup_superbuf_mapping(unsigned long addr,
 	return 1;
 }
 
+#endif /* CI_HAVE_EFCT_AUX */
+
 int efrm_rxq_refresh(struct efrm_efct_rxq *rxq, unsigned long superbufs,
                      uint64_t __user *user_current, unsigned max_superbufs)
 {
+#if CI_HAVE_EFCT_AUX
 	struct xlnx_efct_hugepage *pages;
 	size_t i;
 	int rc = 0;
@@ -179,6 +184,9 @@ int efrm_rxq_refresh(struct efrm_efct_rxq *rxq, unsigned long superbufs,
 
 	kfree(pages);
 	return rc;
+#else
+	return -EOPNOTSUPP;
+#endif /* CI_HAVE_EFCT_AUX */
 }
 EXPORT_SYMBOL(efrm_rxq_refresh);
 