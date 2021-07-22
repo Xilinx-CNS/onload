d4bc079c895711abe976315c4eeb9b4f1a9061e0
Richard Hughes <rhughes@xilinx.com>
2021-07-22 10:59:31 +0100

    ON-13281: Fix char interface checksum, broken when I merged my X3 rx branch

--- src/lib/ciul/pt_endpoint.c
+++ src/lib/ciul/pt_endpoint.c
@@ -370,7 +370,7 @@ void ef_vi_set_intf_ver(char* intf_ver, size_t len)
    * It'd also be possible to enhance the checksum computation to be smarter
    * (e.g. by ignoring comments, etc.).
    */
-  if( strcmp(EFCH_INTF_VER, "118fc3ec6ee6c36a0ad0ffa217f0aea9") ) {
+  if( strcmp(EFCH_INTF_VER, "0210a9e90b34107300d19b01516b4bc6") ) {
     fprintf(stderr, "ef_vi: ERROR: char interface has changed\n");
     abort();
   }
