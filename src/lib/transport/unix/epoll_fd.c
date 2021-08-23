Xilinx-CNS/onload

commit 7ab005e61fbaa145e5b66876b9d6bff996dd8dcb

ON-13023: epoll1_wait(): use one syscall as with epoll3
Kill OO_EPOLL1_IOC_PRIME, which was used in epoll1 mode only.  There is no
reason why epoll3 case can't be used for both epoll1 and epoll3.

src/lib/transport/unix/epoll_fd.c
@@ -2128,35 +2128,7 @@ no_events:
 
   ep->blocking = 1;
 
-#if CI_CFG_EPOLL3
-  if( ! ep->home_stack &&
-      ( CITP_OPTS.ul_epoll == 1 || ! ep->not_mt_safe ) )
-#endif
   {
-    /* If there is no home stack and it can not be created while we are
-     * blocked, we can block on epoll_wait() */
-
-    /* Fixme:
-     * - interrupt-driven stacks do not need to be primed at all;
-     * - home stack is primed by oo_epoll1_poll() function.
-     * - Quantization of the timeout to millisecond granularity means that
-     *   we can block for up to EF_POLL_USECS%1000 microseconds more than the
-     *   user requested (NB: this is the same behaviour we exhibit in poll()
-     *   and select())
-     * We need to prime all other stacks if there are any - in the most
-     * cases, there are no other stacks.
-     */
-    int timeout_ms = timeout_hr_to_ms(timeout_hr);
-    ci_sys_ioctl(ep->epfd_os, OO_EPOLL1_IOC_PRIME);
-
-    if( sigmask != NULL )
-      rc = ci_sys_epoll_pwait(fdi->fd, events, maxevents, timeout_ms, sigmask);
-    else
-      rc = ci_sys_epoll_wait(fdi->fd, events, maxevents, timeout_ms);
-    ep->blocking = 0;
-  }
-#if CI_CFG_EPOLL3
-  else {
     struct oo_epoll1_block_on_arg op;
 
     op.flags = 0;
@@ -2217,7 +2189,6 @@ no_events:
       rc = op.flags & (OO_EPOLL1_EVENT_ON_HOME | OO_EPOLL1_EVENT_ON_OTHER);
     }
   }
-#endif
 
   if( rc && ordering ) {
     ordering->poll_again = 1;

src/driver/linux_onload/epoll_device.c
@@ -1208,12 +1208,6 @@ static long oo_epoll_fop_unlocked_ioctl(struct file* filp,
     break;
   }
 
-  case OO_EPOLL1_IOC_PRIME: {
-    oo_epoll_prime_all_stacks(priv);
-    rc = 0;
-    break;
-  }
-
   case OO_EPOLL_IOC_CLONE: {
     ci_clone_fd_t local_arg;
 

src/include/onload/epoll.h
@@ -108,9 +108,6 @@ enum {
   OO_EPOLL1_OP_ADD_STACK,
 #define OO_EPOLL1_IOC_ADD_STACK \
   _IOW(OO_EPOLL_IOC_BASE, OO_EPOLL1_OP_ADD_STACK, ci_fixed_descriptor_t)
-  OO_EPOLL1_OP_PRIME,
-#define OO_EPOLL1_IOC_PRIME \
-  _IO(OO_EPOLL_IOC_BASE, OO_EPOLL1_OP_PRIME)
   OO_EPOLL_OP_CLONE,
 #define OO_EPOLL_IOC_CLONE \
   _IOWR(OO_EPOLL_IOC_BASE, OO_EPOLL_OP_CLONE, ci_clone_fd_t)

commit 6a6a4d33b541a17948ca970625ab3e1882657ed0

ON-13023: fix ulhelper build
Fixes: 7ab005e61f ON-13023: epoll1_wait(): use one syscall as with epoll3

src/lib/transport/unix/epoll_fd.c
@@ -2159,6 +2159,7 @@ no_events:
         eps.events += rc;
       }
 
+#if CI_CFG_EPOLL3
       /* Are there any events in the home stack? */
       if( op.flags & OO_EPOLL1_EVENT_ON_HOME ) {
         citp_reenter_lib(lib_context);
@@ -2173,6 +2174,7 @@ no_events:
         citp_exit_lib(lib_context, FALSE);
         rc = eps.events - events;
       }
+#endif
 
       if( rc == 0 ) {
         /* False alarm. Let's block again.  */