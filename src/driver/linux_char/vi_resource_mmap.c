Xilinx-CNS/onload

commit 40df45e0107fadb47803f790ba94113fa68f3170

ON-12852: Share ep_state between kernel and user
The memory is used in kernelspace only in the exceptional case where the NIC is
hotplugged/reset, where we need the evq_ptr in order to inject an artificial
event to tell userspace of the reset. In all cases the memory is read-only to
the kernel.

Conflicts:
	src/driver/linux_char/vi_resource.c
	src/driver/linux_char/vi_resource_mmap.c
	src/include/ci/efch/mmap_id.h

(cherry picked from commit 487decf5c1206be3f7db983a8309900bdcf78b76)
(cherry picked from commit 3356e4bf77d1a7811ec50653b9a01711f8247960)

src/driver/linux_char/vi_resource_mmap.c
@@ -18,6 +18,7 @@
 #include <ci/driver/resource/linux_efhw_nic.h>
 #include <ci/efrm/vi_resource_manager.h>
 #include <ci/efrm/efrm_client.h>
+#include <etherfabric/internal/internal.h>
 #include <ci/efch/mmap.h>
 #include "linux_char_internal.h"
 #include "char_internal.h"
@@ -185,6 +186,40 @@ efab_vi_rm_mmap_plugin(struct efrm_vi *virs, unsigned subpage,
 }
 
 
+static int
+efab_vi_rm_mmap_state(struct efrm_vi *virs, unsigned long *bytes, void *opaque,
+                      int *map_num, unsigned long *offset)
+{
+  struct vm_area_struct* vma = opaque;
+  int rc;
+  int len;
+
+  if( ! virs->ep_state )
+    return -EINVAL;  /* Someone used an Onload VI rather than an ef_vi one */
+  if( *offset != 0 )
+    return -EINVAL;
+  len = ef_vi_calc_state_bytes(virs->q[EFHW_RXQ].capacity,
+                               virs->q[EFHW_TXQ].capacity);
+  if( *bytes > CI_ROUND_UP(len, CI_PAGE_SIZE) )
+    return -EINVAL;
+
+  /* ep_state came from vmalloc_user, which handles most of the safety issues
+   * itself (i.e. all memory is zeroed and page-aligned) */
+  rc = remap_vmalloc_range_partial(vma, vma->vm_start, (void*)virs->ep_state,
+#ifdef EFRM_REMAP_VMALLOC_RANGE_PARTIAL_NEW
+                                     /* for linux>=5.7 */
+                                     0,
+#endif
+                                     *bytes);
+  if( rc < 0 )
+    EFCH_ERR("%s: ERROR: remap_vmalloc_range_partial failed rc=%d",
+             __FUNCTION__, rc);
+  else
+    *bytes -= len;
+  return rc;
+}
+
+
 static int 
 efab_vi_rm_mmap_mem(struct efrm_vi *virs,
                     unsigned long *bytes, void *opaque,
@@ -220,6 +255,9 @@ int efab_vi_resource_mmap(struct efrm_vi *virs, unsigned long *bytes,
       break;
     case EFCH_VI_MMAP_CTPIO:
       rc = efab_vi_rm_mmap_ctpio(virs, bytes, vma, map_num, offset);
+      break;
+    case EFCH_VI_MMAP_STATE:
+      rc = efab_vi_rm_mmap_state(virs, bytes, vma, map_num, offset);
       break;
     case EFCH_VI_MMAP_PLUGIN_BASE ... EFCH_VI_MMAP_PLUGIN_MAX:
       rc = efab_vi_rm_mmap_plugin(virs, index - EFCH_VI_MMAP_PLUGIN_BASE,

src/driver/linux_char/vi_resource.c
@@ -11,6 +11,7 @@
 #include <ci/efch/mmap.h>
 #include <ci/efch/op_types.h>
 #include <ci/driver/kernel_compat.h>
+#include <etherfabric/internal/internal.h>
 #include "char_internal.h"
 #include "filter_list.h"
 #include "linux_char_internal.h"
@@ -172,6 +173,11 @@ vi_resource_alloc(struct efrm_vi_attr *attr,
     goto fail_q_alloc;
   if ((rc = efrm_vi_q_alloc(virs, EFHW_TXQ, txq_capacity,
                             tx_q_tag, vi_flags, evq_virs)) < 0)
+    goto fail_q_alloc;
+
+  virs->ep_state = vmalloc_user(ef_vi_calc_state_bytes(rxq_capacity,
+                                                       txq_capacity));
+  if (!virs->ep_state)
     goto fail_q_alloc;
 
   *virs_out = virs;
@@ -352,6 +358,7 @@ void efch_vi_rm_free(efch_resource_t *rs)
   if( rs->vi.sniff_flags & EFCH_TX_SNIFF )
     efrm_tx_port_sniff(rs->rs_base, 0, -1);
   efrm_vi_tx_alt_free(virs);
+  vfree((void*)virs->ep_state);
 }
 
 

src/include/ci/efch/mmap_id.h
@@ -7,6 +7,7 @@
 #define EFCH_VI_MMAP_MEM 1
 #define EFCH_VI_MMAP_PIO 2
 #define EFCH_VI_MMAP_CTPIO 3
+#define EFCH_VI_MMAP_STATE 4
 #define EFCH_VI_MMAP_PLUGIN_BASE 0x10
 #define EFCH_VI_MMAP_PLUGIN_MAX 0x1f
 

src/include/ci/efrm/vi_resource_manager.h
@@ -169,6 +169,10 @@ struct efrm_vi {
 	 */
 	volatile char __iomem *io_page;
 
+	/* Used by sfc_char only (NULL for Onload VIs). Mapped in to userspace and
+	 * read-only to the kernel; be careful when accessing. */
+	const volatile ef_vi_state *ep_state;
+
 	unsigned tx_alt_cp;
 	int      tx_alt_num;
 	unsigned tx_alt_ids[EFRM_VI_TX_ALTERNATIVES_MAX];

src/include/etherfabric/ef_vi.h
@@ -834,6 +834,8 @@ typedef struct ef_vi {
   char*                         vi_ctpio_mmap_ptr;
   /** Controls rate of writes into CTPIO aperture */
   uint32_t                      vi_ctpio_wb_ticks;
+  /** Length of region allocated at ep_state */
+  int                           ep_state_bytes;
   /** True if the virtual interface is in a cluster */
   int                           vi_clustered;
   /** True if packed stream mode is enabled for the virtual interface */

src/lib/ciul/pt_endpoint.c
@@ -388,6 +388,7 @@ int __ef_vi_alloc(ef_vi* vi, ef_driver_handle vi_dh,
   uint32_t* ids;
   void* p;
   int q_label;
+  int state_bytes;
 
   if( txq_capacity < 0 && (s = getenv("EF_VI_TXQ_SIZE")) )
     txq_capacity = atoi(s);
@@ -467,10 +468,14 @@ int __ef_vi_alloc(ef_vi* vi, ef_driver_handle vi_dh,
   txq_capacity = ra.u.vi_out.txq_capacity;
   rxq_capacity = ra.u.vi_out.rxq_capacity;
 
-  rc = -ENOMEM;
-  state = malloc(ef_vi_calc_state_bytes(rxq_capacity, txq_capacity));
-  if( state == NULL )
+  state_bytes = ef_vi_calc_state_bytes(rxq_capacity, txq_capacity);
+  rc = ci_resource_mmap(vi_dh, ra.out_id.index, EFCH_VI_MMAP_STATE,
+                        CI_ROUND_UP(state_bytes, CI_PAGE_SIZE), &p);
+  if( rc < 0 ) {
+    LOGVV(ef_log("%s: ci_resource_mmap (state) %d", __FUNCTION__, rc));
     goto fail1;
+  }
+  state = (void*)p;
 
   if( ra.u.vi_out.io_mmap_bytes ) {
     rc = ci_resource_mmap(vi_dh, ra.out_id.index, EFCH_VI_MMAP_IO,
@@ -565,6 +570,7 @@ int __ef_vi_alloc(ef_vi* vi, ef_driver_handle vi_dh,
   vi->vi_ctpio_mmap_ptr = ctpio_mmap_ptr;
   vi->vi_io_mmap_bytes = ra.u.vi_out.io_mmap_bytes;
   vi->vi_mem_mmap_bytes = ra.u.vi_out.mem_mmap_bytes;
+  vi->ep_state_bytes = state_bytes;
   vi->vi_resource_id = ra.out_id.index;
   if( ra.u.vi_out.out_flags & EFHW_VI_PS_BUF_SIZE_SET )
     vi->vi_ps_buf_size = ra.u.vi_out.ps_buf_size;
@@ -595,7 +601,8 @@ int __ef_vi_alloc(ef_vi* vi, ef_driver_handle vi_dh,
   if( io_mmap_base != NULL )
     ci_resource_munmap(vi_dh, io_mmap_base, ra.u.vi_out.io_mmap_bytes);
  fail2:
-  free(state);
+  if( state != NULL )
+    ci_resource_munmap(vi_dh, state, state_bytes);
  fail1:
   --evq->vi_qs_n;
   return rc;
@@ -678,7 +685,14 @@ int ef_vi_free(ef_vi* ep, ef_driver_handle fd)
     }
   }
 
-  free(ep->ep_state);
+  if( ep->ep_state != NULL ) {
+    rc = ci_resource_munmap(fd, ep->ep_state, ep->ep_state_bytes);
+    if( rc < 0 ) {
+      LOGV(ef_log("%s: ci_resource_munmap %d", __FUNCTION__, rc));
+      return rc;
+    }
+  }
+
   free(ep->tx_alt_id2hw);
   free(ep->tx_alt_hw2id);
   free(ep->vi_stats);

commit c76cf07e173865b01132c184ce490d5ba2e53fc7

ON-13406: ON-13406: remove harmless warning
Fixes: 40df45e01 ON-12852: Share ep_state between kernel and user

src/driver/linux_char/vi_resource_mmap.c
@@ -200,7 +200,8 @@ efab_vi_rm_mmap_state(struct efrm_vi *virs, unsigned long *bytes, void *opaque,
     return -EINVAL;
   len = ef_vi_calc_state_bytes(virs->q[EFHW_RXQ].capacity,
                                virs->q[EFHW_TXQ].capacity);
-  if( *bytes > CI_ROUND_UP(len, CI_PAGE_SIZE) )
+  len = CI_ROUND_UP(len, CI_PAGE_SIZE);
+  if( *bytes != len )
     return -EINVAL;
 
   /* ep_state came from vmalloc_user, which handles most of the safety issues