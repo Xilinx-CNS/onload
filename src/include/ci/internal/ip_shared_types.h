Xilinx-CNS/onload

commit ddb52cb12eada23f7d98491e43b73acc4a4f76a5

ON-13442: decrement send_prequeue_in when sending

src/lib/transport/ip/tcp_send.c
@@ -1202,6 +1202,7 @@ void ci_tcp_sendmsg_enqueue_prequeue(ci_netif* ni, ci_tcp_state* ts,
     ++n_pkts;
   }
   while( OO_PP_NOT_NULL(id) );
+  oo_atomic_add(&ts->send_prequeue_in, -n_pkts);
 
   /* Prep each packet. */
   while( 1 ) {

commit 908a6c232a00d22b80b03341096827c2427806f0

ON-13442: sort out send_prequeue_in usage
Revert the previous commit for this bug.  Add a comment how send_prequeue_in
should be used.

This counter is a sort of atomic addition to send_in counter, and both of
them are never decremented.  send_out is incrememnted instead.

Fixes: ddb52cb12e ON-13442: decrement send_prequeue_in when sending

src/include/ci/internal/ip_shared_types.h
@@ -2849,6 +2849,8 @@ struct ci_tcp_state_s {
   ** lock is contended, and are later transferred to the sendq.  This is a
   ** linked list of packets in reverse order. */
   ci_int32             send_prequeue;
+  /* send_prequeue_in is an atomic addition to send_in; it is never
+   * decremented.  See ci_tcp_sendq_n_pkts(). */
   oo_atomic_t          send_prequeue_in;
 
   struct oo_p_dllink   timeout_q_link;

src/lib/transport/ip/tcp_connect.c
@@ -895,6 +895,9 @@ static int ci_tcp_connect_ul_start(ci_netif *ni, ci_tcp_state* ts, ci_fd_t fd,
 
   /* Recover from previous connection via the same socket: */
   ts->tcpflags &=~ CI_TCPT_FLAG_FIN_RECEIVED;
+  /* send_prequeue may be set to OO_PP_ID_INVALID by a previous connection
+   * attempt.  However there is no need to reinit send_prequeue_in and
+   * other counters, because no real send was possible. */
   ts->send_prequeue = OO_PP_ID_NULL;
   ci_assert_equal(oo_atomic_read(&ts->send_prequeue_in), 0);
 

src/lib/transport/ip/tcp_send.c
@@ -1202,7 +1202,6 @@ void ci_tcp_sendmsg_enqueue_prequeue(ci_netif* ni, ci_tcp_state* ts,
     ++n_pkts;
   }
   while( OO_PP_NOT_NULL(id) );
-  oo_atomic_add(&ts->send_prequeue_in, -n_pkts);
 
   /* Prep each packet. */
   while( 1 ) {
@@ -1308,6 +1307,10 @@ ci_tcp_tx_free_prequeue(ci_netif* ni, ci_tcp_state* ts, int netif_locked)
 
   n_pkts = ci_tcp_sendmsg_free_pkt_list(ni, ts, id, netif_locked, 1);
 
+  /* Despite the comment at send_prequeue_in definition, we do decrement it
+   * here.  This function is called from ci_tcp_sendmsg_handle_tx_errno()
+   * only, i.e. these packets have not really got into sendq, and should
+   * not be accounted at all.  */
   oo_atomic_add(&ts->send_prequeue_in, -n_pkts);
 }
 