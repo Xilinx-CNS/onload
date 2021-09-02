Xilinx-CNS/onload

commit f0bb617b7565de52c8c798e29393ad57dfb2f40b

ON-13461: fix efab_tcp_helper_map_usermem() for !FOLL_SPLIT case
Linux-5.13 does not have FOLL_SPLIT, so there is no way to get
an array of non-compound pages.  As a result, the last compound page
may cross the end of our memory area.

This patch adjust assertions for this case and avoids putting a page which
was never get.

src/lib/efthrm/tcp_helper_resource.c
@@ -6148,7 +6148,11 @@ int efab_tcp_helper_map_usermem(tcp_helper_resource_t* trs,
     last_order = order;
     i += 1 << order;
   }
+#ifdef FOLL_SPLIT
   ci_assert_equal(i, n_pages);
+#else
+  ci_assert_ge(i, n_pages);
+#endif
 
   ioum->groups = kmalloc_array(ioum->n_groups, sizeof(*ioum->groups),
                                GFP_KERNEL);
@@ -6195,7 +6199,11 @@ int efab_tcp_helper_map_usermem(tcp_helper_resource_t* trs,
 
     i += group_pages;
   }
+#ifdef FOLL_SPLIT
   ci_assert_equal(i, n_pages);
+#else
+  ci_assert_ge(i, n_pages);
+#endif
 
   /* For compound pages we only store the head page and we're about to lose
    * the struct page* for all the tail pages. Decref all those tail pages
@@ -6210,13 +6218,23 @@ int efab_tcp_helper_map_usermem(tcp_helper_resource_t* trs,
     int group_pages = g->pages->n_bufs << order;
 
     if (order) {
-      int j;
-      for( j = 0; j < group_pages >> order; ++j )
-        efab_put_pages(pages + i + (j << order) + 1, (1 << order) - 1);
+      while( i < n_pages ) {
+        /* The CI_MIN() below is needed for ! FOLL_SPLIT case only,
+         * i.e. for linux >= 5.13. */
+        efab_put_pages(pages + i + 1,
+                       CI_MIN(1 << order, n_pages - i) - 1);
+        i += 1 << order;
+      }
     }
-    i += group_pages;
+    else {
+      i += group_pages;
+    }
   }
+#ifdef FOLL_SPLIT
   ci_assert_equal(i, n_pages);
+#else
+  ci_assert_ge(i, n_pages);
+#endif
   kfree(pages);
 
   *hw_addrs_out = hw_addrs;

commit 8b73cd04159574a882e5876493291f23c441b4be

ON-13461: another !FOLL_SPLIT fix in efab_tcp_helper_map_usermem()
Fixes: f0bb617b75 ON-13461: fix efab_tcp_helper_map_usermem() for !FOLL_SPLIT case

src/lib/efthrm/tcp_helper_resource.c
@@ -6232,11 +6232,12 @@ int efab_tcp_helper_map_usermem(tcp_helper_resource_t* trs,
     int group_pages = g->pages->n_bufs << order;
 
     if (order) {
-      while( i < n_pages ) {
-        /* The CI_MIN() below is needed for ! FOLL_SPLIT case only,
+        /* The 2 CI_MIN()s below are needed for ! FOLL_SPLIT case only,
          * i.e. for linux >= 5.13. */
+      int i_max = CI_MIN(n_pages, i + group_pages);
+      while( i < i_max ) {
         efab_put_pages(pages + i + 1,
-                       CI_MIN(1 << order, n_pages - i) - 1);
+                       CI_MIN(1 << order, i_max - i) - 1);
         i += 1 << order;
       }
     }