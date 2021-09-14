Xilinx-CNS/onload

commit b02e8be3d5ae3796941617d63610b326ed873fb8

ON-13342: [2/4] Generalise and extract the AF_XDP filter->ethtool converter

src/lib/efhw/af_xdp.c
@@ -25,6 +25,7 @@
 #include <ci/efrm/efrm_filter.h>
 
 #include "ethtool_rxclass.h"
+#include "ethtool_flow.h"
 
 /* sys_call_area: a process-mapped area which can be used to perform
  * system calls from a module.
@@ -1413,12 +1414,8 @@ af_xdp_rss_flags(struct efhw_nic *nic, u32 *flags_out)
 	return -ENOSYS;
 }
 
-#define EFX_IP_FILTER_MATCH_FLAGS \
-                (EFX_FILTER_MATCH_ETHER_TYPE | EFX_FILTER_MATCH_IP_PROTO | \
-                 EFX_FILTER_MATCH_LOC_HOST | EFX_FILTER_MATCH_LOC_PORT)
-
-static int efx_spec_to_ethtool_flow(struct efx_filter_spec* efx_spec,
-				    struct ethtool_rx_flow_spec* fs)
+static int af_xdp_efx_spec_to_ethtool_flow(struct efx_filter_spec* efx_spec,
+					   struct ethtool_rx_flow_spec* fs)
 {
 	/* In order to support different driver capabilities we need to
 	 * always install the same filter type. This means that we will
@@ -1441,34 +1438,27 @@ static int efx_spec_to_ethtool_flow(struct efx_filter_spec* efx_spec,
 	 * back to traffic via the kernel, so I'm living with it for now.
 	 */
 
-	/* Check that this is an IP filter */
-	if ((efx_spec->match_flags & EFX_IP_FILTER_MATCH_FLAGS) !=
-	    EFX_IP_FILTER_MATCH_FLAGS)
+	int rc = efx_spec_to_ethtool_flow(efx_spec, fs);
+	if (rc < 0)
+		return rc;
+
+	switch (fs->flow_type) {
+	case UDP_V4_FLOW:
+		if (fs->m_u.udp_ip4_spec.ip4src || fs->m_u.udp_ip4_spec.psrc ||
+		    fs->m_u.udp_ip4_spec.tos)
+			return -EOPNOTSUPP;
+		break;
+	case TCP_V4_FLOW:
+		if (fs->m_u.tcp_ip4_spec.ip4src || fs->m_u.tcp_ip4_spec.psrc ||
+		    fs->m_u.tcp_ip4_spec.tos)
+			return -EOPNOTSUPP;
+		break;
+	default:
+		/* FIXME AF_XDP need to check whether we can install both IPv6
+		 * and IPv4 filters. For now just support IPv4.
+		 */
 		return -EOPNOTSUPP;
-
-	/* FIXME AF_XDP need to check whether we can install both IPv6 and
-	 * IPv4 filters. For now just support IPv4.
-	 */
-	if (efx_spec->ether_type != ntohs(ETH_P_IP))
-		return -EOPNOTSUPP;
-
-	if (efx_spec->ip_proto == IPPROTO_TCP)
-		fs->flow_type = TCP_V4_FLOW;
-	else if (efx_spec->ip_proto == IPPROTO_UDP)
-		fs->flow_type = UDP_V4_FLOW;
-	else
-		return -EINVAL;
-
-	/* Populate the match fields. For each field we need to set both the
-	 * value, and a mask of which bits in that field to match against.
-	 */
-	fs->h_u.tcp_ip4_spec.ip4dst = efx_spec->loc_host[0];
-	fs->m_u.tcp_ip4_spec.ip4dst = 0xffffffff;
-	fs->h_u.tcp_ip4_spec.pdst = efx_spec->loc_port;
-	fs->m_u.tcp_ip4_spec.pdst = 0xffff;
-
-	/* Give the driver free rein on where to insert the filter. */
-	fs->location = RX_CLS_LOC_ANY;
+	}
 
 	/* TODO AF_XDP: for now assume dmaq_id matches NIC channel
 	 * based on insight into efhw/af_xdp.c */
@@ -1489,7 +1479,7 @@ af_xdp_filter_insert(struct efhw_nic *nic, struct efx_filter_spec *spec,
 
 	memset(&info, 0, sizeof(info));
 	info.cmd = ETHTOOL_SRXCLSRLINS;
-	rc = efx_spec_to_ethtool_flow(spec, &info.fs);
+	rc = af_xdp_efx_spec_to_ethtool_flow(spec, &info.fs);
 	if ( rc < 0 )
 		return rc;
 

src/driver/linux_resource/mmake.mk
@@ -15,9 +15,10 @@ RESOURCE_SRCS	:= resource_driver.c \
 	aux_driver.c aux_efct.c
 
 EFHW_SRCS	:= nic.c eventq.c ef10.c ef100.c af_xdp.c ethtool_rxclass.c \
-		efct.c
+		ethtool_flow.c efct.c
 
-EFHW_HDRS	:= ef10_mcdi.h ethtool_rxclass.h ef10_ef100.h efct.h
+EFHW_HDRS	:= ef10_mcdi.h ethtool_rxclass.h ethtool_flow.h ef10_ef100.h \
+		efct.h
 
 EFRM_SRCS	:=			\
 		assert_valid.c		\

src/lib/efhw/ethtool_flow.c
@@ -1,0 +1,109 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* X-SPDX-Copyright-Text: (c) Copyright 2019-2020 Xilinx, Inc. */
+#include <uapi/linux/ethtool.h>
+#include <ci/driver/driverlink_api.h>
+
+static uint32_t combine_ports(uint16_t loc, uint16_t rem)
+{
+  return htonl(ntohs(loc) | (htons(rem) << 16));
+}
+
+int efx_spec_to_ethtool_flow(const struct efx_filter_spec *src,
+                             struct ethtool_rx_flow_spec *dst)
+{
+  int proto = -1;
+  uint32_t loc_ip = 0, loc_ip_mask = 0;
+  uint16_t loc_port = 0, loc_port_mask = 0;
+  uint32_t rem_ip = 0, rem_ip_mask = 0;
+  uint16_t rem_port = 0, rem_port_mask = 0;
+
+  memset(dst, 0, sizeof(*dst));
+  dst->location = RX_CLS_LOC_ANY;
+
+  if( src->flags & ~(EFX_FILTER_FLAG_RX | EFX_FILTER_FLAG_STACK_ID |
+                     EFX_FILTER_FLAG_VPORT_ID | EFX_FILTER_FLAG_RX_SCATTER) )
+    return -EPROTONOSUPPORT;
+
+  if( src->match_flags == EFX_FILTER_MATCH_LOC_MAC_IG &&
+      src->loc_mac[0] == 1 ) {
+    dst->flow_type = UDP_V4_FLOW;
+    dst->h_u.udp_ip4_spec.ip4dst = htonl(0xe0000000);
+    dst->m_u.udp_ip4_spec.ip4dst = htonl(0xf0000000);
+    return 0;
+  }
+
+  if( src->match_flags & ~(EFX_FILTER_MATCH_REM_HOST |
+                            EFX_FILTER_MATCH_LOC_HOST |
+                            EFX_FILTER_MATCH_REM_PORT |
+                            EFX_FILTER_MATCH_LOC_PORT |
+                            EFX_FILTER_MATCH_IP_PROTO |
+                            EFX_FILTER_MATCH_ETHER_TYPE) )
+    return -EPROTONOSUPPORT;
+  if( (src->match_flags &
+       (EFX_FILTER_MATCH_REM_HOST | EFX_FILTER_MATCH_LOC_HOST)) ==
+      EFX_FILTER_MATCH_REM_HOST ||
+      (src->match_flags &
+       (EFX_FILTER_MATCH_REM_PORT | EFX_FILTER_MATCH_LOC_PORT)) ==
+      EFX_FILTER_MATCH_REM_PORT )
+    return -EPROTONOSUPPORT;
+  if( src->match_flags & EFX_FILTER_MATCH_ETHER_TYPE &&
+      src->ether_type != htons(ETH_P_IP) )
+    return -EPROTONOSUPPORT;
+
+  if( src->match_flags & EFX_FILTER_MATCH_IP_PROTO )
+    proto = src->ip_proto;
+
+  if( src->match_flags & EFX_FILTER_MATCH_LOC_HOST ) {
+    loc_ip = src->loc_host[0];
+    loc_ip_mask = -1;
+  }
+  if( src->match_flags & EFX_FILTER_MATCH_LOC_PORT ) {
+    loc_port = src->loc_port;
+    loc_port_mask = -1;
+  }
+  if( src->match_flags & EFX_FILTER_MATCH_REM_HOST ) {
+    rem_ip = src->rem_host[0];
+    rem_ip_mask = -1;
+  }
+  if( src->match_flags & EFX_FILTER_MATCH_REM_PORT ) {
+    rem_port = src->rem_port;
+    rem_port_mask = -1;
+  }
+  switch( proto ) {
+  case IPPROTO_UDP:
+    dst->flow_type = UDP_V4_FLOW;
+    dst->h_u.udp_ip4_spec.ip4dst = loc_ip;
+    dst->h_u.udp_ip4_spec.pdst = loc_port;
+    dst->h_u.udp_ip4_spec.ip4src = rem_ip;
+    dst->h_u.udp_ip4_spec.psrc = rem_port;
+    dst->m_u.udp_ip4_spec.ip4dst = loc_ip_mask;
+    dst->m_u.udp_ip4_spec.pdst = loc_port_mask;
+    dst->m_u.udp_ip4_spec.ip4src = rem_ip_mask;
+    dst->m_u.udp_ip4_spec.psrc = rem_port_mask;
+    break;
+  case IPPROTO_TCP:
+    dst->flow_type = TCP_V4_FLOW;
+    dst->h_u.tcp_ip4_spec.ip4dst = loc_ip;
+    dst->h_u.tcp_ip4_spec.pdst = loc_port;
+    dst->h_u.tcp_ip4_spec.ip4src = rem_ip;
+    dst->h_u.tcp_ip4_spec.psrc = rem_port;
+    dst->m_u.tcp_ip4_spec.ip4dst = loc_ip_mask;
+    dst->m_u.tcp_ip4_spec.pdst = loc_port_mask;
+    dst->m_u.tcp_ip4_spec.ip4src = rem_ip_mask;
+    dst->m_u.tcp_ip4_spec.psrc = rem_port_mask;
+    break;
+  default:
+    dst->flow_type = IPV4_USER_FLOW;
+    dst->h_u.usr_ip4_spec.proto = proto;
+    dst->h_u.usr_ip4_spec.ip4dst = loc_ip;
+    dst->h_u.usr_ip4_spec.ip4src = rem_ip;
+    dst->h_u.usr_ip4_spec.l4_4_bytes = combine_ports(loc_port, rem_port);
+    dst->m_u.usr_ip4_spec.proto = proto < 0 ? 0 : -1;
+    dst->m_u.usr_ip4_spec.ip4dst = loc_ip_mask;
+    dst->m_u.usr_ip4_spec.ip4src = rem_ip_mask;
+    dst->m_u.usr_ip4_spec.l4_4_bytes = combine_ports(loc_port_mask,
+                                                     rem_port_mask);
+    break;
+  }
+  return 0;
+}

src/lib/efhw/ethtool_flow.h
@@ -1,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* X-SPDX-Copyright-Text: (c) Copyright 2019-2020 Xilinx, Inc. */
+#ifndef INCLUDED_ETHTOOL_FLOW_H_
+#define INCLUDED_ETHTOOL_FLOW_H_
+
+struct efx_filter_spec;
+struct ethtool_rx_flow_spec;
+
+/* Converts src to dst. Note that the capabilities of this function may be
+ * enhanced arbitrarily in the future, so callers must perform their own
+ * validation that they're not getting a flow with more features than they
+ * support. Zeros and does not populate the ring_cookie field: the caller
+ * needs to do that. */
+int efx_spec_to_ethtool_flow(const struct efx_filter_spec *src,
+                             struct ethtool_rx_flow_spec *dst);
+
+#endif

src/lib/efhw/mmake.mk
@@ -11,7 +11,8 @@ LIB_SRCS	:= nic.c \
 		   ef100.c \
 		   efct.c \
 		   af_xdp.c \
-		   ethtool_rxclass.c
+		   ethtool_rxclass.c \
+		   ethtool_flow.c
 
 
 ifndef MMAKE_NO_RULES

commit 7b4fc091c824d300bbdc6288b0ba0dbbd26897c8

ON-13499: ON-13342: AF_XDP ethtool filter conversion got made too high fidelity
Regression introduced by b02e8be3

src/lib/efhw/af_xdp.c
@@ -1449,14 +1449,20 @@ static int af_xdp_efx_spec_to_ethtool_flow(struct efx_filter_spec* efx_spec,
 
 	switch (fs->flow_type) {
 	case UDP_V4_FLOW:
-		if (fs->m_u.udp_ip4_spec.ip4src || fs->m_u.udp_ip4_spec.psrc ||
-		    fs->m_u.udp_ip4_spec.tos)
+		if (fs->m_u.udp_ip4_spec.tos)
 			return -EOPNOTSUPP;
+		fs->h_u.udp_ip4_spec.ip4src = 0;
+		fs->h_u.udp_ip4_spec.psrc = 0;
+		fs->m_u.udp_ip4_spec.ip4src = 0;
+		fs->m_u.udp_ip4_spec.psrc = 0;
 		break;
 	case TCP_V4_FLOW:
-		if (fs->m_u.tcp_ip4_spec.ip4src || fs->m_u.tcp_ip4_spec.psrc ||
-		    fs->m_u.tcp_ip4_spec.tos)
+		if (fs->m_u.tcp_ip4_spec.tos)
 			return -EOPNOTSUPP;
+		fs->h_u.tcp_ip4_spec.ip4src = 0;
+		fs->h_u.tcp_ip4_spec.psrc = 0;
+		fs->m_u.tcp_ip4_spec.ip4src = 0;
+		fs->m_u.tcp_ip4_spec.psrc = 0;
 		break;
 	default:
 		/* FIXME AF_XDP need to check whether we can install both IPv6