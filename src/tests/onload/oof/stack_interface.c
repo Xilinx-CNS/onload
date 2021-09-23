Xilinx-CNS/onload

commit 0517e558e5854fce2f37695be5fe86d33e54f68f

ON-13449: [3/3] Attach to efct rxqs on Onload filter add

src/lib/efthrm/tcp_filters.c
@@ -304,6 +304,20 @@ oo_hw_filter_set_hwport(struct oo_hw_filter* oofilter, int hwport,
       oofilter->filter_id[hwport] = rc;
       rc = 0;
     }
+    if( rc == 0 && oofilter->filter_id[hwport] >= 0 ) {
+      rc = tcp_helper_post_filter_add(oofilter->trs, hwport, &spec, rxq,
+                                      replace);
+      if( rc < 0 ) {
+        if( ! replace )
+          efrm_filter_remove(get_client(hwport), oofilter->filter_id[hwport]);
+        /* We ideally want to restore replaced filters too, but we're not
+         * keeping enough info to know how. In any case, we currently have no
+         * hardware on which this matters (no hardware uses both a non-trivial
+         * tcp_helper_post_filter_add() and the replace option), so it'd be
+         * untested code. If/when that hardware comes, there are many possible
+         * locations for the solution. */
+      }
+    }
   }
   return rc;
 }

src/tests/onload/oof/stack_interface.c
@@ -40,3 +40,9 @@ int tcp_helper_vi_hw_drop_filter_supported(tcp_helper_resource_t* trs,
   return 1;
 }
 
+int tcp_helper_post_filter_add(tcp_helper_resource_t* trs, int hwport,
+                               const struct efx_filter_spec* spec, int rxq,
+                               bool replace)
+{
+  return 0;
+}

src/tests/onload/oof/stack_interface.h
@@ -7,6 +7,8 @@
 #include "efrm_interface.h"
 #include "stack.h"
 
+struct efx_filter_spec;
+
 /* Return the instance number of the VI associated with the named hwport,
  * or -1 if we don't have a VI for that hwport.
  */
@@ -36,5 +38,9 @@ extern int tcp_helper_vi_hw_rx_loopback_supported(tcp_helper_resource_t* trs,
 
 extern int tcp_helper_vi_hw_drop_filter_supported(tcp_helper_resource_t* trs,
                                                   int hwport);
+
+int tcp_helper_post_filter_add(tcp_helper_resource_t* trs, int hwport,
+                               const struct efx_filter_spec* spec, int rxq,
+                               bool replace);
 
 #endif /* __OOF_TEST_STACK_INTERFACE_H__ */

src/include/etherfabric/internal/internal.h
@@ -131,6 +131,7 @@ void efct_vi_munmap_internal(ef_vi* vi);
 int efct_vi_find_free_rxq(ef_vi* vi, int qid);
 void efct_vi_attach_rxq_internal(ef_vi* vi, int ix, int resource_id,
                                  ef_vi_efct_superbuf_refresh_t *refresh_func);
+void efct_vi_start_rxq(ef_vi* vi, int ix);
 
 
 /* This returns the ID of the next RX buffer in the RXQ.  In the absence of

src/include/onload/tcp_helper_endpoint.h
@@ -59,6 +59,11 @@ extern int tcp_helper_vi_hw_rx_loopback_supported(tcp_helper_resource_t* trs,
 extern int tcp_helper_vi_hw_drop_filter_supported(tcp_helper_resource_t* trs,
                                                   int hwport);
 
+/* Executed after any hardware filter add. Used by efct to bring up a new rxq
+ * if one has been added */
+extern int tcp_helper_post_filter_add(tcp_helper_resource_t* trs, int hwport,
+                                      const struct efx_filter_spec* spec,
+                                      int rxq, bool replace);
 
 /*--------------------------------------------------------------------
 *

src/lib/ciul/efct_vi.c
@@ -944,10 +944,7 @@ int efct_vi_attach_rxq(ef_vi* vi, int qid, unsigned n_superbufs)
 
   efct_vi_attach_rxq_internal(vi, ix, ra.out_id.index,
                               superbuf_config_refresh);
-  /* This is a totally fake pkt_id, but it makes efct_poll_rx() think that a
-   * rollover is needed. We use +1 as a marker that this is the first packet,
-   * i.e. ignore the first metadata: */
-  vi->ep_state->rxq.rxq_ptr[ix].next = 1 + vi->efct_shm[ix].superbuf_pkts;
+  efct_vi_start_rxq(vi, ix);
   return 0;
 }
 #endif
@@ -961,6 +958,14 @@ void efct_vi_attach_rxq_internal(ef_vi* vi, int ix, int resource_id,
   rxq->resource_id = resource_id;
   rxq->config_generation = 0;
   rxq->refresh_func = refresh_func;
+}
+
+void efct_vi_start_rxq(ef_vi* vi, int ix)
+{
+  /* This is a totally fake pkt_id, but it makes efct_poll_rx() think that a
+   * rollover is needed. We use +1 as a marker that this is the first packet,
+   * i.e. ignore the first metadata: */
+  vi->ep_state->rxq.rxq_ptr[ix].next = 1 + vi->efct_shm[ix].superbuf_pkts;
 }
 
 /* efct_vi_detach_rxq not yet implemented */

src/lib/efthrm/tcp_helper_resource.c
@@ -931,6 +931,46 @@ int tcp_helper_vi_hw_drop_filter_supported(tcp_helper_resource_t* trs,
 }
 
 
+int tcp_helper_post_filter_add(tcp_helper_resource_t* trs, int hwport,
+                               const struct efx_filter_spec* spec, int rxq,
+                               bool replace)
+{
+  int intf_i;
+  struct efhw_nic* nic;
+  const unsigned HUGEPAGES_PER_RXQ = 2;  /* EFCT TODO: un-hardcode */
+
+  ci_assert_lt((unsigned) hwport, CI_CFG_MAX_HWPORTS);
+  if( (intf_i = trs->netif.hwport_to_intf_i[hwport]) < 0 )
+    return 0;
+
+  nic = efrm_client_get_nic(trs->nic[intf_i].thn_oo_nic->efrm_client);
+  if( efhw_nic_max_shared_rxqs(nic) ) {
+    struct efrm_vi* vi_rs = tcp_helper_vi(trs, intf_i);
+    ef_vi* vi = ci_netif_vi(&trs->netif, intf_i);
+    int qix;
+    int rc;
+
+    ci_assert_ge(rxq, 0);
+    qix = efct_vi_find_free_rxq(vi, rxq);
+    if( qix == -EALREADY )
+      return 0;
+
+    /* EFCT TODO: some hard-coded parameters here: */
+    rc = efrm_rxq_alloc(vi_rs, rxq, qix, cpu_all_mask, true, HUGEPAGES_PER_RXQ,
+                        trs->thc_efct_memfd,
+                        (intf_i * EF_VI_MAX_EFCT_RXQS + qix) *
+                            HUGEPAGES_PER_RXQ * CI_HUGEPAGE_SIZE,
+                        &trs->nic[intf_i].thn_efct_rxq[0]);
+    if( rc < 0 ) {
+      ci_log("%s: ERROR: efrm_rxq_alloc failed (%d)\n", __func__, rc);
+      return rc;
+    }
+    efct_vi_start_rxq(vi, qix);
+  }
+  return 0;
+}
+
+
 #if CI_CFG_PIO
 
 static int allocate_pio(tcp_helper_resource_t* trs, int intf_i, 
@@ -1714,26 +1754,6 @@ static int allocate_vis(tcp_helper_resource_t* trs,
       }
     }
 #endif
-
-    /* TODO EFCT: this shouldn't be here, it should be in filter add instead */
-    if( efhw_nic_max_shared_rxqs(nic) ) {
-      const unsigned HUGEPAGES_PER_RXQ = 2;  /* EFCT TODO: un-hardcode */
-      rc = efrm_rxq_alloc(vi_rs, 0, 0, cpu_all_mask, true, HUGEPAGES_PER_RXQ,
-                          trs->thc_efct_memfd,
-                          (intf_i * EF_VI_MAX_EFCT_RXQS) *
-                              HUGEPAGES_PER_RXQ * CI_HUGEPAGE_SIZE,
-                          &trs_nic->thn_efct_rxq[0]);
-      if( rc < 0 ) {
-        ci_log("%s: ERROR: efrm_rxq_alloc failed (%d)\n", __func__, rc);
-        efrm_pd_release(alloc_info.pd);
-        goto error_out;
-      }
-      /* NB: when moving this line to a more appropriate place,
-       * "EFCT_RX_SUPERBUF_BYTES / EFCT_PKT_STRIDE" should turn in to
-       * shm->superbuf_pkts: */
-      vi->ep_state->rxq.rxq_ptr[0].next =
-                                1 + (EFCT_RX_SUPERBUF_BYTES / EFCT_PKT_STRIDE);
-    }
 
 #if CI_CFG_TCP_OFFLOAD_RECYCLER
     /* The TCP plugin uses multiple VIs per intf to distinguish various types

commit a880a121ffd02cbc2041b7ac2c8cd060b735b0b1

ON-13514: ON-13449: fix kernel panic adding filters to clusters
Regression from 0517e558e5854fce2f37695be5fe86d33e54f68f

src/include/onload/tcp_helper_fns.h
@@ -272,6 +272,11 @@ extern int
 tcp_helper_cluster_from_cluster(tcp_helper_resource_t* thr);
 
 extern int
+tcp_helper_cluster_post_filter_add(tcp_helper_cluster_t* thc, int hwport,
+                                   const struct efx_filter_spec* spec,
+                                   int rxq, bool replace);
+
+extern int
 tcp_helper_cluster_dump(tcp_helper_resource_t* thr, void* buf, int buf_len);
 
 extern int tcp_helper_cluster_alloc_thr(const char* name,

src/lib/efthrm/tcp_filters.c
@@ -305,17 +305,27 @@ oo_hw_filter_set_hwport(struct oo_hw_filter* oofilter, int hwport,
       rc = 0;
     }
     if( rc == 0 && oofilter->filter_id[hwport] >= 0 ) {
-      rc = tcp_helper_post_filter_add(oofilter->trs, hwport, &spec, rxq,
-                                      replace);
-      if( rc < 0 ) {
-        if( ! replace )
-          efrm_filter_remove(get_client(hwport), oofilter->filter_id[hwport]);
-        /* We ideally want to restore replaced filters too, but we're not
-         * keeping enough info to know how. In any case, we currently have no
-         * hardware on which this matters (no hardware uses both a non-trivial
-         * tcp_helper_post_filter_add() and the replace option), so it'd be
-         * untested code. If/when that hardware comes, there are many possible
-         * locations for the solution. */
+      /* post_filter_add only has an effect for NICs which share queues between
+       * stacks, therefore is fundamentally at odds with RSS. This could (like
+       * everything) change in the future, but it's difficult to predict in
+       * what way. */
+      if( ! kernel_redirect ) {
+        if( cluster )
+          rc = tcp_helper_cluster_post_filter_add(oofilter->thc, hwport, &spec,
+                                                  rxq, replace);
+        else
+          rc = tcp_helper_post_filter_add(oofilter->trs, hwport, &spec, rxq,
+                                          replace);
+        if( rc < 0 ) {
+          if( ! replace )
+            efrm_filter_remove(get_client(hwport), oofilter->filter_id[hwport]);
+          /* We ideally want to restore replaced filters too, but we're not
+           * keeping enough info to know how. In any case, we currently have no
+           * hardware on which this matters (no hardware uses both a non-trivial
+           * tcp_helper_post_filter_add() and the replace option), so it'd be
+           * untested code. If/when that hardware comes, there are many possible
+           * locations for the solution. */
+        }
       }
     }
   }

src/lib/efthrm/tcp_filters_deps.h
@@ -5,6 +5,7 @@
 
 #include <onload/debug.h>
 #include <onload/tcp_helper_endpoint.h>
+#include <onload/tcp_helper_fns.h>
 #include <onload/driverlink_filter.h>
 #include <ci/efrm/efrm_client.h>
 #include <ci/efrm/efrm_filter.h>

src/lib/efthrm/tcp_helper_cluster.c
@@ -1615,6 +1615,23 @@ unlock_sock:
 }
 
 
+int tcp_helper_cluster_post_filter_add(tcp_helper_cluster_t* thc, int hwport,
+                                       const struct efx_filter_spec* spec,
+                                       int rxq, bool replace)
+{
+  ci_dllink* link;
+  int rc = 0;
+
+  CI_DLLIST_FOR_EACH(link, &thc->thc_thr_list) {
+    tcp_helper_resource_t* trs = CI_CONTAINER(tcp_helper_resource_t,
+                                              thc_thr_link, link);
+    rc = tcp_helper_post_filter_add(trs, hwport, spec, rxq, replace);
+    if( rc < 0 )
+      break;
+  }
+  return rc;
+}
+
 
 /****************************************************************
 Cluster dump functions

src/tests/onload/oof/stack_interface.c
@@ -46,3 +46,10 @@ int tcp_helper_post_filter_add(tcp_helper_resource_t* trs, int hwport,
 {
   return 0;
 }
+
+int tcp_helper_cluster_post_filter_add(tcp_helper_cluster_t* thc, int hwport,
+                                       const struct efx_filter_spec* spec,
+                                       int rxq, bool replace)
+{
+  return 0;
+}

src/tests/onload/oof/stack_interface.h
@@ -43,4 +43,8 @@ int tcp_helper_post_filter_add(tcp_helper_resource_t* trs, int hwport,
                                const struct efx_filter_spec* spec, int rxq,
                                bool replace);
 
+int tcp_helper_cluster_post_filter_add(tcp_helper_cluster_t* thc, int hwport,
+                                       const struct efx_filter_spec* spec,
+                                       int rxq, bool replace);
+
 #endif /* __OOF_TEST_STACK_INTERFACE_H__ */