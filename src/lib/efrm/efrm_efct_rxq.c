Xilinx-CNS/onload

commit 6ebfe5de259e2c2e6da6df1e65aeb926a65e95b6

ON-13099: [2/3] ef_vi X3 rx wakeups: in-kernel rxq dispatch and priming

src/lib/efrm/efrm_efct_rxq.c
@@ -69,7 +69,8 @@ int efrm_rxq_alloc(struct efrm_vi *vi, int qid, int shm_ix,
 	rxq->vi = vi;
 	rc = efct_nic_rxq_bind(vi_rs->rs_client->nic, qid, mask, timestamp_req,
 	                       n_hugepages, memfd, memfd_off,
-	                       &vi->efct_shm[shm_ix], &rxq->hw);
+	                       &vi->efct_shm[shm_ix], vi->rs.rs_instance,
+						   &rxq->hw);
 	if (rc < 0) {
 		kfree(rxq);
 		return rc;
@@ -233,6 +234,16 @@ int efrm_rxq_refresh_kernel(struct efhw_nic *nic, int hwqid,
 #endif /* CI_HAVE_EFCT_AUX */
 }
 EXPORT_SYMBOL(efrm_rxq_refresh_kernel);
+
+
+void efrm_rxq_request_wakeup(struct efrm_efct_rxq *rxq, unsigned sbseq,
+                             unsigned pktix)
+{
+	struct efhw_nic *nic = rxq->vi->rs.rs_client->nic;
+	efct_request_wakeup(nic->arch_extra, &rxq->hw, sbseq, pktix);
+}
+EXPORT_SYMBOL(efrm_rxq_request_wakeup);
+
 
 
 static void efrm_rxq_rm_dtor(struct efrm_resource_manager *rm)

src/driver/linux_char/vi_resource_event.c
@@ -3,9 +3,11 @@
 #include <ci/efrm/vi_resource_manager.h>
 #include <ci/efrm/efrm_nic.h>
 #include <ci/efrm/efrm_client.h>
+#include <ci/efrm/efct_rxq.h>
 #include <ci/driver/efab/hardware.h>
 #include <ci/driver/internal.h>
 #include <ci/efrm/driver_private.h> /* FIXME for efrm_rm_table */
+#include <ci/efch/op_types.h>
 #include "char_internal.h"
 #include "linux_char_internal.h"
 
@@ -201,8 +203,8 @@ static int efab_vi_rm_prime_cb(void *arg, int is_timeout,
 }
 
 
-static int efab_vi_rm_prime(struct efrm_vi* virs, ci_private_char_t* priv,
-                            unsigned current_ptr)
+static bool efab_vi_prepare_request_wakeup(struct efrm_vi* virs,
+                                           ci_private_char_t* priv)
 {
   struct efhw_nic* nic = efrm_client_get_nic(virs->rs.rs_client);
   struct efrm_nic_per_vi* cb_info = &efrm_nic(nic)->vis[virs->rs.rs_instance];
@@ -210,41 +212,97 @@ static int efab_vi_rm_prime(struct efrm_vi* virs, ci_private_char_t* priv,
 
   priv->cpcp_readable = 0;
   bit = test_and_set_bit(VI_RESOURCE_EVQ_STATE_WAKEUP_PENDING, &cb_info->state);
-  if( ! bit )
-    efrm_eventq_request_wakeup(virs, current_ptr / sizeof(efhw_event_t));
+  return ! bit;
+}
+
+
+static int efab_vi_rm_prime_lookup_vi(ci_private_char_t* priv,
+                                      efch_resource_id_t vi_rs_id,
+                                      bool expect_rxqs)
+{
+  int rc = 0;
+
+  if(likely( priv->cpcp_vi ))
+    return 0;
+  /* First time: Find the VI object. */
+  mutex_lock(&efch_mutex);
+  if( priv->cpcp_vi == NULL ) {
+    efch_resource_t* rs;
+    rc = efch_resource_id_lookup(vi_rs_id, &priv->rt, &rs);
+    if( rc == 0 ) {
+      if( rs->rs_base->rs_type == EFRM_RESOURCE_VI ) {
+        struct efrm_vi* virs = efrm_vi(rs->rs_base);
+        struct efhw_nic* nic = efrm_client_get_nic(virs->rs.rs_client);
+        if( (efhw_nic_max_shared_rxqs(nic) != 0) != expect_rxqs )
+          rc = -EINVAL;
+        else
+          rc = efrm_eventq_register_callback(virs, efab_vi_rm_prime_cb, priv);
+        if( rc == 0 )
+          priv->cpcp_vi = virs;
+      }
+      else {
+        rc = -EOPNOTSUPP;
+      }
+    }
+  }
+  mutex_unlock(&efch_mutex);
+  return rc;
+}
+
+
+/* Prime a simple VI (i.e. an evq) for interrupts */
+int efch_vi_prime(ci_private_char_t* priv, efch_resource_id_t vi_rs_id,
+                  unsigned current_ptr)
+{
+  int rc = efab_vi_rm_prime_lookup_vi(priv, vi_rs_id, false);
+  if( rc < 0 )
+    return rc;
+
+  if( efab_vi_prepare_request_wakeup(priv->cpcp_vi, priv) )
+    efrm_eventq_request_wakeup(priv->cpcp_vi,
+                               current_ptr / sizeof(efhw_event_t));
   return 0;
 }
 
 
-int efch_vi_prime(ci_private_char_t* priv, efch_resource_id_t vi_rs_id,
-                  unsigned current_ptr)
+/* Prime a multi-queue VI for interrupts, i.e. an efct VI which has multiple
+ * rxqs and 0 or 1 txqs underneath it, without a single evq gathering them all
+ * together. This effectively means to prime each underlying queue in turn. */
+int efch_vi_prime_qs(ci_private_char_t* priv,
+                     const ci_resource_prime_qs_op_t* args)
 {
-  int rc = 0;
+  int rc;
 
-  if( priv->cpcp_vi == NULL ) {
-    /* First time: Find the VI object. */
-    mutex_lock(&efch_mutex);
-    if( priv->cpcp_vi == NULL ) {
-      efch_resource_t* rs;
-      rc = efch_resource_id_lookup(vi_rs_id, &priv->rt, &rs);
-      if( rc == 0 ) {
-        if( rs->rs_base->rs_type == EFRM_RESOURCE_VI ) {
-          struct efrm_vi* virs = efrm_vi(rs->rs_base);
-          rc = efrm_eventq_register_callback(virs, efab_vi_rm_prime_cb, priv);
-          if( rc == 0 )
-            priv->cpcp_vi = virs;
-        }
-        else {
-          rc = -EOPNOTSUPP;
-        }
-      }
-    }
-    mutex_unlock(&efch_mutex);
-  }
+  if( args->n_rxqs > EF_VI_MAX_EFCT_RXQS || args->n_txqs > 1 )
+    return -EINVAL;
+
+  rc = efab_vi_rm_prime_lookup_vi(priv, args->crp_id, true);
   if( rc < 0 )
     return rc;
 
-  return efab_vi_rm_prime(priv->cpcp_vi, priv, current_ptr);
+  /* EFCT TODO: txqs */
+
+  if( efab_vi_prepare_request_wakeup(priv->cpcp_vi, priv) ) {
+    unsigned i;
+    for( i = 0; i < args->n_rxqs; ++i ) {
+      efch_resource_t* rs;
+      rc = efch_resource_id_lookup(args->rxq_current[i].rxq_id, &priv->rt,
+                                   &rs);
+      if( rc < 0 )
+        break;
+      if( rs->rs_base->rs_type != EFRM_RESOURCE_EFCT_RXQ ) {
+        rc = -EINVAL;
+        /* It doesn't really matter if we abort half way after priming some
+         * queues and not others: it only means that some spurious wakes will
+         * happen */
+        break;
+      }
+      efrm_rxq_request_wakeup(efrm_rxq_from_resource(rs->rs_base),
+                              args->rxq_current[i].sbseq,
+                              args->rxq_current[i].pktix);
+    }
+  }
+  return rc;
 }
 
 

src/include/ci/efch/op_types.h
@@ -452,6 +452,18 @@ typedef struct ci_resource_prime_op_s {
   uint32_t           crp_current_ptr;
 } ci_resource_prime_op_t;
 
+typedef struct ci_resource_prime_qs_op_s {
+  efch_resource_id_t crp_id;
+  uint32_t           n_rxqs;
+  uint32_t           n_txqs;
+  struct {
+    efch_resource_id_t rxq_id;
+    uint32_t           sbseq;
+    uint32_t           pktix;
+  } rxq_current[EF_VI_MAX_EFCT_RXQS];
+  uint32_t           txq_current;
+} ci_resource_prime_qs_op_t;
+
 
 /**********************************************************************
  *

src/include/ci/efhw/efct.h
@@ -78,7 +78,7 @@ int efct_nic_rxq_bind(struct efhw_nic *nic, int qid,
                       const struct cpumask *mask, bool timestamp_req,
                       size_t n_hugepages, struct file* memfd, off_t* memfd_off,
                       struct efab_efct_rxq_uk_shm *shm,
-                      struct efhw_efct_rxq *rxq);
+                      unsigned wakeup_instance, struct efhw_efct_rxq *rxq);
 void efct_nic_rxq_free(struct efhw_nic *nic, struct efhw_efct_rxq *rxq,
                        efhw_efct_rxq_free_func_t *freer);
 int efct_get_hugepages(struct efhw_nic *nic, int hwqid,

src/include/ci/efrm/efct_rxq.h
@@ -25,6 +25,8 @@ int efrm_rxq_refresh(struct efrm_efct_rxq *rxq,
                      unsigned max_superbufs);
 int efrm_rxq_refresh_kernel(struct efhw_nic *nic, int hwqid,
                             const char** superbufs);
+void efrm_rxq_request_wakeup(struct efrm_efct_rxq *rxq, unsigned sbseq,
+                             unsigned pktix);
 
 
 #endif

src/lib/efhw/efct.c
@@ -19,7 +19,7 @@ int
 efct_nic_rxq_bind(struct efhw_nic *nic, int qid, const struct cpumask *mask,
                   bool timestamp_req, size_t n_hugepages, struct file* memfd,
                   off_t* memfd_off, struct efab_efct_rxq_uk_shm *shm,
-                  struct efhw_efct_rxq *rxq)
+                  unsigned wakeup_instance, struct efhw_efct_rxq *rxq)
 {
   struct device *dev;
   struct xlnx_efct_device* edev;
@@ -46,6 +46,8 @@ efct_nic_rxq_bind(struct efhw_nic *nic, int qid, const struct cpumask *mask,
   rxq->n_hugepages = n_hugepages;
   rxq->max_allowed_superbufs = n_hugepages * CI_EFCT_SUPERBUFS_PER_PAGE;
   rxq->shm = shm;
+  rxq->wakeup_instance = wakeup_instance;
+  rxq->wake_at_seqno = EFCT_INVALID_PKT_SEQNO;
 
   efct_provide_bind_memfd(memfd, *memfd_off);
   EFCT_PRE(dev, edev, cli, nic, rc);

commit 547cdb36548e253800bfe51e69958b66c67136cc

ON-13099: Fix !x3 build, broken by 6ebfe5de25

src/lib/efrm/efrm_efct_rxq.c
@@ -239,8 +239,10 @@ EXPORT_SYMBOL(efrm_rxq_refresh_kernel);
 void efrm_rxq_request_wakeup(struct efrm_efct_rxq *rxq, unsigned sbseq,
                              unsigned pktix)
 {
+#if CI_HAVE_EFCT_AUX
 	struct efhw_nic *nic = rxq->vi->rs.rs_client->nic;
 	efct_request_wakeup(nic->arch_extra, &rxq->hw, sbseq, pktix);
+#endif
 }
 EXPORT_SYMBOL(efrm_rxq_request_wakeup);
 