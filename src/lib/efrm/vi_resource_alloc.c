Xilinx-CNS/onload

commit 57ab6822180c80fe52b32bc70537fe9680cc372b

ON-13095: X3: handle TX interrupts

src/lib/efrm/vi_resource_alloc.c
@@ -1295,7 +1295,8 @@ efrm_vi_resource_alloc(struct efrm_client *client,
 	 * one VI has one IRQ.
 	 * See ON-10914.
 	 */
-	if (client->nic->devtype.arch == EFHW_ARCH_EF100 &&
+	if ((client->nic->devtype.arch == EFHW_ARCH_EF100 ||
+	     client->nic->devtype.arch == EFHW_ARCH_EFCT) &&
 	    evq_virs == NULL) {
 		rc = efrm_vi_request_irq(virs);
 		if (rc != 0)

src/driver/linux_resource/aux_efct.c
@@ -442,6 +442,7 @@ static int efct_resource_init(struct xlnx_efct_device *edev,
 {
   union xlnx_efct_param_value val;
   int rc;
+  int i;
 
   rc = edev->ops->get_param(client, XLNX_EFCT_NIC_RESOURCES, &val);
   if( rc < 0 )
@@ -450,6 +451,17 @@ static int efct_resource_init(struct xlnx_efct_device *edev,
   res_dim->vi_min = val.nic_res.evq_min;
   res_dim->vi_lim = val.nic_res.evq_lim;
   res_dim->mem_bar = VI_RES_MEM_BAR_UNDEFINED;
+
+  rc = edev->ops->get_param(client, XLNX_EFCT_IRQ_RESOURCES, &val);
+  if( rc < 0 )
+    return rc;
+
+  res_dim->irq_n_ranges = val.irq_res->n_ranges;
+  EFRM_ASSERT(res_dim->irq_n_ranges <= IRQ_N_RANGES_MAX);
+  for( i = 0; i < res_dim->irq_n_ranges; i++ ) {
+      res_dim->irq_ranges[i].irq_base = val.irq_res->irq_ranges[i].vector;
+      res_dim->irq_ranges[i].irq_range = val.irq_res->irq_ranges[i].range;
+  }
 
   return 0;
 }

src/include/ci/efrm/driver_private.h
@@ -89,7 +89,7 @@ struct vi_resource_dimensions {
 	unsigned mem_bar;
 	unsigned vi_stride;
 
-	/* EF100 only */
+	/* EF100/X3 only */
 	/* 8 is an arbitrary number which is empirically larger than any number
 	 * which has been observed to be needed. It can't go too high without
 	 * moving this struct off the stack. */

src/lib/efhw/efct.c
@@ -171,6 +171,7 @@ efct_nic_init_hardware(struct efhw_nic *nic,
   memcpy(nic->mac_addr, mac_addr, ETH_ALEN);
   nic->flags |= NIC_FLAG_TX_CTPIO | NIC_FLAG_CTPIO_ONLY
              | NIC_FLAG_HW_RX_TIMESTAMPING | NIC_FLAG_HW_TX_TIMESTAMPING;
+  efct_nic_tweak_hardware(nic);
   return 0;
 }
 

commit f9f4afdc5661622662acfa9ad56a3e80d7a35812

ON-13095: check NIC_FLAG_EVQ_IRQ instead of NIC arch
Fixes: 57ab6822 ON-13095: X3: handle TX interrupts

src/lib/efrm/vi_resource_alloc.c
@@ -1295,9 +1295,7 @@ efrm_vi_resource_alloc(struct efrm_client *client,
 	 * one VI has one IRQ.
 	 * See ON-10914.
 	 */
-	if ((client->nic->devtype.arch == EFHW_ARCH_EF100 ||
-	     client->nic->devtype.arch == EFHW_ARCH_EFCT) &&
-	    evq_virs == NULL) {
+	if ((client->nic->flags & NIC_FLAG_EVQ_IRQ) && evq_virs == NULL) {
 		rc = efrm_vi_request_irq(virs);
 		if (rc != 0)
 			goto fail_irq;