Xilinx-CNS/onload

commit e772d677227057b3cfecb83b979ffc769b89220d

ON-13440: fix out-of-bound access in /proc/driver/onload/cp_server_pids

src/driver/linux_onload/cplane.c
@@ -2533,19 +2533,17 @@ static void* cp_server_pids_next(struct seq_file* s, void* state_,
 
   state->cp = state->cp->next;
 
-  while( ci_dllist_is_anchor(&cp_hash_table[state->bucket], state->cp) &&
-         (state->bucket < CP_INSTANCE_HASH_SIZE) ) {
+  while( ci_dllist_is_anchor(&cp_hash_table[state->bucket], state->cp) ) {
     state->bucket++;
+    if( state->bucket == CP_INSTANCE_HASH_SIZE ) {
+      /* End of file. */
+      kfree(state);
+      return NULL;
+    }
     state->cp = ci_dllist_head(&cp_hash_table[state->bucket]);
   }
 
   *pos = state->offset;
-
-  if( state->bucket == CP_INSTANCE_HASH_SIZE ) {
-    /* End of file. */
-    kfree(state);
-    return NULL;
-  }
 
   return state;
 }

commit 8f8a11eeaf9d38e7a7b2fc4c754c975f31b232a7

ON-13602: fix buggy .next function cp_server_pids_next
Fix kernel complain about cp_server_pids_next.  Purge cp_pid_seq_state.offset
field, and increment the pos pointer directly.

Somewhat repeats
0714e467a7 ON-13021: fix kernel warning in cp_server_pids_next()
which was broken by
e772d67722 ON-13440: fix out-of-bound access in /proc/driver/onload/cp_server_pids

src/driver/linux_onload/cplane.c
@@ -2519,7 +2519,6 @@ int cp_proc_stats_open(struct inode *inode, struct file *file)
 
 struct cp_pid_seq_state {
   ci_dllink* cp;
-  loff_t offset;
   unsigned bucket;
 };
 
@@ -2529,7 +2528,7 @@ static void* cp_server_pids_next(struct seq_file* s, void* state_,
 {
   struct cp_pid_seq_state* state = state_;
 
-  state->offset++;
+  (*pos)++;
 
   state->cp = state->cp->next;
 
@@ -2542,8 +2541,6 @@ static void* cp_server_pids_next(struct seq_file* s, void* state_,
     }
     state->cp = ci_dllist_head(&cp_hash_table[state->bucket]);
   }
-
-  *pos = state->offset;
 
   return state;
 }
@@ -2560,8 +2557,6 @@ static void* cp_server_pids_start(struct seq_file* s, loff_t* pos)
   if ( !state ) {
     return NULL;
   }
-
-  state->offset = 0;
 
   for( state->bucket = 0;
        state->bucket < CP_INSTANCE_HASH_SIZE;