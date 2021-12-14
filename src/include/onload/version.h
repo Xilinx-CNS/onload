Xilinx-CNS/onload

commit f57b5ccefc080edacc5a65cf911c6d9ea88d0b57

ON-13675: autogenerate ONLOAD_VERSION
Autogenerate onload_version.h file in ciul library, and include it from:
- onload_version.c file in ciul library;
- driver/linux_onload/driver.c for MODULE_VERSION macro.

All the other code does not use ONLOAD_VERSION macro any more; it uses
onload_version variable, exported by ciul library.

src/include/onload/version.h
@@ -15,16 +15,12 @@
 #define __ONLOAD_VERSION_H__
 
 
-#ifndef ONLOAD_VERSION
-# define ONLOAD_VERSION  "<dev-snapshot>"
-#endif
-
-#define ONLOAD_PRODUCT  "Onload"
-
-#define ONLOAD_COPYRIGHT  "Copyright 2019-present Xilinx, 2006-2019 Solarflare Communications, 2002-2005 Level 5 Networks"
+extern const char* onload_version;
+extern const char* onload_product;
+extern const char* onload_copyright;
 
 /* Max length of version string used for version skew checking. */
-enum { OO_VER_STR_LEN = 40 };
+enum { OO_VER_STR_LEN = 80 };
 
 
 /* We use an md5sum over certain headers to ensure that userland and kernel

src/lib/ciul/onload_version.c
@@ -1,0 +1,7 @@
+
+/* Include autogenerated macros */
+#include "onload_version.h"
+
+const char* const onload_product = ONLOAD_PRODUCT;
+const char* const onload_copyright = ONLOAD_COPYRIGHT;
+const char* const onload_version = ONLOAD_VERSION;

src/lib/transport/unix/startup.c
@@ -687,10 +687,9 @@ void _fini(void)
    Ensure that no libc() functions are used */
 void onload_version_msg(void)
 {
-  struct iovec v[1];
-  static const char msg0[] =
-    ONLOAD_PRODUCT" "ONLOAD_VERSION"\n"
-    ONLOAD_COPYRIGHT"\n"
+  const char* msg0[] = {
+    onload_product, " ", onload_version, "\n",
+    onload_copyright, "\n"
     "Built: "__DATE__" "__TIME__" "
 #ifdef NDEBUG
     "(release)"
@@ -698,16 +697,20 @@ void onload_version_msg(void)
     "(debug)"
 #endif
     "\n"
-    "Build profile header: " OO_STRINGIFY(TRANSPORT_CONFIG_OPT_HDR) "\n";
+    "Build profile header: " OO_STRINGIFY(TRANSPORT_CONFIG_OPT_HDR) "\n"};
+#define MSG0_SIZE CI_ARRAY_SIZE(msg0)
+  struct iovec v[MSG0_SIZE];
+  int i;
 
-  v[0].iov_base = (void*) msg0;
-  v[0].iov_len  = sizeof(msg0)-1;
 
-  my_syscall3(writev, STDOUT_FILENO, (long) v, 1);
+  for( i = 0; i < MSG0_SIZE; i++ ) {
+    v[i].iov_base = (char*)msg0[i]; /* discard const qualifier */
+    v[i].iov_len = strlen(msg0[i]);
+  }
+
+  my_syscall3(writev, STDOUT_FILENO, (long) v, MSG0_SIZE);
   my_syscall3(exit, 0, 0, 0); 
 }
 
-
-const char*const onload_version = ONLOAD_VERSION;
 
 /*! \cidoxg_end */

scripts/lib/mkdist/preprocess_source
@@ -44,11 +44,12 @@ update_product_strings() {
   local product="$2"
   local version="$3"
 
-  try sed -i \
-    -e "/define ONLOAD_VERSION/ c# define ONLOAD_VERSION \"$version\"" \
-    -e "/define ONLOAD_PRODUCT/ c#define ONLOAD_PRODUCT \"$Product\"" \
-    -e "/define ONLOAD_COPYRIGHT/ c#define ONLOAD_COPYRIGHT \"$banner\"" \
-    src/include/onload/version.h
+  (
+    echo "echo \"#define ONLOAD_VERSION  \\\"$version\\\"\""
+    echo "echo \"#define ONLOAD_SHORT_VERSION ONLOAD_VERSION\""
+    echo "echo \"#define ONLOAD_PRODUCT \\\"$Product\\\"\""
+    echo "echo \"#define ONLOAD_COPYRIGHT \\\"$banner\\\"\""
+  ) > scripts/onload_version_gen
   try sed -i \
     "/define pkgversion/ c%define pkgversion $version" \
     scripts/onload_misc/openonload.spec

scripts/onload_mkdist
@@ -160,6 +160,7 @@ docopy scripts                onload_rpmtest
 docopy scripts                libc_compat.sh check_library_presence
 docopy scripts                check_prototype
 docopy scripts                check_header_presence
+docopy scripts                onload_version_gen
 docopy scripts/shell-fns      fns mmake-fns mmakebuildtree_gen disttag
 docopy .                      mk
 

scripts/onload_version_gen
@@ -1,0 +1,17 @@
+#!/bin/sh
+
+echo "// Autogenerated file, do not edit"
+echo "#define ONLOAD_PRODUCT \"Onload\""
+year=`date '+%Y'`
+echo "#define ONLOAD_COPYRIGHT \"Copyright 2019-$year Xilinx, 2006-2019 Solarflare Communications, 2002-2005 Level 5 Networks\""
+echo -n "#define ONLOAD_VERSION \""
+if test -f `which git`; then
+  hash=`git show -s --format="format:%h %cd"  --date=short HEAD`
+  echo -n $hash
+  human_name=`git symbolic-ref -q --short HEAD`
+  test -z "$human_name" && human_name=`git describe --tags --exact-match`
+  test -n "$human_name" && echo -n " $human_name"
+else
+  echo -n "<dev-snapshot>"
+fi
+echo "\""

src/driver/linux_onload/driver.c
@@ -29,6 +29,7 @@
 #include "onload_kernel_compat.h"
 #include <ci/driver/chrdev.h>
 #include <ci/tools.h>
+#include "lib/ciul/onload_version.h"
 
 
 /*--------------------------------------------------------------------

src/driver/linux_onload/linux_stats.c
@@ -314,8 +314,8 @@ static struct proc_ops efab_stacks_k_seq_fops = {
 static int 
 efab_version_read_proc(struct seq_file *seq, void *s)
 {
-  seq_printf(seq, "onload_product: %s\n", ONLOAD_PRODUCT);
-  seq_printf(seq, "onload_version: %s\n", ONLOAD_VERSION);
+  seq_printf(seq, "onload_product: %s\n", onload_product);
+  seq_printf(seq, "onload_version: %s\n", onload_version);
   seq_printf(seq, "uk_intf_ver: %s\n", oo_uk_intf_ver);
   return 0;
 }

commit c9823df2027622bb9fe1b4a79fa5ed7b4595c95e

ON-13675: fix message when executing onload library
When Onload library is executed, libciul is not initialised
and onload_version variable contains NULL.

Fixes: f57b5cc ON-13675: autogenerate ONLOAD_VERSION

src/include/onload/version.h
@@ -15,10 +15,14 @@
 #define __ONLOAD_VERSION_H__
 
 
+/* onload_version and onload_version_private are the same data, but _private
+ * is not exported so can't be overridden by the dynamic linker so has the
+ * correct value at early startup */
 extern const char* onload_version;
-extern const char* onload_short_version;
-extern const char* onload_product;
-extern const char* onload_copyright;
+extern const char onload_version_private[];
+extern const char onload_short_version[];
+extern const char onload_product[];
+extern const char onload_copyright[];
 
 /* Max length of version string used for version skew checking. */
 enum { OO_VER_STR_LEN = 80 };

src/lib/ciapp/onload_info.c
@@ -16,18 +16,18 @@
 #include <ci/app/onload.h>
 
 
-/* If the onload library is present, and defines onload_version, then this
- * will resolve to the onload library.  Otherwise &onload_version will be
+/* If the onload library is present, and defines onload_version_weak, then this
+ * will resolve to the onload library.  Otherwise &onload_version_weak will be
  * null (because it is weak and undefined).
  */
-extern const char*const onload_version __attribute__((weak));
+extern const char*const onload_version_weak __attribute__((weak));
 extern char** environ;
 
 
 int ci_onload_is_active(void)
 {
   const char* ld_preload;
-  if( &onload_version )
+  if( &onload_version_weak )
     return 1;
   ld_preload = getenv("LD_PRELOAD");
   if( ld_preload == NULL )
@@ -45,8 +45,8 @@ void ci_onload_info_dump(FILE* f, const char* pf)
   ld_preload = getenv("LD_PRELOAD");
   if( ld_preload )
     fprintf(f, "%sLD_PRELOAD=%s\n", pf, ld_preload);
-  if( &onload_version )
-    fprintf(f, "%sonload_version=%s\n", pf, onload_version);
+  if( &onload_version_weak )
+    fprintf(f, "%sonload_version=%s\n", pf, onload_version_weak);
   if( ci_onload_is_active() )
     for( p = environ; *p != NULL; ++p )
       if( strncmp("EF_", *p, 3) == 0 )

src/lib/ciul/onload_version.c
@@ -2,7 +2,8 @@
 /* Include autogenerated macros */
 #include "onload_version.h"
 
-const char* const onload_product = ONLOAD_PRODUCT;
-const char* const onload_copyright = ONLOAD_COPYRIGHT;
+const char onload_product[] = ONLOAD_PRODUCT;
+const char onload_copyright[] = ONLOAD_COPYRIGHT;
 const char* const onload_version = ONLOAD_VERSION;
-const char* const onload_short_version = ONLOAD_SHORT_VERSION;
+const char onload_version_private[] = ONLOAD_VERSION;
+const char onload_short_version[] = ONLOAD_SHORT_VERSION;

src/lib/transport/unix/startup.c
@@ -683,12 +683,22 @@ void _fini(void)
 }
 
 
+/* We're not allowed to use libc in onload_version_msg() */
+static size_t local_strlen(const char *s)
+{
+  size_t len = 0;
+  while (*s++)
+    ++len;
+  return len;
+}
+
+
 /* This is called if the library is run as an executable!
    Ensure that no libc() functions are used */
 void onload_version_msg(void)
 {
   const char* msg0[] = {
-    onload_product, " ", onload_version, "\n",
+    onload_product, " ", onload_version_private, "\n",
     onload_copyright, "\n"
     "Built: "__DATE__" "__TIME__" "
 #ifdef NDEBUG
@@ -705,7 +715,7 @@ void onload_version_msg(void)
 
   for( i = 0; i < MSG0_SIZE; i++ ) {
     v[i].iov_base = (char*)msg0[i]; /* discard const qualifier */
-    v[i].iov_len = strlen(msg0[i]);
+    v[i].iov_len = local_strlen(msg0[i]);
   }
 
   my_syscall3(writev, STDOUT_FILENO, (long) v, MSG0_SIZE);