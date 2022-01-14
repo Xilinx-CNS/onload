e9cce35520cb6e132a99962403452aa380a369a6
Andrew Bower <andrew.bower@xilinx.com>
2022-01-14 16:33:10 +0000

    cleanup: update imports.mk

--- imports.mk
+++ imports.mk
@@ -1,18 +1,18 @@
 # SPDX-License-Identifier: BSD-2-Clause
-# X-SPDX-Copyright-Text: (c) Copyright 2009-2020 Xilinx, Inc.
+# X-SPDX-Copyright-Text: (c) Copyright 2009-2022 Xilinx, Inc.
 
 # please restrict this file to [REPO]_VERSION=[VERSION] and # comments
 
 # This refers to the version of the sfutils codebase and does not contain any
 # information about the firmware versions included in sfupdate.
-SFUTILS_VERSION=v8_1_4_1000
+SFUTILS_VERSION=v8_2_4_1003
 
 # Release collection includes different firmware family versions
 # for different products ranges and within these different selections of
 # firmware images per product. The version number below is two levels of
 # indirection above M/C firmware version.
 #
-# Note: FIRMWARE_VERSIN or FIRMWAREFAMILY_VERSION is deprecated and works
+# Note: FIRMWARE_VERSION or FIRMWAREFAMILY_VERSION is deprecated and works
 # with legacy sfutils versions.
-RELEASECOLLECTION_VERSION=v7.8.4.1000-internal
+RELEASECOLLECTION_VERSION=v8.5.2.1000-internal
 
