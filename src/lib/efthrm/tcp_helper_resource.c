Xilinx-CNS/onload

commit da14d90c0c1464e7dfcbc45d27a62873bf9f7e48

ON-13374: Track which efct rxqs are enabled by bitmask rather than struct member
Improves latency of apps not using EF_VI_EFCT_UNIQUEUE. No effect on
apps which were using it.

src/lib/efthrm/tcp_helper_resource.c
@@ -1468,8 +1468,8 @@ static int allocate_vi(ci_netif* ni, struct vi_allocate_info* info,
   } while( feature_mask && feature_mask-- >= required_feature_mask );
 
   n_shm_rxqs = efhw_nic_max_shared_rxqs(efrm_client_get_nic(info->client));
-  info->vi_efct_shm_mmap_bytes = CI_ROUND_UP(sizeof(*evq_virs->efct_shm) *
-                                             n_shm_rxqs, PAGE_SIZE);
+  info->vi_efct_shm_mmap_bytes = CI_ROUND_UP(CI_EFCT_SHM_BYTES(n_shm_rxqs),
+                                             PAGE_SIZE);
 
   if( rc < 0 ) {
     OO_DEBUG_VM (ci_log ("%s: ERROR: efrm_vi_resource_alloc(%d) failed %d",
@@ -1495,7 +1495,7 @@ static int allocate_vi(ci_netif* ni, struct vi_allocate_info* info,
 
 static int tcp_helper_superbuf_config_refresh(ef_vi* vi, int qid)
 {
-  return efrm_rxq_refresh_kernel(vi->dh, vi->efct_shm[qid].qid,
+  return efrm_rxq_refresh_kernel(vi->dh, vi->efct_shm->q[qid].qid,
                                  vi->efct_rxq[qid].superbufs);
 }
 

src/driver/linux_char/efct_rxq.c
@@ -127,14 +127,6 @@ rxq_rm_rsops(efch_resource_t* rs, ci_resource_table_t* priv_opt,
 }
 
 
-static int rxq_rm_mmap_bytes(struct efrm_resource* rs, int map_type)
-{
-  if( map_type != 0 )
-    return -EINVAL;
-  return round_up(sizeof(struct efab_efct_rxq_uk_shm), PAGE_SIZE);
-}
-
-
 efch_resource_ops efch_efct_rxq_ops = {
   .rm_alloc  = rxq_rm_alloc,
   .rm_free   = rxq_rm_free,
@@ -142,5 +134,5 @@ efch_resource_ops efch_efct_rxq_ops = {
   .rm_nopage = NULL,
   .rm_dump   = NULL,
   .rm_rsops  = rxq_rm_rsops,
-  .rm_mmap_bytes = rxq_rm_mmap_bytes,
+  .rm_mmap_bytes = NULL,
 };

src/driver/linux_resource/efct_superbuf.c
@@ -246,7 +246,7 @@ __efct_nic_rxq_bind(struct xlnx_efct_device* edev,
                     struct xlnx_efct_rxq_params *rxq_params,
                     struct efhw_nic_efct *efct,
                     int n_hugepages,
-                    struct efab_efct_rxq_uk_shm *shm,
+                    struct efab_efct_rxq_uk_shm_q *shm,
                     unsigned wakeup_instance,
                     struct efhw_efct_rxq *rxq)
 {
@@ -297,6 +297,7 @@ __efct_nic_rxq_free(struct xlnx_efct_device* edev,
                     struct efhw_efct_rxq *rxq,
                     efhw_efct_rxq_free_func_t *freer)
 {
+  rxq->shm->superbuf_pkts = 0;
   rxq->destroy = true;
   rxq->freer = freer;
   edev->ops->free_rxq(cli, rxq->qid, rxq->n_hugepages);

src/driver/linux_resource/efct_superbuf.h
@@ -14,7 +14,7 @@ __efct_nic_rxq_bind(struct xlnx_efct_device* edev,
                     struct xlnx_efct_rxq_params *rxq_params,
                     struct efhw_nic_efct *efct,
                     int n_hugepages,
-                    struct efab_efct_rxq_uk_shm *shm,
+                    struct efab_efct_rxq_uk_shm_q *shm,
                     unsigned wakeup_instance,
                     struct efhw_efct_rxq *rxq);
 

src/include/ci/efhw/efct.h
@@ -22,7 +22,7 @@ typedef void efhw_efct_rxq_int_wake_func_t(struct efhw_efct_rxq*);
 
 struct efhw_efct_rxq {
   struct efhw_efct_rxq *next;
-  struct efab_efct_rxq_uk_shm *shm;
+  struct efab_efct_rxq_uk_shm_q *shm;
   unsigned qid;
   bool destroy;
   uint32_t next_sbuf_seq;
@@ -106,7 +106,7 @@ struct efhw_nic_efct {
 int efct_nic_rxq_bind(struct efhw_nic *nic, int qid,
                       const struct cpumask *mask, bool timestamp_req,
                       size_t n_hugepages, struct file* memfd, off_t* memfd_off,
-                      struct efab_efct_rxq_uk_shm *shm,
+                      struct efab_efct_rxq_uk_shm_q *shm,
                       unsigned wakeup_instance, struct efhw_efct_rxq *rxq);
 void efct_nic_rxq_free(struct efhw_nic *nic, struct efhw_efct_rxq *rxq,
                        efhw_efct_rxq_free_func_t *freer);

src/include/ci/efrm/vi_resource_manager.h
@@ -187,7 +187,7 @@ struct efrm_vi {
 	struct efrm_vi_set *vi_set;
 	struct efrm_bt_manager bt_manager;
 	struct efrm_vi_q q[EFHW_N_Q_TYPES];
-	struct efab_efct_rxq_uk_shm *efct_shm;
+	struct efab_efct_rxq_uk_shm_base *efct_shm;
 
 	int net_drv_wakeup_channel;
 	struct efrm_interrupt_vector *vec;

src/include/ci/internal/ip_types.h
@@ -139,7 +139,7 @@ struct ci_netif_s {
 #endif
   char*                buf_ptr;
 #endif
-  struct efab_efct_rxq_uk_shm* efct_shm_ptr;
+  struct efab_efct_rxq_uk_shm_base* efct_shm_ptr;
 
 #ifdef __ci_driver__
   cicp_hwport_mask_t   hwport_mask; /* hwports accelearted by the stack */

src/include/etherfabric/ef_vi.h
@@ -957,9 +957,8 @@ typedef struct ef_vi {
   int                           vi_qs_n;
   /** Attached rxqs for efct VIs (NB: not necessarily in rxq order) */
   ef_vi_efct_rxq                efct_rxq[EF_VI_MAX_EFCT_RXQS];
-  /** efct kernel/userspace shared queue area. Array of length
-   * EF_VI_MAX_EFCT_RXQS with the same indices as efct_rxq */
-  struct efab_efct_rxq_uk_shm*  efct_shm;
+  /** efct kernel/userspace shared queue area. */
+  struct efab_efct_rxq_uk_shm_base* efct_shm;
   /** 1 + highest allowed index of a used element in efct_rxq */
   int                           max_efct_rxq;
 

commit 4aa53d2b362a07964fe8cde5b2d560303eb8e957

ON-13374: Fix startup assert on non-X3 hardware
Introduced by da14d90c0c

src/lib/efthrm/tcp_helper_resource.c
@@ -1468,8 +1468,8 @@ static int allocate_vi(ci_netif* ni, struct vi_allocate_info* info,
   } while( feature_mask && feature_mask-- >= required_feature_mask );
 
   n_shm_rxqs = efhw_nic_max_shared_rxqs(efrm_client_get_nic(info->client));
-  info->vi_efct_shm_mmap_bytes = CI_ROUND_UP(CI_EFCT_SHM_BYTES(n_shm_rxqs),
-                                             PAGE_SIZE);
+  info->vi_efct_shm_mmap_bytes =
+        n_shm_rxqs ? CI_ROUND_UP(CI_EFCT_SHM_BYTES(n_shm_rxqs), PAGE_SIZE) : 0;
 
   if( rc < 0 ) {
     OO_DEBUG_VM (ci_log ("%s: ERROR: efrm_vi_resource_alloc(%d) failed %d",