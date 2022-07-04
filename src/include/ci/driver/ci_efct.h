f590d2bcfff41999e38cb1745ebf30608180b2a8
Maciej Jablonski <maciejj@xilinx.com>
2022-07-04 09:57:58 +0100

    ON-14165: fix resolution of xlnx_efct.h header on debian
    Follow exactly the pattern used for auxiliary bus module
    
    Change-Id: I60e188ca16d8358580e9d3b31cc9d3e0f03be9f1

--- src/include/ci/driver/ci_efct.h
+++ src/include/ci/driver/ci_efct.h
@@ -6,9 +6,16 @@
 
 #include <ci/driver/ci_aux.h>
 
-#if CI_HAVE_AUX_BUS && CI_HAVE_X3_NET
+#if CI_HAVE_AUX_BUS
+#ifdef EFRM_HAS_XLNX_EFCT_H
+  #include <linux/net/xilinx/xlnx_efct.h>
+  #define CI_HAVE_EFCT_AUX 1
+#elif CI_HAVE_X3_NET
   #include CI_XLNX_EFCT_HEADER
   #define CI_HAVE_EFCT_AUX 1
+#else
+  #define CI_HAVE_EFCT_AUX 0
+#endif
 #else
   #define CI_HAVE_EFCT_AUX 0
 #endif
