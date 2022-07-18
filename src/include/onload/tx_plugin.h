7cd68d9d86a41d45b4f987a2c01bb97cf4073301
Samuel McHale <samuelm@amd.com>
2022-07-18 15:56:22 +0100

    VIRTBLK-1887: renaming messages

--- src/include/onload/tx_plugin.h
+++ src/include/onload/tx_plugin.h
@@ -9,22 +9,22 @@
 #include <stdint.h>
 #endif
 
-static const uint8_t XSN_STORAGE_TXPLUGIN[] = {
+static const uint8_t XSN_STORAGE_TX[] = {
   0x44, 0xe5, 0x62, 0xd2, 0xdd, 0xc9, 0x4c, 0xb3,
   0xaf, 0x0b, 0xe4, 0xfd, 0x18, 0x79, 0x19, 0x51};
 
-struct xsn_storage_txplugin {
+struct xsn_storage_tx_read_debug  {
     union {
         uint32_t debug_id;
         uint32_t debug_data;
     };
 };
 
-struct xsn_storage_txplugin_create_app {
+struct xsn_storage_tx_create_app {
   uint16_t vi_id;
 } __attribute__((aligned(4)));
 
-#define XSN_STORAGE_TXPLUGIN_READ_DEBUG 0
-#define XSN_STORAGE_TXPLUGIN_CREATE_APP 1
+#define XSN_STORAGE_TX_READ_DEBUG 0
+#define XSN_STORAGE_TX_CREATE_APP 1
 
 #endif
