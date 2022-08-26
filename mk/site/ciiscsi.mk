7b81eb762e508bce575594d9928449db93635214
Siân James <sian.james@xilinx.com>
2022-08-26 16:33:27 +0100

    ON-14333: Purge unused makefiles.
    (cherry picked from commit 3214d11846e07ecb844dec4fbaec00f15b673d19)

--- mk/site/ciiscsi.mk
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause
-# X-SPDX-Copyright-Text: (c) Copyright 2005-2020 Xilinx, Inc.
-lib_ver   := 1
-lib_name  := ciiscsi
-lib_where := lib/iscsi
-CIISCSI_LIB		:= $(MMakeGenerateLibTarget)
-CIISCSI_LIB_DEPEND	:= $(MMakeGenerateLibDepend)
-LINK_CIISCSI_LIB	:= $(MMakeGenerateLibLink)
-
--- mk/site/efhw.mk
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause
-# X-SPDX-Copyright-Text: (c) Copyright 2010-2020 Xilinx, Inc.
-lib_ver   := 1
-lib_name  := efhw
-lib_where := lib/efhw
-EFHW_LIB		:= $(MMakeGenerateLibTarget)
-EFHW_LIB_DEPEND		:= $(MMakeGenerateLibDepend)
-LINK_EFHW_LIB		:= $(MMakeGenerateLibLink)
