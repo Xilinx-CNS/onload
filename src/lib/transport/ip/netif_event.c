45aa0c206865e6927a12b39e19cbe8aad9ad18e1
Krish De Souza <Krish.De-Souza@amd.com>
2022-12-01 10:44:10 +0000

    ON-14496: Added mapping from efct errors to  ef10 errors

--- src/lib/transport/ip/netif_event.c
+++ src/lib/transport/ip/netif_event.c
@@ -535,6 +535,12 @@ static unsigned convert_discard_flags_efct_ef10(unsigned flags)
     return EF_EVENT_RX_DISCARD_INNER_CSUM_BAD;
   if( flags & EF_VI_DISCARD_RX_INNER_L3_CSUM_ERR )
     return EF_EVENT_RX_DISCARD_INNER_CSUM_BAD;
+  if( flags & EF_VI_DISCARD_RX_L2_CLASS_OTHER )
+    return EF_EVENT_RX_DISCARD_OTHER;
+  if( flags & EF_VI_DISCARD_RX_L3_CLASS_OTHER )
+    return EF_EVENT_RX_DISCARD_OTHER;
+  if( flags & EF_VI_DISCARD_RX_L4_CLASS_OTHER )
+    return EF_EVENT_RX_DISCARD_OTHER;
   return 0;
 }
 
