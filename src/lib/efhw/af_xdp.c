Xilinx-CNS/onload

commit cf9a523901fac870e72aee1f385550e5b8f58f05

ON-14118: Move accept_vi_constraints to efhw hal.
We already have two hardware specific tests here, and we want a third for efct.

src/lib/efhw/af_xdp.c
@@ -1041,6 +1041,13 @@ static void af_xdp_nic_sw_event(struct efhw_nic *nic, int data, int evq)
 	EFHW_ERR("%s: FIXME AF_XDP", __FUNCTION__);
 }
 
+static bool af_xdp_accept_vi_constraints(struct efhw_nic *nic, int low,
+					 unsigned order, void* arg)
+{
+	struct efhw_vi_constraints *avc = arg;
+	return avc->channel == low;
+}
+
 /*--------------------------------------------------------------------
  *
  * EF10 specific event callbacks
@@ -1591,6 +1598,7 @@ struct efhw_func_ops af_xdp_char_functional_units = {
 	af_xdp_nic_wakeup_request,
 	af_xdp_nic_sw_event,
 	af_xdp_handle_event,
+	af_xdp_accept_vi_constraints,
 	af_xdp_dmaq_tx_q_init,
 	af_xdp_dmaq_rx_q_init,
 	af_xdp_flush_tx_dma_channel,

src/driver/linux_resource/driverlink_new.c
@@ -444,6 +444,7 @@ efrm_dl_probe(struct efx_dl_device *efrm_dev,
 
 	nic = &lnic->efrm_nic.efhw_nic;
 	nic->mtu = net_dev->mtu + ETH_HLEN; /* ? + ETH_VLAN_HLEN */
+	nic->rss_channel_count = ef10_res->rss_channel_count;
 	efrm_dev->priv = nic;
 
 	efrm_notify_nic_probe(net_dev);

src/include/ci/driver/efab/hardware.h
@@ -101,6 +101,9 @@
 #define efhw_nic_handle_event(nic, ev, budget) \
 	((nic)->efhw_func->handle_event((nic), (ev), (budget)))
 
+#define efhw_nic_accept_vi_constraints(nic, low, order, arg) \
+	((nic)->efhw_func->accept_vi_constraints((nic), (low), (order), (arg)))
+
 /*-------------- DMA support  ------------ */
 #define efhw_nic_dmaq_tx_q_init(nic, client_id, params) \
 	((nic)->efhw_func->dmaq_tx_q_init((nic), (client_id), (params)))

src/include/ci/efhw/efhw_types.h
@@ -149,6 +149,12 @@ struct efhw_dmaq_params {
 	};
 };
 
+struct efhw_vi_constraints {
+	int channel;
+	int min_vis_in_set;
+	int has_rss_context;
+};
+
 /**********************************************************************
  * Portable HW interface. ***************************************
  **********************************************************************/
@@ -207,6 +213,9 @@ struct efhw_func_ops {
 	/*! Handle an event from hardware, e.g. delivered via driverlink */
 	int (*handle_event) (struct efhw_nic *nic, efhw_event_t *ev,
 			     int budget);
+
+	bool (*accept_vi_constraints) (struct efhw_nic *nic, int low,
+				       unsigned order, void* arg);
 
   /*-------------- DMA support  ------------ */
 
@@ -519,6 +528,7 @@ struct efhw_nic {
 		unsigned base;
 		unsigned range;
 	} vi_irq_ranges[NIC_IRQ_MAX_RANGES];
+	unsigned rss_channel_count;
 
 	/* Size of PIO buffer */
 	unsigned pio_size;

src/lib/efhw/ef10.c
@@ -1158,6 +1158,24 @@ static void ef10_nic_sw_event(struct efhw_nic *nic, int data, int evq)
 	EFHW_TRACE("%s: evq[%d]->%x", __FUNCTION__, evq, data);
 }
 
+
+bool ef10_ef100_accept_vi_constraints(struct efhw_nic *nic, int low,
+				      unsigned order, void* arg)
+{
+	struct efhw_vi_constraints *avc = arg;
+	int high = low + avc->min_vis_in_set;
+	int ok = 1;
+	if ((avc->min_vis_in_set > 1) && (!avc->has_rss_context)) {
+		/* We need to ensure that if an RSS-enabled filter is
+		 * pointed at this VI-set then the queue selected will be
+		 * within the default set.  The queue selected by RSS will be 
+		 * in the range (low | (rss_channel_count - 1)).
+		 */
+		ok &= ((low | (nic->rss_channel_count - 1)) < high);
+	}
+	return ok;
+}
+
 /*--------------------------------------------------------------------
  *
  * EF10 specific event callbacks
@@ -2598,6 +2616,7 @@ struct efhw_func_ops ef10_char_functional_units = {
 	ef10_nic_wakeup_request,
 	ef10_nic_sw_event,
 	ef10_handle_event,
+	ef10_ef100_accept_vi_constraints,
 	ef10_dmaq_tx_q_init,
 	ef10_dmaq_rx_q_init,
 	ef10_ef100_flush_tx_dma_channel,

src/lib/efhw/ef100.c
@@ -796,6 +796,7 @@ struct efhw_func_ops ef100_char_functional_units = {
 	ef100_nic_wakeup_request,
 	ef100_nic_sw_event,
 	ef100_handle_event,
+	ef10_ef100_accept_vi_constraints,
 	ef100_dmaq_tx_q_init,
 	ef100_dmaq_rx_q_init,
 	ef10_ef100_flush_tx_dma_channel,

src/lib/efhw/ef10_ef100.h
@@ -42,6 +42,9 @@ extern int ef10_ef100_mcdi_cmd_event_queue_enable(struct efhw_nic *nic,
 extern void ef10_ef100_mcdi_cmd_event_queue_disable(struct efhw_nic *nic,
 						    uint32_t client_id, uint evq);
 
+extern bool ef10_ef100_accept_vi_constraints(struct efhw_nic *nic, int low,
+					     unsigned order, void* arg);
+
 extern void ef10_ef100_mcdi_cmd_driver_event(struct efhw_nic *nic, uint64_t data,
 					     uint32_t evq);
 

src/lib/efhw/efct.c
@@ -324,6 +324,12 @@ static void efct_nic_sw_event(struct efhw_nic *nic, int data, int evq)
 {
 }
 
+static bool efct_accept_vi_constraints(struct efhw_nic *nic, int low,
+                                       unsigned order, void* arg)
+{
+  return true;
+}
+
 /*--------------------------------------------------------------------
  *
  * EF10 specific event callbacks
@@ -1381,6 +1387,7 @@ struct efhw_func_ops efct_char_functional_units = {
   efct_nic_wakeup_request,
   efct_nic_sw_event,
   efct_handle_event,
+  efct_accept_vi_constraints,
   efct_dmaq_tx_q_init,
   efct_dmaq_rx_q_init,
   efct_flush_tx_dma_channel,

commit ac06dbf68ff6efcc5011ac14225b98ed04beceda

af_xdp: re-enable AF_XDP stacks
Fixes: cf9a523901 ("ON-14118: Move accept_vi_constraints to efhw hal.")

src/lib/efhw/af_xdp.c
@@ -1045,7 +1045,8 @@ static bool af_xdp_accept_vi_constraints(struct efhw_nic *nic, int low,
 					 unsigned order, void* arg)
 {
 	struct efhw_vi_constraints *avc = arg;
-	return avc->channel == low;
+	/* The only constrain is "no RSS, no sets". */
+	return avc->min_vis_in_set == 1 && avc->has_rss_context == 0;
 }
 
 /*--------------------------------------------------------------------