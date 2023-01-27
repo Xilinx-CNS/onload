d3e61f0e406ed0f8b236e88dc1177e1201e354e2
Sergey Nikitin <sergey.nikitin@oktetlabs.ru>
2023-01-27 19:12:21 +0300

    aarch64: use kallsyms only if it is available
    Starting from 5.7 kallsyms_on_each_symbol() is not exported for
    modules, so we can't rely on it.

--- src/driver/linux_resource/syscall_aarch64.c
+++ src/driver/linux_resource/syscall_aarch64.c
@@ -10,11 +10,22 @@ void** efrm_syscall_table = NULL;
 EXPORT_SYMBOL(efrm_syscall_table);
 
 
-static typeof(aarch64_insn_decode_immediate) *aarch64_insn_decode_immediate_sym;
-static typeof(aarch64_insn_read) *aarch64_insn_read_sym;
-static typeof(aarch64_insn_extract_system_reg) *aarch64_insn_extract_system_reg_sym;
-static typeof(aarch64_get_branch_offset) *aarch64_get_branch_offset_sym;
-static typeof(aarch64_insn_adrp_get_offset) *aarch64_insn_adrp_get_offset_sym;
+/* The kernel contains some neat routines for doing AArch64 code inspection.
+ * Some of them are available as inline routines, but some are unfortunately
+ * not exported to modules.
+ * So we use efrm_find_ksym() to resolve these functions if it is available.
+ * Otherwise we copy their implementations here.
+ */
+#ifdef EFRM_HAVE_NEW_KALLSYMS
+static typeof(aarch64_insn_decode_immediate) *ci_aarch64_insn_decode_immediate;
+static typeof(aarch64_insn_read) *ci_aarch64_insn_read;
+static typeof(aarch64_insn_extract_system_reg) *ci_aarch64_insn_extract_system_reg;
+static typeof(aarch64_get_branch_offset) *ci_aarch64_get_branch_offset;
+static typeof(aarch64_insn_adrp_get_offset) *ci_aarch64_insn_adrp_get_offset;
+#else
+#include "ci_arm64_patching.h"
+#include "ci_arm64_insn.h"
+#endif /* EFRM_HAVE_NEW_KALLSYMS */
 
 /* Depending on the kernel version, locating the syscall table may be more
  * or less straigthforward.
@@ -62,8 +73,11 @@ find_el_sync(void)
 {
   ci_uint8 *vectors;
   ci_uint32 insn;
-  ci_uint8 *el_sync = efrm_find_ksym("el0_sync");
+  ci_uint8 *el_sync = NULL;
 
+#ifdef EFRM_HAVE_NEW_KALLSYMS
+  el_sync = efrm_find_ksym("el0_sync");
+#endif
   if (el_sync != NULL)
     return el_sync;
 
@@ -75,7 +89,7 @@ find_el_sync(void)
   vectors += 5 * sizeof(ci_uint32);
 #endif
 #endif
-  if (aarch64_insn_read_sym(vectors, &insn)) {
+  if (ci_aarch64_insn_read(vectors, &insn)) {
     EFRM_WARN("%s: cannot read vbar_el1", __func__);
     return NULL;
   }
@@ -83,7 +97,7 @@ find_el_sync(void)
     EFRM_WARN("%s: el0_sync entry is not a branch", __func__);
     return NULL;
   }
-  el_sync = vectors + aarch64_get_branch_offset_sym(insn);
+  el_sync = vectors + ci_aarch64_get_branch_offset(insn);
 
   return el_sync;
 }
@@ -91,10 +105,13 @@ find_el_sync(void)
 static ci_uint8 *
 find_el_svc_entry(void)
 {
-  ci_uint8 *el_svc = efrm_find_ksym("el0_svc");
+  ci_uint8 *el_svc = NULL;
   ci_uint8 *el_sync;
   ci_uint32 insn;
 
+#ifdef EFRM_HAVE_NEW_KALLSYMS
+  el_svc = efrm_find_ksym("el0_svc");
+#endif
   if (el_svc != NULL)
     return el_svc;
   el_sync = find_el_sync();
@@ -102,25 +119,25 @@ find_el_svc_entry(void)
     return NULL;
 
   while (1) {
-    if (aarch64_insn_read_sym(el_sync, &insn)) {
+    if (ci_aarch64_insn_read(el_sync, &insn)) {
       EFRM_WARN("cannot read el0_sync code @ %d", __LINE__);
       return NULL;
     }
     if (aarch64_insn_is_mrs(insn) &&
-        aarch64_insn_extract_system_reg_sym(insn) == SYS_ESR_EL1) {
+        ci_aarch64_insn_extract_system_reg(insn) == SYS_ESR_EL1) {
       el_sync += 2 * sizeof(ci_uint32);
-      if (aarch64_insn_read_sym(el_sync, &insn)) {
+      if (ci_aarch64_insn_read(el_sync, &insn)) {
         EFRM_WARN("cannot read el0_sync code @ %d", __LINE__);
         return NULL;
       }
       if (!aarch64_insn_is_subs_imm(insn) ||
-          aarch64_insn_decode_immediate_sym(AARCH64_INSN_IMM_12, insn) !=
+          ci_aarch64_insn_decode_immediate(AARCH64_INSN_IMM_12, insn) !=
           ESR_ELx_EC_SVC64) {
         EFRM_WARN("%s: expected check for ESR_ELx_EC_SVC64", __func__);
         return NULL;
       }
       el_sync += sizeof(ci_uint32);
-      if (aarch64_insn_read_sym(el_sync, &insn)) {
+      if (ci_aarch64_insn_read(el_sync, &insn)) {
         EFRM_WARN("cannot read el0_sync code @ %d", __LINE__);
         return NULL;
       }
@@ -129,7 +146,7 @@ find_el_svc_entry(void)
         EFRM_WARN("%s: branching to el0_svc not found", __func__);
         return NULL;
       }
-      el_svc = el_sync + aarch64_get_branch_offset_sym(insn);
+      el_svc = el_sync + ci_aarch64_get_branch_offset(insn);
       break;
     }
     el_sync += sizeof(ci_uint32);
@@ -146,7 +163,7 @@ find_syscall_table_via_vbar(void)
   if (el_svc == NULL)
     return NULL;
 
-  if (aarch64_insn_read_sym(el_svc, &insn)) {
+  if (ci_aarch64_insn_read(el_svc, &insn)) {
     EFRM_WARN("cannot read el0_svc start @ %016lx", (unsigned long)el_svc);
     return NULL;
   }
@@ -154,41 +171,39 @@ find_syscall_table_via_vbar(void)
     EFRM_WARN("expected adrp instruction at el0_svc, found %08x", insn);
     return NULL;
   }
-  return CI_PTR_ALIGN_BACK(el_svc, SZ_4K) + aarch64_insn_adrp_get_offset_sym(insn);
+  return CI_PTR_ALIGN_BACK(el_svc, SZ_4K) + ci_aarch64_insn_adrp_get_offset(insn);
 }
 
 static void *find_syscall_table(void)
 {
-  void *syscalls = efrm_find_ksym("sys_call_table");
+  void *syscalls = NULL;
 
+#ifdef EFRM_HAVE_NEW_KALLSYMS
+  syscalls = efrm_find_ksym("sys_call_table");
+#endif
   if (syscalls != NULL)
     return syscalls;
 
-/* The kernel contains some neat routines for doing AArch64 code inspection.
- * Some of them are available as inline routines, but some are
- * unfortunately not exported to modules. We could reimplement them, of
- * course, but since we do efrm_find_ksym in other places, that seems like
- * the least effort path
- */
-
-  aarch64_insn_decode_immediate_sym =
+#ifdef EFRM_HAVE_NEW_KALLSYMS
+  ci_aarch64_insn_decode_immediate =
                           efrm_find_ksym("aarch64_insn_decode_immediate");
-  aarch64_insn_read_sym = efrm_find_ksym("aarch64_insn_read");
-  aarch64_insn_extract_system_reg_sym =
+  ci_aarch64_insn_read = efrm_find_ksym("aarch64_insn_read");
+  ci_aarch64_insn_extract_system_reg =
                           efrm_find_ksym("aarch64_insn_extract_system_reg");
-  aarch64_get_branch_offset_sym =
+  ci_aarch64_get_branch_offset =
                           efrm_find_ksym("aarch64_get_branch_offset");
-  aarch64_insn_adrp_get_offset_sym =
+  ci_aarch64_insn_adrp_get_offset =
                           efrm_find_ksym("aarch64_insn_adrp_get_offset");
-  if (aarch64_insn_decode_immediate_sym == NULL ||
-      aarch64_insn_read_sym == NULL ||
-      aarch64_insn_extract_system_reg_sym == NULL ||
-      aarch64_get_branch_offset_sym == NULL ||
-      aarch64_insn_adrp_get_offset_sym == NULL) {
+  if (ci_aarch64_insn_decode_immediate == NULL ||
+      ci_aarch64_insn_read == NULL ||
+      ci_aarch64_insn_extract_system_reg == NULL ||
+      ci_aarch64_get_branch_offset == NULL ||
+      ci_aarch64_insn_adrp_get_offset == NULL) {
     EFRM_WARN("%s: some symbols required for AArch64 assembler analysis "
               "are not found", __func__);
     return NULL;
   }
+#endif
 
   return find_syscall_table_via_vbar();
 }
