c846b8bef1874d39c72b70cbd4d3b03d73be8537
Ivan Teterevkov <ivan.teterevkov@amd.com>
2023-02-14 13:50:47 +0000

    ON-14653: Clarify dependency on EFCT v2
    This patch tightens the HAVE_CNS_EFCT computation and refers explicitly
    to the v2 header file. Previously, HAVE_CNS_EFCT was "1" for both driver
    versions v1 and v2, which deferred compatibility issues to compile time.
    
    Also, it clarifies the error message when EFCT or AUX cannot be found.

--- mk/linux_kbuild.mk
+++ mk/linux_kbuild.mk
@@ -64,7 +64,7 @@ else ifneq ($(wildcard $(dir $(KPATH))/source/include/linux/net/amd/efct_client.
 HAVE_KERNEL_EFCT := 1
 else
 X3_NET_PATH ?= $(TOPPATH)/../x3-net-linux
-HAVE_CNS_EFCT := $(or $(and $(wildcard $(X3_NET_PATH)),1),0)
+HAVE_CNS_EFCT := $(or $(and $(wildcard $(X3_NET_PATH)/include/linux/net/amd/efct_client.h),1),0)
 endif
 
 ifeq ($(or $(filter 1, $(HAVE_KERNEL_EFCT) $(HAVE_CNS_EFCT)),0),1)
@@ -76,7 +76,7 @@ else
   ifneq ($(HAVE_EFCT),1)
     EXTRA_CFLAGS += -DCI_HAVE_EFCT_AUX=0
   else
-    $(error Unable to build Onload with EFCT or AUX bus support)
+    $(error Unable to build Onload with EFCT v2 or AUX bus support)
   endif
 endif
 
