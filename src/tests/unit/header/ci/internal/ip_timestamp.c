fda81bc147199a368a2f38238e310ca5d8a610e6
Mike Seymour <mseymour@xilinx.com>
2023-02-21 11:56:15 +0000

    Cleanup: support unit tests for header-only code

---
+++ src/tests/unit/header/ci/internal/ip_timestamp.c
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause */
+/* X-SPDX-Copyright-Text: (c) Copyright 2023 Xilinx, Inc. */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  printf("*** simulating test %s\n", argv[0]);
+}
