831cdcbfeb970a10a98dd03e590c9e1d6a6ae7aa
Willem de Bruijn <willemb@google.com>
2023-08-21 13:54:38 -0400

    af_xdp: add XDP program that supports rx timestamps
    XDP rx metadata [1] makes it possible to store additional metadata
    along with the packet contents in the AF_XDP buffer. The metadata
    precedes packet contents.
    
    This program defines a struct onload_xdp_rx_meta metadata layout. The
    Onload recv code in netif_event.c must use the same definition.
    
    One type of metadata that can be accessed is the NIC hardware receive
    timestamp, by calling bpf_xdp_metadata_rx_timestamp from the XDP prog.
    This calls into device driver callback logic to extract the receive
    timestamp from the device specific descriptor. This function may fail,
    as it requires explicit driver support. It is expected to succeed, as
    support for the feature has been tested in __af_xdp_nic_init_hardware
    (nic->net_dev->xdp_metadata_ops->xmo_rx_timestamp).
    
    [1] https://docs.kernel.org/networking/xdp-rx-metadata.html
    
    Signed-off-by: Willem de Bruijn <willemb@google.com>

---
+++ src/tools/bpf_link_helper/xdp_tstamp.c
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/* xdp_tstamp.c -- xdp program that reads optional hw rx timestamp
+ *
+ * See also https://docs.kernel.org/networking/xdp-rx-metadata.html
+ *
+ *
+ * # BUILD
+ *
+ * clang -target bpf -O2 -g -c xdp_tstamp.c -o ./xdp_tstamp.o
+ *
+ * # RUN
+ *
+ * # insert the xdp prog *before* loading the onload kernel modules
+ * ./xdp_onload_prepare $DEV xdp_tstamp.o
+ */
+
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <xdp/xdp_helpers.h>
+
+/* must match map created by onload/src/lib/efhw/af_xdp.c
+ *
+ * max_entries matches the number passed to
+ * /sys/module/sfc_resource/afxdp/register
+ */
+struct {
+	__uint(type, BPF_MAP_TYPE_XSKMAP);
+	__uint(key_size, sizeof(int));
+	__uint(value_size, sizeof(int));
+	__uint(max_entries, 4);
+} onload_xdp_xsk SEC(".maps");
+
+struct onload_xdp_rx_meta {
+#define ONLOAD_XDP_RX_META_TSTAMP 0x1
+	__u64 flags;
+	__u64 tstamp;
+};
+
+extern int bpf_xdp_metadata_rx_timestamp(const struct xdp_md *ctx,
+					 __u64 *timestamp) __ksym;
+
+SEC("xdp")
+int xdp_onload_prog(struct xdp_md *ctx)
+{
+	struct onload_xdp_rx_meta *meta;
+	void *data, *data_meta;
+	int ret;
+
+	ret = bpf_xdp_adjust_meta(ctx, -(int)(sizeof(struct onload_xdp_rx_meta)));
+	if (ret != 0)
+		return XDP_PASS;
+
+	data = (void *)(long)ctx->data;
+	data_meta = (void *)(long)ctx->data_meta;
+
+	if ((data_meta + sizeof(*meta)) > data)
+		return XDP_PASS;
+
+	meta = data_meta;
+
+	meta->tstamp = 0;
+	if (!bpf_xdp_metadata_rx_timestamp(ctx, &meta->tstamp))
+		meta->flags = ONLOAD_XDP_RX_META_TSTAMP;
+	else
+		meta->flags = 0;
+
+	return bpf_redirect_map(&onload_xdp_xsk, ctx->rx_queue_index, XDP_PASS);
+}
+
+char _license[] SEC("license") = "GPL";
