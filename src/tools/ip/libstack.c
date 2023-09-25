320c94934c7585a334fde6745c6fc14b943ca96f
Anantharam Kalepu <anatharam.kalepu@amd.com>
2023-09-25 11:24:37 +0100

    ON-15272: reverting back to printf() instead of ci_log_nonl()

--- src/tools/ip/libstack.c
+++ src/tools/ip/libstack.c
@@ -1055,7 +1055,10 @@ static int dump_via_buffers(oo_dump_request_fn_t dump_req_fn, void* arg,
     }
     rc = dump_req_fn(arg, buf, buf_len);
     if( rc >= 0 && rc <= buf_len )
-      ci_log_nonl("%s", buf);
+      /* This buffer often outputs more than 511 characters. 
+       * So, printf() is used instead of ci_log_nonl().
+       */
+      printf("%s", buf);
     free(buf);
     if( rc < 0 ) {
       if( flags & DVB_LOG_FAILURE )
