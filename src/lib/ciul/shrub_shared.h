f7eb12f694a38b8f0bdb4898ac8d48ffb9f2a527
Mike Seymour <mseymour@xilinx.com>
2023-11-07 15:48:17 +0000

    ON-15247: Protocol for sharing buffers via file descriptors and shared memory

---
+++ src/lib/ciul/shrub_shared.h
@@ -0,0 +1,62 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/* X-SPDX-Copyright-Text: (c) Copyright 2023 Advanced Micro Devices, Inc. */
+
+/* Shared Readable User Buffers, shared data structures
+ *
+ * Once a client connection has been established, it is managed via three
+ * shared data structures:
+ *
+ * The buffers themselves
+ * A "server" FIFO, to which the server posts buffers for consumption by clients
+ * A "client" FIFO, to which the client posts buffers to release after use
+ *
+ * The FIFOs are implemented as circularly-addressed arrays sufficiently large
+ * to contain all available buffers with at least one empty slot, so that
+ * readers can detect newly posted buffers with no further shared state.
+ */
+
+#include <stdint.h>
+
+/* Identifier for a buffer, an index into the shared buffer memory */
+typedef uint32_t ef_shrub_buffer_id;
+
+/* Protocol version, to check compatibility between client and server */
+#define EF_SHRUB_VERSION 1
+
+/* An identifier that does not represent a buffer, used to indicate empty
+ * slots in the FIFOs.
+ */
+#define EF_SHRUB_INVALID_BUFFER ((ef_shrub_buffer_id)(-1))
+
+/* Memory is shared via an array of file descriptors passed as ancilliary
+ * data alongside the metrics. These are the indexes and size for the array.
+ */
+#define EF_SHRUB_FD_BUFFERS     0
+#define EF_SHRUB_FD_SERVER_FIFO 1
+#define EF_SHRUB_FD_CLIENT_FIFO 2
+#define EF_SHRUB_FD_COUNT       3
+
+/* This structure is sent to each client immediately after accepting a
+ * connection, providing the information needed to access shared data.
+ */ 
+struct ef_shrub_shared_metrics {
+  /* Server's protocol version, to check compatibility */
+  uint64_t server_version;
+
+  /* Mapping information for shared buffer memory.
+   * Read only for clients.
+   * Offset is zero, length is buffer_bytes * buffer_count */
+  uint64_t buffer_bytes;
+  uint64_t buffer_count;
+
+  /* Mapping information for the FIFO for the server to post buffers to clients.
+   * Read only for clients.
+   * Offset is zero, length is sizeof(ef_shrub_buffer_id) * size */
+  uint64_t server_fifo_size;
+
+  /* Mapping information for the FIFO for clients to release buffers to server.
+   * Write only for clients.
+   * Offset is provided, length is sizeof(ef_shrub_buffer_id) * size */
+  uint64_t client_fifo_offset;
+  uint64_t client_fifo_size;
+};
