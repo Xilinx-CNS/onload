Xilinx-CNS/onload

commit e34a911603afb2a20e08715d097adbf88b5f99bc

driver: compat for lookup_fd_rcu()
Linux-6.7 replaces lookup_fd_rcu with lookup_fdget_rcu.
Make a compat code for that.

See Linux commit 0ede61d8589cc2d93aa78230d74ac58b5b8d0244

Signed-off-by: Sergey Nikitin <sergey.nikitin@oktet.tech>

src/driver/linux_onload/onload_kernel_compat.h
@@ -115,13 +115,17 @@ static inline int efrm_unregister_netdevice_notifier(struct notifier_block *b)
 #define unregister_netdevice_notifier efrm_unregister_netdevice_notifier
 #endif
 
-/* Linux-5.11 introduces lookup_fd_rcu().
- * It was called fcheck() before.
- */
-#ifndef EFRM_HAS_LOOKUP_FD_RCU
-static inline struct file *lookup_fd_rcu(unsigned int fd)
+/* Linux-6.7 introduces lookup_fdget_rcu().
+ * 5.11 >= linux < 6.7 - lookup_fd_rcu().
+ * linux < 5.11 - fcheck(). */
+#ifndef EFRM_HAS_LOOKUP_FDGET_RCU
+static inline struct file *lookup_fdget_rcu(unsigned int fd)
 {
+#ifdef EFRM_HAS_LOOKUP_FD_RCU
+  return lookup_fd_rcu(fd);
+#else
   return fcheck(fd);
+#endif
 }
 #endif
 

src/driver/linux_onload/ossock_calls.c
@@ -112,7 +112,7 @@ oo_fd_replace_file(struct file* old_filp, struct file* new_filp,
   }
 
   rcu_read_lock();
-  if( lookup_fd_rcu(old_fd) != old_filp ) {
+  if( lookup_fdget_rcu(old_fd) != old_filp ) {
     rcu_read_unlock();
     spin_unlock(&current->files->file_lock);
     return -EINVAL;

src/driver/linux_resource/kernel_compat.sh
@@ -140,6 +140,7 @@ EFRM_HAS_REMAP_VMALLOC_RANGE_PARTIAL	export	remap_vmalloc_range_partial	include/
 EFRM_HAS_KTIME_GET_REAL_SECONDS	export	ktime_get_real_seconds	include/linux/timekeeping.h	kernel/time/timekeeping.c
 EFRM_FILE_HAS_F_EP	member	struct_file	f_ep	include/linux/fs.h
 EFRM_HAS_LOOKUP_FD_RCU	symbol	lookup_fd_rcu	include/linux/fdtable.h
+EFRM_HAS_LOOKUP_FDGET_RCU	symbol	lookup_fdget_rcu	include/linux/fdtable.h
 
 EFRM_HAS_FLUSH_DELAYED_FPUT	export	flush_delayed_fput	include/linux/file.h	fs/file_table.c
 

commit 099239136d943d81a3bc7a4d0f91ff293e44ab15

driver/onload: simplify compat code for oo_fd_replace_file()
Replace linux-version-dependent code for obtaining file pointer
out of the descriptor with simple fget(). It allows not to deal
with different linux versions.

fget() takes a file reference, so we must do extra fput().
Since the new `lookup_fdget_rcu()`, which we were supposed
to use, do it either, there is no reason to use it.
Call fget() instead and remove all the related compat code.

Fixes: e34a911603af ("driver: compat for lookup_fd_rcu()")
Signed-off-by: Sergey Nikitin <sergey.nikitin@oktet.tech>

src/driver/linux_onload/onload_kernel_compat.h
@@ -115,18 +115,5 @@ static inline int efrm_unregister_netdevice_notifier(struct notifier_block *b)
 #define unregister_netdevice_notifier efrm_unregister_netdevice_notifier
 #endif
 
-/* Linux-6.7 introduces lookup_fdget_rcu().
- * 5.11 >= linux < 6.7 - lookup_fd_rcu().
- * linux < 5.11 - fcheck(). */
-#ifndef EFRM_HAS_LOOKUP_FDGET_RCU
-static inline struct file *lookup_fdget_rcu(unsigned int fd)
-{
-#ifdef EFRM_HAS_LOOKUP_FD_RCU
-  return lookup_fd_rcu(fd);
-#else
-  return fcheck(fd);
-#endif
-}
-#endif
 
 #endif /* __ONLOAD_KERNEL_COMPAT_H__ */

src/driver/linux_onload/ossock_calls.c
@@ -81,6 +81,8 @@ static int
 oo_fd_replace_file(struct file* old_filp, struct file* new_filp,
                    int old_fd, int* new_fd_p)
 {
+  struct file* tmp_filp = NULL;
+
   task_lock(current);
   if( atomic_read(&current->files->count) != 1 ) {
     /* This is a multithreaded application, and someone can be already
@@ -111,16 +113,18 @@ oo_fd_replace_file(struct file* old_filp, struct file* new_filp,
     return 0;
   }
 
-  rcu_read_lock();
-  if( lookup_fdget_rcu(old_fd) != old_filp ) {
-    rcu_read_unlock();
+  tmp_filp = fget(old_fd);
+  if( tmp_filp != old_filp ) {
+    if( tmp_filp )
+      fput(tmp_filp);
     task_unlock(current);
     return -EINVAL;
   }
 
+  fput(tmp_filp);
+
   get_file(new_filp);
   rcu_assign_pointer(files_fdtable(current->files)->fd[old_fd], new_filp);
-  rcu_read_unlock();
   task_unlock(current);
 
   /* No synchronize_rcu() is needed here.  See do_dup2() for an example,

src/driver/linux_resource/kernel_compat.sh
@@ -124,8 +124,6 @@ EFRM_HAS_REMAP_VMALLOC_RANGE_PARTIAL	export	remap_vmalloc_range_partial	include/
 
 EFRM_HAS_KTIME_GET_REAL_SECONDS	export	ktime_get_real_seconds	include/linux/timekeeping.h	kernel/time/timekeeping.c
 EFRM_FILE_HAS_F_EP	member	struct_file	f_ep	include/linux/fs.h
-EFRM_HAS_LOOKUP_FD_RCU	symbol	lookup_fd_rcu	include/linux/fdtable.h
-EFRM_HAS_LOOKUP_FDGET_RCU	symbol	lookup_fdget_rcu	include/linux/fdtable.h
 
 EFRM_HAS_FLUSH_DELAYED_FPUT	export	flush_delayed_fput	include/linux/file.h	fs/file_table.c
 