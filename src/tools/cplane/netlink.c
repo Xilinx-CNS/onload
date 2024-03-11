Xilinx-CNS/onload

commit a3ab3538d6a2e97e65b16f157ec4f5653418fc98

ON-12878: Expose bond master ifindex through the MIB
It would have been possible to expose the entire bond table through the
MIB instead, i.e. revert 7439705ca1, but that includes a whole bunch of
irrelevant stuff and brings MIB_UPDATE_LOOP complexity to that data
structure, which is annoying. Just adding the field we need to the
existing LLAP table is easier.

src/tools/cplane/netlink.c
@@ -412,6 +412,8 @@ llap_handle(struct cp_session* s, uint16_t nlmsg_type,
 
   if( link_ifindex == 0)
     link_ifindex = ifindex;
+  if( master_ifindex == 0)
+    master_ifindex = ifindex;
 
   cicp_rowid_t vlan_rowid = CICP_ROWID_BAD;
   const cicp_llap_row_t* lower_llap = NULL;
@@ -503,6 +505,7 @@ llap_handle(struct cp_session* s, uint16_t nlmsg_type,
           ((llap->flags & CP_LLAP_IMPORTED) != 0 &&
             (llap->encap.type & type) != type) ||
           llap->encap.link_ifindex != link_ifindex ||
+          llap->encap.master_ifindex != master_ifindex ||
           llap->xdp_prog_id != xdp_prog_id ) {
         changed = true;
         old_rx_hwports = llap->rx_hwports;
@@ -521,6 +524,7 @@ llap_handle(struct cp_session* s, uint16_t nlmsg_type,
         llap->encap.type &=~ (s->llap_type_os_mask & ~type);
 
         llap->encap.link_ifindex = link_ifindex;
+        llap->encap.master_ifindex = master_ifindex;
         llap->encap.vlan_id = vlan;
         if( vlan_rowid != CICP_ROWID_BAD ) {
           if( ! unsupported ) {

src/include/cplane/mib.h
@@ -110,6 +110,7 @@ typedef struct {
   cicp_llap_type_t type;
   ci_uint16 vlan_id;
   ci_ifid_t link_ifindex;     /*< ifindex for VLAN master, veth-peer, etc. */
+  ci_ifid_t master_ifindex;   /*< ifindex for bond, if this is a slave */
 } cicp_encap_t;
 
 

commit e027058de5dfca2180fa287b6990bb77147a9bcb

ON-12878: Fix teaming tests, broken by a3ab3538d6

src/tools/cplane/netlink.c
@@ -412,8 +412,9 @@ llap_handle(struct cp_session* s, uint16_t nlmsg_type,
 
   if( link_ifindex == 0)
     link_ifindex = ifindex;
-  if( master_ifindex == 0)
-    master_ifindex = ifindex;
+  int encap_master_ifindex = master_ifindex;
+  if( encap_master_ifindex == 0)
+    encap_master_ifindex = ifindex;
 
   cicp_rowid_t vlan_rowid = CICP_ROWID_BAD;
   const cicp_llap_row_t* lower_llap = NULL;
@@ -505,7 +506,7 @@ llap_handle(struct cp_session* s, uint16_t nlmsg_type,
           ((llap->flags & CP_LLAP_IMPORTED) != 0 &&
             (llap->encap.type & type) != type) ||
           llap->encap.link_ifindex != link_ifindex ||
-          llap->encap.master_ifindex != master_ifindex ||
+          llap->encap.master_ifindex != encap_master_ifindex ||
           llap->xdp_prog_id != xdp_prog_id ) {
         changed = true;
         old_rx_hwports = llap->rx_hwports;
@@ -524,7 +525,7 @@ llap_handle(struct cp_session* s, uint16_t nlmsg_type,
         llap->encap.type &=~ (s->llap_type_os_mask & ~type);
 
         llap->encap.link_ifindex = link_ifindex;
-        llap->encap.master_ifindex = master_ifindex;
+        llap->encap.master_ifindex = encap_master_ifindex;
         llap->encap.vlan_id = vlan;
         if( vlan_rowid != CICP_ROWID_BAD ) {
           if( ! unsupported ) {