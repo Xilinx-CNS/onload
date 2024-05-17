6b4ce40922945afb32c63d17a86eb5bfa1981aa2
Siân James <sian.james@amd.com>
2024-05-17 10:15:29 +0100

    ON-15657: Fix potential issue causing interrupts to stop being generated.
    The problematic code path:
    
    - efab_tcp_helper_netif_lock_callback is entered in dl_context with the prime
      flag set
    - ci_netif_unlock_slow_common will clear the prime flag from the lock
    - in most conditions we would then handle the prime after exiting the loop
    - if another flag has been set on the lock during the loop we will reloop
    - there are multiple local variables tracking flags we have cleared by not
      handled, the prime flag is only tracked through the local after_unlock_flags
    - if the flag that caused the reloop is one that cannot be handled in
      dl_context we will reset the flags we haven't and defer the handling to a
      safe context by resetting unhandled flags on the lock
    - the reset of unhandled flags does not include the prime or pkt_wake flag
      and so these are lost without handling them

--- src/lib/efthrm/tcp_helper_resource.c
+++ src/lib/efthrm/tcp_helper_resource.c
@@ -7970,7 +7970,8 @@ efab_tcp_helper_netif_lock_callback(eplock_helper_t* epl, ci_uint64 lock_val,
                              __FUNCTION__, thr->id));
 
         /* set flags in case we get deferred */
-        ef_eplock_holder_set_flags(&ni->state->lock, flags_set);
+        ef_eplock_holder_set_flags(&ni->state->lock,
+                                   flags_set | after_unlock_flags);
 
         /* lets clear IN_DL_CONTEXT flag as expected by oo_trusted_lock_drop */
         if( in_dl_context )
@@ -8001,7 +8002,8 @@ efab_tcp_helper_netif_lock_callback(eplock_helper_t* epl, ci_uint64 lock_val,
       OO_DEBUG_TCPH(ci_log("%s: [%u] defer to workitem, flags %llx",
                            __FUNCTION__, thr->id, lock_val));
       /* set flags so work item can pick up the work */
-      ef_eplock_holder_set_flags(&ni->state->lock, flags_set);
+      ef_eplock_holder_set_flags(&ni->state->lock,
+                                 flags_set | after_unlock_flags);
       tcp_helper_defer_dl2work(thr, OO_THR_AFLAG_UNLOCK_UNTRUSTED);
       return 0;
     }
