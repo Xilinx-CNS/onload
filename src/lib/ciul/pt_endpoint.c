81c7484f83d9c0fa6e30eeb1c9f6c68ff79fd218
Thomas Crawley <thomas.crawley@amd.com>
2024-05-28 09:11:08 +0100

    Cleanup: Fix char interface hash

--- src/lib/ciul/pt_endpoint.c
+++ src/lib/ciul/pt_endpoint.c
@@ -370,7 +370,7 @@ void ef_vi_set_intf_ver(char* intf_ver, size_t len)
    * It'd also be possible to enhance the checksum computation to be smarter
    * (e.g. by ignoring comments, etc.).
    */
-  if( strcmp(EFCH_INTF_VER, "905a7723b5b3fd4163e5315f76585862") ) {
+  if( strcmp(EFCH_INTF_VER, "0e632659c5a27e20c837adb613029099") ) {
     fprintf(stderr, "ef_vi: ERROR: char interface has changed\n");
     abort();
   }
