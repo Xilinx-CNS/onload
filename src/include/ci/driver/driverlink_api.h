ea585d1afcff6b4d6729f8abee4c567eb6ec42ba
Siân James <sian.james@amd.com>
2024-08-01 15:14:04 +0100

    ON-15766: Fix RHEL8.6 build failure

--- src/include/ci/driver/driverlink_api.h
+++ src/include/ci/driver/driverlink_api.h
@@ -3,6 +3,16 @@
 #ifndef __CI_DRIVER_DRIVERLINK_API__
 #define __CI_DRIVER_DRIVERLINK_API__
 
+#if !defined(struct_group)
+#define __struct_group(TAG, NAME, ATTRS, MEMBERS...) \
+        union { \
+                struct { MEMBERS } ATTRS; \
+                struct TAG { MEMBERS } ATTRS NAME; \
+        }
+#define struct_group(NAME, MEMBERS...)  \
+        __struct_group(/* no tag */, NAME, /* no attrs */, MEMBERS)
+#endif
+
 #include <../driver/linux_net/drivers/net/ethernet/sfc/filter.h>
 
 #endif  /* __CI_DRIVER_DRIVERLINK_API__ */
