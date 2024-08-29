6a3e598956a5f16509d36482af17c900fc521ebd
Andrew Bower <andrew.bower@amd.com>
2024-08-29 11:14:15 +0100

    cleanup: appease scanner

--- scripts/onload_misc/setup.py
+++ scripts/onload_misc/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(name='OpenOnload utilities',
       version='1.0',
       author='David Riddoch',
-      author_email='driddoch@solarflare.com',
+      author_email='support-nic@amd.com',
       url='http://www.openonload.org/',
       py_modules=['sfcmask', 'sfcaffinity'],
       )
