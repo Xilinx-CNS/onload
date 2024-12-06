258032a03dc1caf3032429f180e9ab7b3cdad8b5
Liam Gallagher <liam.gallagher@amd.com>
2024-12-06 16:58:52 +0000

    ON-16214: Fix typo in onload 9.0 merge
    Replace lower cased macros with their true upper case form.

--- src/lib/efhw/efct.c
+++ src/lib/efhw/efct.c
@@ -161,7 +161,7 @@ efct_nic_shared_rxq_refresh(struct efhw_nic *nic, int hwqid,
                n_hugepages * CI_EFCT_SUPERBUFS_PER_PAGE);
   }
 
-  pages = kmalloc_array(ci_efct_max_hugepages, sizeof(pages[0]), gfp_kernel);
+  pages = kmalloc_array(CI_EFCT_MAX_HUGEPAGES, sizeof(pages[0]), GFP_KERNEL);
   if (!pages)
     return -ENOMEM;
 
