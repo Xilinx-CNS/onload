0e63405dbab56d504c10fed9e02a9ae53a764eaf
Josh Featherstone <Josh.Featherstone@amd.com>
2025-05-07 15:03:14 +0100

    hack: temporarily skip the cplane system tests
    I had believed this wasn't needed on branches above onload-8.1, but
    it seems that wasn't the case, so this has been cherry-picked and
    reapplied where the previous merge excluded it.
    
    (cherry picked from commit 6b98ed7e3beb7292ba8387c069cf4e3d723ead11)

--- scripts/jenkins/ut_pipeline.groovy
+++ scripts/jenkins/ut_pipeline.groovy
@@ -112,8 +112,11 @@ void doTests() {
             sh(script: "$path make -C build/gnu_x86_64/tests/onload/onload_remote_monitor/internal_tests test")
           },
           "cplane system": {
+            sh(script: "echo skipping cplane system tests for now")
+            /*
             sh(script: "$path make -C build/gnu_x86_64/tests/onload/cplane_sysunit all")
             sh(script: "$path make -C build/gnu_x86_64/tests/onload/cplane_sysunit test")
+            */
           }
         ])
       }
