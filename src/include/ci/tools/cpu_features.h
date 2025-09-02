9b8fd34b671a1608c17c96c5dd35c059ff3ab3b7
Josh Featherstone <Josh.Featherstone@amd.com>
2025-09-02 14:00:40 +0100

    ON-16868: make ci_cpu_has_feature more generic
    The current implementation of ci_cpu_has_feature matches a string and
    then inspects the output of the required register to see which bit is
    set. This notably only allows for checking of the 64 CPU features
    included in the result of cpuid when EAX=1 (EDX and ECX are set).
    
    This isn't particularly extensible as we may want to check other
    features which require different input values for EAX (and ECX).
    This commit refactors this code to allow a generic request with
    options for setting the input values and specification of the
    expected output register and bit.

--- src/include/ci/tools/cpu_features.h
+++ src/include/ci/tools/cpu_features.h
@@ -16,41 +16,85 @@
 #ifndef __CI_TOOLS_CPU_FEATURES_H__
 #define __CI_TOOLS_CPU_FEATURES_H__
 
+/* It is perhaps possible to limit the size of the struct as the set of
+ * possible values is far smaller than sizeof(int). I don't expect this
+ * really has much/any benefit over this more explicit approach though. */
+typedef struct ci_cpu_feature_s {
+  int eax;
+  int ecx;
+  int output_register;
+  int output_bitmask;
+} ci_cpu_feature_t;
+
+#define CI_CPU_FEATURE_REGISTER_EAX 0
+#define CI_CPU_FEATURE_REGISTER_EBX 1
+#define CI_CPU_FEATURE_REGISTER_ECX 2
+#define CI_CPU_FEATURE_REGISTER_EDX 3
+#define CI_CPU_FEATURE_REGISTER_COUNT 4
+
+#define CI_CPU_FEATURE(eax_val, ecx_val, reg, bit) \
+  ((ci_cpu_feature_t) { \
+    .eax = eax_val, \
+    .ecx = ecx_val, \
+    .output_register = CI_CPU_FEATURE_REGISTER_##reg, \
+    .output_bitmask = 1 << bit \
+  })
+
+#define CI_CPU_FEATURE_PCLMULQDQ CI_CPU_FEATURE(1, 0, ECX, 1)
+
 #if defined(__i386__)
 #if defined(__GNUC__)
 ci_inline void
-get_cpuid(int op, int *eax, int *ebx, int *ecx, int *edx)
+get_cpuid(int in_eax, int in_ecx, int *eax, int *ebx, int *ecx, int *edx)
 {
   /* NB. We have to save [ebx] when building position indepent code. */
   __asm__ __volatile__ ("pushl %%ebx; cpuid; mov %%ebx, %0; popl %%ebx"
 			: "=r" (*ebx), "=a" (*eax), "=c" (*ecx), "=d" (*edx)
-			: "a" (op));
+			: "a" (in_eax), "c" (in_ecx));
 }
 #endif
 #elif defined(__x86_64__)
 ci_inline void
-get_cpuid(int op, int *eax, int *ebx, int *ecx, int *edx)
+get_cpuid(int in_eax, int in_ecx, int *eax, int *ebx, int *ecx, int *edx)
 {
   __asm__ __volatile__ ("cpuid\n\t"
                         : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
-                        : "a" (op));
+                        : "a" (in_eax), "c" (in_ecx));
 }
 #endif
 
-int ci_cpu_has_feature(char* feature)
+ci_inline int ci_cpu_has_feature(ci_cpu_feature_t feature)
 {
 #if defined(__x86_64__) || defined(__i386__)
-  int eax, ebx, ecx, edx;
+  static int max_feature_request = -1;
+  int registers[CI_CPU_FEATURE_REGISTER_COUNT];
 
-  /* Leaf 1 = CPUID feature bits */
-  get_cpuid(1, &eax, &ebx, &ecx, &edx);
+  /* This doesn't currently support extended feature queries, for which the MSB
+   * is set, though that has a separate maximum which needs checking. */
+  if(CI_UNLIKELY( max_feature_request == -1 )) {
+    get_cpuid(0, 0,
+              &registers[CI_CPU_FEATURE_REGISTER_EAX],
+              &registers[CI_CPU_FEATURE_REGISTER_EBX],
+              &registers[CI_CPU_FEATURE_REGISTER_ECX],
+              &registers[CI_CPU_FEATURE_REGISTER_EDX]);
+    max_feature_request = registers[CI_CPU_FEATURE_REGISTER_EAX];
+  }
 
-  if( ! strcmp(feature, "pclmul") )
-    return ecx & 0x00000002;
-#endif
+  if( feature.eax > max_feature_request )
+    return 0;
 
+  get_cpuid(feature.eax, feature.ecx,
+            &registers[CI_CPU_FEATURE_REGISTER_EAX],
+            &registers[CI_CPU_FEATURE_REGISTER_EBX],
+            &registers[CI_CPU_FEATURE_REGISTER_ECX],
+            &registers[CI_CPU_FEATURE_REGISTER_EDX]);
+
+  return (registers[feature.output_register] & feature.output_bitmask) ==
+         feature.output_bitmask;
+#else
   /* Not supported on platforms that don't implement the CPUID instruction */
   return 0;
+#endif
 }
 
 #endif  /* __CI_TOOLS_CPU_FEATURES_H__ */
