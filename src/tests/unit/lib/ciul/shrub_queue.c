33c48055daa63d623ce8d68717ba49633806cdc5
Josh Featherstone <Josh.Featherstone@amd.com>
2026-01-20 10:32:43 +0000

    ON-17146: unit test shrub fine-grained buffer reference tracking

--- src/tests/unit/lib/ciul/shrub_queue.c
+++ src/tests/unit/lib/ciul/shrub_queue.c
@@ -11,6 +11,8 @@
 #include "shrub_connection.h"
 #include <etherfabric/ef_vi.h>
 #include <sys/mman.h>
+#include <assert.h>
+#include <limits.h>
 
 static const int buffer_fd = 10;
 static const int server_fifo_fd = 11;
@@ -72,6 +74,7 @@ int ef_shrub_server_mmap(void** addr_out, size_t size,
 struct mock_connection
 {
   struct ef_shrub_connection connection;
+  uint64_t buffer_refs;
   struct ef_shrub_client_state state;
 };
 
@@ -79,6 +82,14 @@ struct ef_shrub_client_state*
 ef_shrub_connection_client_state(struct ef_shrub_connection* connection)
 {
   return &((struct mock_connection*)connection)->state;
+}
+
+static struct mock_connection*
+shrub_connection_to_mock_connection(struct ef_shrub_connection* shrub_conn)
+{
+  return (struct mock_connection*)(
+           (char*)shrub_conn - offsetof(struct mock_connection, connection)
+         );
 }
 
 static int mock_attach(struct ef_vi* vi_, int qid_, int buf_fd,
@@ -175,6 +186,7 @@ static struct mock_connection* open_connection(void)
   for( i = 0; i < fifo_size; ++i )
     mock->connection.fifo[i] = EF_SHRUB_INVALID_BUFFER;
 
+  mock->buffer_refs = 0ull;
   for( i = 0; i < queue->buffer_count; ++i )
     mock->connection.buffer_refs[i] = false;
 
@@ -182,6 +194,76 @@ static struct mock_connection* open_connection(void)
 
   ef_shrub_queue_attached(queue, &mock->connection);
   return mock;
+}
+
+static void remove_connection(struct ef_shrub_connection** list,
+                              struct ef_shrub_connection* conn)
+{
+  while( *list ) {
+    if( *list == conn ) {
+      *list = conn->next;
+      break;
+    }
+    list = &(*list)->next;
+  }
+}
+
+static void close_connection(struct mock_connection* conn)
+{
+  conn->connection.queue = NULL;
+  STATE_ACCEPT(conn, connection.queue);
+  remove_connection(&queue->connections, &conn->connection);
+  STATE_ACCEPT(queue, connections);
+}
+
+static void assert_queue_ref_counts_valid(struct ef_shrub_queue* queue)
+{
+  struct ef_shrub_connection *shrub_conn;
+  int buffer_idx;
+
+  /* Our tracking mask must have space for the number of buffers used */
+  assert(buffer_count <=
+         sizeof(((struct mock_connection*)NULL)->buffer_refs) * CHAR_BIT);
+
+  /* The real connection buffer_ref tracking must match our mock tracking */
+  for( shrub_conn = queue->connections;
+       shrub_conn;
+       shrub_conn = shrub_conn->next ) {
+    struct mock_connection* mock_conn =
+      shrub_connection_to_mock_connection(shrub_conn);
+    uint64_t real_ref_mask = 0ull;
+    for( buffer_idx = 0; buffer_idx < buffer_count; buffer_idx++ )
+      real_ref_mask |=
+        ((uint64_t)shrub_conn->buffer_refs[buffer_idx] << buffer_idx);
+
+    CHECK(mock_conn->buffer_refs, ==, real_ref_mask);
+  }
+
+  /* buffer[i] ref count = sum of connections with buffer_ref[i] */
+  for( buffer_idx = 0; buffer_idx < buffer_count; buffer_idx++ ) {
+    int ref_count = 0;
+    for( shrub_conn = queue->connections;
+         shrub_conn;
+         shrub_conn = shrub_conn->next )
+      ref_count += shrub_conn->buffer_refs[buffer_idx];
+
+    CHECK(ef_shrub_queue_buffer_get_ref_count(queue, buffer_idx), ==,
+          ref_count);
+  }
+}
+
+static void mock_connection_take_buffer_ref(struct mock_connection* conn,
+                                            int buffer_idx)
+{
+  conn->buffer_refs |= (1ull << buffer_idx);
+  STATE_ACCEPT(conn, buffer_refs);
+}
+
+static void mock_connection_drop_buffer_ref(struct mock_connection* conn,
+                                            int buffer_idx)
+{
+  conn->buffer_refs &= ~(1ull << buffer_idx);
+  STATE_ACCEPT(conn, buffer_refs);
 }
 
 /* Tests */
@@ -207,6 +289,7 @@ static void test_shrub_queue_open(void)
   CHECK(queue.vi, ==, vi);
   CHECK(queue.fifo, ==, server_fifo);
   CHECK(queue.connections, ==, NULL);
+  assert_queue_ref_counts_valid(&queue);
 
   ef_shrub_queue_close(&queue);
 
@@ -225,6 +308,7 @@ static void test_shrub_queue_connections(void)
   c[0] = open_connection();
   STATE_UPDATE(queue, connection_count, 1);
   STATE_UPDATE(c[0], state.server_fifo_index, 0);
+  assert_queue_ref_counts_valid(queue);
 
   /* First poll will put all available buffers in the queue
    * FIXME: probably should wait for connections. */
@@ -234,6 +318,10 @@ static void test_shrub_queue_connections(void)
   CHECK(buffer_seq, ==, old_seq + buffer_count);
   for( i = 0; i < buffer_count; ++i )
     CHECK(queue->fifo[i], ==, buffer_id(i));
+
+  for( i = 0; i < buffer_count; ++i )
+    mock_connection_take_buffer_ref(c[0], i);
+  assert_queue_ref_counts_valid(queue);
 
   /* New connection: oldest buffer is still at index zero */
   c[1] = open_connection();
@@ -245,6 +333,11 @@ static void test_shrub_queue_connections(void)
   ef_shrub_queue_poll(queue);
   STATE_UPDATE(c[0], connection.fifo_index, 1);
   CHECK(queue->fifo[0], ==, buffer_id(0));
+
+  for( i = 0; i < buffer_count; ++i )
+    mock_connection_take_buffer_ref(c[1], i);
+  mock_connection_drop_buffer_ref(c[0], 0);
+  assert_queue_ref_counts_valid(queue);
 
   c[1]->connection.fifo[0] = 0;
   expect_free = 0;
@@ -259,10 +352,18 @@ static void test_shrub_queue_connections(void)
     CHECK(queue->fifo[i], ==, buffer_id(i));
   CHECK(queue->fifo[buffer_count], ==, buffer_id(0));
 
+  /* We have dropped all references to the new buffer, and it has been reposted
+   * so now all connections have this buffer again. */
+  mock_connection_take_buffer_ref(c[0], 0);
+  assert_queue_ref_counts_valid(queue);
+
   /* A new connection will start at the next index (1) */
   c[2] = open_connection();
   STATE_UPDATE(queue, connection_count, 3);
   STATE_UPDATE(c[2], state.server_fifo_index, 1);
+  for( i = 0; i < buffer_count; ++i )
+    mock_connection_take_buffer_ref(c[2], i);
+  assert_queue_ref_counts_valid(queue);
 
   /* Free a more recent buffer */
   expect_free = 3;
@@ -285,10 +386,23 @@ static void test_shrub_queue_connections(void)
   CHECK(queue->fifo[buffer_count + 1], ==, buffer_id(3));
   STATE_UPDATE(queue, fifo_index, buffer_count + 2);
 
+  /* All connections dropped their ref to `expect_free` at the same time so
+   * they should all have dropped and retaken references to it within the same
+   * poll. */
+  assert_queue_ref_counts_valid(queue);
+
   /* A new connection will start after the gap at index 4 */
   c[3] = open_connection();
   STATE_UPDATE(queue, connection_count, 4);
   STATE_UPDATE(c[3], state.server_fifo_index, 4);
+
+  /* By this point, 0 has been freed and reposted at the end, and 3 has been
+   * freed by all (and reposted to the fifo). So the new connection has refs
+   * to [3, buffer_count) and 0. */
+  mock_connection_take_buffer_ref(c[3], 0);
+  for( i = 3; i < buffer_count; i++ )
+    mock_connection_take_buffer_ref(c[3], i);
+  assert_queue_ref_counts_valid(queue);
 
   /* Release a buffer from three connections and close the fourth */
   c[0]->connection.fifo[2] = 5;
@@ -298,6 +412,11 @@ static void test_shrub_queue_connections(void)
   STATE_UPDATE(c[0], connection.fifo_index, 3);
   STATE_UPDATE(c[1], connection.fifo_index, 3);
   STATE_UPDATE(c[2], connection.fifo_index, 2);
+
+  mock_connection_drop_buffer_ref(c[0], 5);
+  mock_connection_drop_buffer_ref(c[1], 5);
+  mock_connection_drop_buffer_ref(c[2], 5);
+  assert_queue_ref_counts_valid(queue);
 
   expect_free = 5;
   ef_shrub_queue_detached(queue, &c[3]->connection);
@@ -310,11 +429,25 @@ static void test_shrub_queue_connections(void)
   CHECK(queue->fifo[buffer_count], ==, buffer_id(0));
   CHECK(queue->fifo[buffer_count + 1], ==, buffer_id(3));
 
+  /* The closed connection should have no remaining references */
+  for( i = 0; i < buffer_count; i++ )
+    mock_connection_drop_buffer_ref(c[3], i);
+  assert_queue_ref_counts_valid(queue);
+
+  /* Close the connection fully before the server recycles the buffer as we'll
+   * end up with a stale reference otherwise. */
+  close_connection(c[3]);
+
   /* The freed buffer is recycled on the next poll */
   CHECK(queue->fifo[buffer_count + 2], ==, EF_SHRUB_INVALID_BUFFER);
   ef_shrub_queue_poll(queue);
   CHECK(queue->fifo[buffer_count + 2], ==, buffer_id(5));
   STATE_UPDATE(queue, fifo_index, buffer_count + 3);
+
+  mock_connection_take_buffer_ref(c[0], 5);
+  mock_connection_take_buffer_ref(c[1], 5);
+  mock_connection_take_buffer_ref(c[2], 5);
+  assert_queue_ref_counts_valid(queue);
 
   /* Check that older buffers (no longer in the fifo) are freed correctly. */
   /* Leave buffer 1 until we have reused its old slot. */
@@ -324,6 +457,8 @@ static void test_shrub_queue_connections(void)
     c[1]->connection.fifo[3+i] = expect_free;
     c[2]->connection.fifo[2+i] = expect_free;
     ef_shrub_queue_poll(queue);
+    /* all references to this buffer dropped and reacquired */
+    assert_queue_ref_counts_valid(queue);
     STATE_UPDATE(c[0], connection.fifo_index, 4+i);
     STATE_UPDATE(c[1], connection.fifo_index, 4+i);
     STATE_UPDATE(c[2], connection.fifo_index, 3+i);
@@ -355,6 +490,9 @@ static void test_shrub_queue_connections(void)
   STATE_UPDATE(c[2], connection.fifo_index, 6);
   CHECK(expect_free, ==, -1);
   STATE_UPDATE(queue, fifo_index, 3);
+
+  /* all references to buffer 1 dropped and reacquired */
+  assert_queue_ref_counts_valid(queue);
 
   CHECK(queue->fifo[0], ==, buffer_id(4));
   CHECK(queue->fifo[1], ==, buffer_id(6));
