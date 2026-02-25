Xilinx-CNS/onload

commit c896f2dc7bb1e0fd9740b24962a96f5b9981dd78

ON-16312: restrict shrub fifo dumping to debug builds

src/tools/onload_remote_monitor/mmake.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0
 # X-SPDX-Copyright-Text: (c) Copyright 2014-2020 Xilinx, Inc.
 
-APPS := orm_json
+APPS :=
 
 SRCS := orm_json orm_json_lib
 

src/lib/ciul/shrub_connection.c
@@ -189,10 +189,12 @@ int ef_shrub_connection_attach_queue(struct ef_shrub_connection* connection,
   return 0;
 }
 
+#ifndef NDEBUG
 static int prev_fifo_index(int index, int fifo_size)
 {
   return index == 0 ? fifo_size - 1 : index - 1;
 }
+#endif
 
 void ef_shrub_connection_dump_to_fd(struct ef_shrub_connection* connection,
                                     int fd, char* buf, size_t buflen)
@@ -202,7 +204,9 @@ void ef_shrub_connection_dump_to_fd(struct ef_shrub_connection* connection,
   bool print_comma = false;
   int printed_chars = 0;
   int buffer_index;
+#ifndef NDEBUG
   int fifo_index;
+#endif
 
   shrub_log_to_fd(fd, buf, buflen, "    connection[fd %d]: "
                   "queue_fifo_index: %llu\n", connection->socket,
@@ -215,6 +219,7 @@ void ef_shrub_connection_dump_to_fd(struct ef_shrub_connection* connection,
                   connection->server_fifo_index,
                   client_state->server_fifo_index,
                   client_state->metrics.server_fifo_size);
+#ifndef NDEBUG
   for( fifo_index = prev_fifo_index(connection->server_fifo_index,
                                     client_state->metrics.server_fifo_size);
        connection->server_fifo[fifo_index] != EF_SHRUB_INVALID_BUFFER;
@@ -230,6 +235,7 @@ void ef_shrub_connection_dump_to_fd(struct ef_shrub_connection* connection,
                     "buffer_sbseq: %d\n", ef_shrub_buffer_sentinel(buffer_id),
                     ef_shrub_buffer_sbseq(buffer_id));
   }
+#endif
 
   shrub_log_to_fd(fd, buf, buflen,
                   "      client_fifo_index_write: %llu "
@@ -238,6 +244,7 @@ void ef_shrub_connection_dump_to_fd(struct ef_shrub_connection* connection,
                   client_state->client_fifo_index,
                   connection->client_fifo_index,
                   client_state->metrics.client_fifo_size);
+#ifndef NDEBUG
   for( fifo_index = prev_fifo_index(client_state->client_fifo_index,
                                     client_state->metrics.client_fifo_size);
        connection->client_fifo[fifo_index] != EF_SHRUB_INVALID_BUFFER;
@@ -253,6 +260,7 @@ void ef_shrub_connection_dump_to_fd(struct ef_shrub_connection* connection,
                     "buffer_sbseq: %d\n", ef_shrub_buffer_sentinel(buffer_id),
                     ef_shrub_buffer_sbseq(buffer_id));
   }
+#endif
 
   shrub_log_to_fd(fd, buf, buflen, "      referenced_buffer_count: %zu "
                   "max_referenced_buffers: %zu\n",

src/lib/ciul/shrub_queue.c
@@ -448,8 +448,10 @@ void ef_shrub_queue_dump_to_fd(struct ef_shrub_queue* queue, int fd,
   struct ef_shrub_connection *connection;
   ef_vi_efct_rxq_state *rxq_state =
     &queue->vi->ep_state->rxq.efct_state[queue->ix];
+#ifndef NDEBUG
   int fifo_index, buffer_index;
   int idx, iter;
+#endif
 
   shrub_log_to_fd(fd, buf, buflen, "  rxq[%d]: hw: %d\n",
                   queue->ix, rxq_state->qid);
@@ -461,6 +463,7 @@ void ef_shrub_queue_dump_to_fd(struct ef_shrub_queue* queue, int fd,
                   rxq_state->fifo_tail_sw, rxq_state->fifo_count_hw,
                   rxq_state->fifo_count_sw);
 
+#ifndef NDEBUG
   shrub_log_to_fd(fd, buf, buflen, "    free_list: ");
   for( idx = 0, iter = rxq_state->free_head;
        idx < queue->buffer_count && iter != -1;
@@ -484,8 +487,10 @@ void ef_shrub_queue_dump_to_fd(struct ef_shrub_queue* queue, int fd,
     shrub_log_to_fd(fd, buf, buflen, "%s%d", idx == 0 ? "" : " -> ", iter);
   }
   shrub_log_to_fd(fd, buf, buflen, "\n");
+#endif
 
   shrub_log_to_fd(fd, buf, buflen, "    fifo_size: %d\n", queue->fifo_size);
+#ifndef NDEBUG
   for( fifo_index = prev_fifo_index(queue->fifo_index, queue->fifo_size);
        queue->fifo[fifo_index] != EF_SHRUB_INVALID_BUFFER;
        fifo_index = prev_fifo_index(fifo_index, queue->fifo_size) ) {
@@ -497,10 +502,12 @@ void ef_shrub_queue_dump_to_fd(struct ef_shrub_queue* queue, int fd,
                     "buffer_sbseq: %d\n", ef_shrub_buffer_sentinel(buffer_id),
                     ef_shrub_buffer_sbseq(buffer_id));
   }
+#endif
 
   shrub_log_to_fd(fd, buf, buflen,
                   "    buffer_count: %zu reserved_buffer_count: %zu\n",
                   queue->buffer_count, queue->reserved_buffer_count);
+#ifndef NDEBUG
   for( buffer_index = 0; buffer_index < queue->buffer_count; buffer_index++ ) {
     struct ef_shrub_queue_buffer* buffer = &queue->buffers[buffer_index];
     if( buffer->ref_count != 0 ) {
@@ -509,6 +516,7 @@ void ef_shrub_queue_dump_to_fd(struct ef_shrub_queue* queue, int fd,
                       buffer->fifo_index);
     }
   }
+#endif
 
   shrub_log_to_fd(fd, buf, buflen, "    connection_count: %llu\n",
                   queue->connection_count);

commit 271dacdee6943d8c3d09746b809cdecff7e945b7

ON-17230, ON-16312: readd orm_json to the build
This was accidentally removed in an unrelated commit.

Fixes: c896f2dc7bb1 (ON-16312: restrict shrub fifo dumping to debug builds, 2026-02-20)

src/tools/onload_remote_monitor/mmake.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0
 # X-SPDX-Copyright-Text: (c) Copyright 2014-2020 Xilinx, Inc.
 
-APPS :=
+APPS := orm_json
 
 SRCS := orm_json orm_json_lib
 