2d4b07f44e1950e5e20b66865e00e5fdf7c7282e
Mike Seymour <mseymour@xilinx.com>
2026-03-12 09:21:27 +0000

    cleanup: fix c99isms for older kernels

--- src/lib/ciul/shrub_socket_kernel.c
+++ src/lib/ciul/shrub_socket_kernel.c
@@ -4,15 +4,15 @@
 #include "ef_vi_internal.h"
 #include <etherfabric/internal/shrub_socket.h>
 
-int ef_shrub_socket_open(uintptr_t*) {return -ENOTSUPP;}
-int ef_shrub_socket_close_socket(uintptr_t) {return -ENOTSUPP;}
-int ef_shrub_socket_close_file(uintptr_t) {return -ENOTSUPP;}
-int ef_shrub_socket_connect(uintptr_t, const char*) {return -ENOTSUPP;}
-int ef_shrub_socket_send(uintptr_t, void*, size_t) {return -ENOTSUPP;}
-int ef_shrub_socket_recv(uintptr_t, void*, size_t) {return -ENOTSUPP;}
-int ef_shrub_socket_recv_metrics(struct ef_shrub_shared_metrics*,
-                                 uint64_t*, uintptr_t) {return -ENOTSUPP;}
-int ef_shrub_socket_mmap(uint64_t*, void*, size_t,
-                         uintptr_t, size_t, int) {return -ENOTSUPP;}
-void ef_shrub_socket_munmap(uint64_t, size_t, int) {}
+int ef_shrub_socket_open(uintptr_t* s) {return -ENOTSUPP;}
+int ef_shrub_socket_close_socket(uintptr_t s) {return -ENOTSUPP;}
+int ef_shrub_socket_close_file(uintptr_t f) {return -ENOTSUPP;}
+int ef_shrub_socket_connect(uintptr_t s, const char* a) {return -ENOTSUPP;}
+int ef_shrub_socket_send(uintptr_t s, void* p, size_t n) {return -ENOTSUPP;}
+int ef_shrub_socket_recv(uintptr_t s, void* p, size_t n) {return -ENOTSUPP;}
+int ef_shrub_socket_recv_metrics(struct ef_shrub_shared_metrics* m,
+                                 uint64_t* p, uintptr_t n) {return -ENOTSUPP;}
+int ef_shrub_socket_mmap(uint64_t* m, void* a, size_t s,
+                         uintptr_t f, size_t o, int t) {return -ENOTSUPP;}
+void ef_shrub_socket_munmap(uint64_t m, size_t n, int t) {}
 
